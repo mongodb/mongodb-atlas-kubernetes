dec982bc54ef612fae539c6189ad47f8df7a2772
antonlisovenko <anton.lisovenko@mongodb.com>
2020-11-25 10:20:49 +0000

    CLOUDP-77633: scaffolding the Atlas Project controller

--- main.go
+++ main.go
@@ -75,6 +75,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AtlasCluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.AtlasProjectReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("AtlasProject"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AtlasProject")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
