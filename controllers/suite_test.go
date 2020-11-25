f3a203698c372fea801c4132048a5681c7180595
antonlisovenko <anton.lisovenko@mongodb.com>
2020-11-25 15:49:23 +0000

    Removing wrong scaffolded code

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -65,9 +65,6 @@ var _ = BeforeSuite(func(done Done) {
 	err = mongodbcomv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
-	err = mongodbcomv1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
 	// +kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
