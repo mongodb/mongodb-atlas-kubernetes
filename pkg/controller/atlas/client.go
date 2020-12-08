cd466d85cac6d9102982d3baa08c05a231184601
antonlisovenko <anton.lisovenko@mongodb.com>
2020-12-08 11:32:16 +0000

    user agent

--- pkg/controller/atlas/client.go
+++ pkg/controller/atlas/client.go
@@ -1,11 +1,17 @@
 package atlas
 
 import (
+	"fmt"
+	"runtime"
+
 	"github.com/mongodb-forks/digest"
 	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/util/httputil"
 	"go.mongodb.org/atlas/mongodbatlas"
 	"go.uber.org/zap"
 )
+
+// TODO proper version passed on compile time
+var userAgent = fmt.Sprintf("%s/%s (%s;%s)", "MongoDBAtlasKubernetesOperator", "version TODO", runtime.GOOS, runtime.GOARCH)
 
 // AtlasClient is the central place to create a client for Atlas using specified API keys and a server URL.
 // Note, that the default HTTP transport is reused globally by Go so all caching, keep-alive etc will be in action.
@@ -17,5 +23,10 @@ func AtlasClient(connection Connection, log *zap.SugaredLogger) (*mongodbatlas.C
 	}
 	loggingClient := httputil.NewLoggingClient(*tc, log)
 	// TODO configuration for base URL (as a global Operator config?)
-	return mongodbatlas.New(&loggingClient, mongodbatlas.SetBaseURL("https://cloud-qa.mongodb.com/api/atlas/v1.0/"))
+	client, err := mongodbatlas.New(&loggingClient, mongodbatlas.SetBaseURL("https://cloud-qa.mongodb.com/api/atlas/v1.0/"))
+	if err != nil {
+		return nil, err
+	}
+	client.UserAgent = userAgent
+	return client, err
 }
