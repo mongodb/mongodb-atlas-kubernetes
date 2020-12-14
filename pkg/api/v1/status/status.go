5925d70ce8ec9eef75bafac8653bef2a89a7d1a6
antonlisovenko <anton.lisovenko@mongodb.com>
2020-12-14 15:51:52 +0000

    wip

--- pkg/api/v1/status/status.go
+++ pkg/api/v1/status/status.go
@@ -7,6 +7,6 @@ type Reader interface {
 
 // Common is the struct shared by all statuses in existing Custom Resources.
 type Common struct {
-	// The phase the current Custom Resource is in. Possible values: 'Reconciling', 'Pending', 'Running', 'Failed'
+	// The phase the current Custom Resource is in. Possible values: "Reconciling", "Pending", "Running", "Failed"
 	Phase Phase `json:"phase"`
 }
