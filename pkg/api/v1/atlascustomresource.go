9f43289b05e1838e1aa77b96f3fdfb908ff0ec05
Anton <anton.lisovenko@10gen.com>
2020-12-15 16:10:59 +0000

    CLOUDP-78895: basic support for status (#15)

---
+++ pkg/api/v1/atlascustomresource.go
@@ -0,0 +1,15 @@
+package v1
+
+import (
+	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/api/v1/status"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+//+k8s:deepcopy-gen=false
+// AtlasCustomResource is the interface common for all Atlas entities
+type AtlasCustomResource interface {
+	runtime.Object
+	status.Reader
+}
+
+var _ AtlasCustomResource = &AtlasProject{}
