c2f49b643c285b2b8c1604ecd3f0a903cee6cbb6
Pavel <explosere@gmail.com>
2020-12-29 19:10:39 +0300

    Fix autogenerated stuff

--- pkg/api/v1/status/atlascluster.go
+++ pkg/api/v1/status/atlascluster.go
@@ -5,15 +5,28 @@ import "go.mongodb.org/atlas/mongodbatlas"
 // AtlasClusterStatus defines the observed state of AtlasCluster.
 type AtlasClusterStatus struct {
 	Common            `json:",inline"`
-	StateName         string                          `json:"stateName,omitempty"`
-	MongoDBVersion    string                          `json:"mongoDBVersion,omitempty"`
-	ConnectionStrings *mongodbatlas.ConnectionStrings `json:"connectionStrings"`
-	MongoURIUpdated   string                          `json:"mongoURIUpdated,omitempty"`
+	StateName         string             `json:"stateName,omitempty"`
+	MongoDBVersion    string             `json:"mongoDBVersion,omitempty"`
+	ConnectionStrings *ConnectionStrings `json:"connectionStrings"`
+	MongoURIUpdated   string             `json:"mongoURIUpdated,omitempty"`
 }
+
+// ConnectionStrings is a copy of mongodbatlas.ConnectionStrings for deepcopy compatibility purposes.
+type ConnectionStrings struct {
+	Standard          string            `json:"standard,omitempty"`
+	StandardSrv       string            `json:"standardSrv,omitempty"`
+	AwsPrivateLink    map[string]string `json:"awsPrivateLink,omitempty"`
+	AwsPrivateLinkSrv map[string]string `json:"awsPrivateLinkSrv,omitempty"`
+	Private           string            `json:"private,omitempty"`
+	PrivateSrv        string            `json:"privateSrv,omitempty"`
+}
+
+// check that the two types are compatible.
+var _ = ConnectionStrings(mongodbatlas.ConnectionStrings{})
 
 // +k8s:deepcopy-gen=false
 
-// AtlasClusterStatusOption is the option that is applied to Atlas Project Status
+// AtlasClusterStatusOption is the option that is applied to Atlas Project Status.
 type AtlasClusterStatusOption func(s *AtlasClusterStatus)
 
 func AtlasClusterStateNameOption(stateName string) AtlasClusterStatusOption {
@@ -30,7 +43,8 @@ func AtlasClusterMongoDBVersionOption(mongoDBVersion string) AtlasClusterStatusO
 
 func AtlasClusterConnectionStringsOption(connectionStrings *mongodbatlas.ConnectionStrings) AtlasClusterStatusOption {
 	return func(s *AtlasClusterStatus) {
-		s.ConnectionStrings = connectionStrings
+		cs := ConnectionStrings(*connectionStrings)
+		s.ConnectionStrings = &cs
 	}
 }
 
