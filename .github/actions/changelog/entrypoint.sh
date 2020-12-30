fd311d638c0745a9fa1056b7acff902fcca66346
Svetlana <5437289+leo-ri@users.noreply.github.com>
2020-12-30 19:24:08 +0300

    CLOUDP-77933 GH actions: 2 steps release workflow (#21)

---
+++ .github/actions/changelog/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# simple changelog: get all commits beetween 2 tags and put them into file
+
+echo "Commits: " > changelog.md
+#find previos tag
+start_tag=$(git describe --abbrev=0 --tags --match 'v*')
+if [ -n "${start_tag}" ]; then
+    git log "${start_tag}...HEAD" --pretty=format:"- %s" >> changelog.md
+else
+    # first tag
+    git log --pretty=format:"- %s" >> changelog.md
+fi
+
+cat changelog.md
