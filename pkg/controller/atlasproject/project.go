c22aac8a08b5cdfd50bb2a8fa366fd47f23ddc90
antonlisovenko <anton.lisovenko@mongodb.com>
2020-12-30 14:18:17 +0000

    CLOUDP-79537: catch the API error if the project already exists

--- pkg/controller/atlasproject/project.go
+++ pkg/controller/atlasproject/project.go
@@ -2,10 +2,11 @@ package atlasproject
 
 import (
 	"context"
-
+	"errors"
 	mdbv1 "github.com/mongodb/mongodb-atlas-kubernetes/pkg/api/v1"
 	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/controller/atlas"
 	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/controller/workflow"
+	"github.com/prometheus/common/log"
 	"go.mongodb.org/atlas/mongodbatlas"
 )
 
@@ -18,23 +19,24 @@ func ensureProjectExists(ctx *workflow.Context, connection atlas.Connection, pro
 	// Try to find the project
 	p, _, err := client.Projects.GetOneProjectByName(context.Background(), project.Spec.Name)
 	if err != nil {
-		return "", workflow.Terminate(workflow.ProjectNotCreatedInAtlas, err.Error())
+		var apiError *mongodbatlas.ErrorResponse
+		if errors.As(err, &apiError) && apiError.ErrorCode == atlas.NotInGroup {
+			// Project doesn't exist? Try to create it
+			p = &mongodbatlas.Project{
+				OrgID: connection.OrgID,
+				Name:  project.Spec.Name,
+			}
+			if p, _, err = client.Projects.Create(context.Background(), p); err != nil {
+				return "", workflow.Terminate(workflow.ProjectNotCreatedInAtlas, err.Error())
+			}
+			ctx.Log.Infow("Created Atlas Project", "name", project.Spec.Name, "id", p.ID)
+		} else {
+			return "", workflow.Terminate(workflow.ProjectNotCreatedInAtlas, err.Error())
+		}
 	}
-	if p.ID != "" {
-		ctx.Log.Debugw("Found Atlas Project", "id", p.ID)
-		return p.ID, workflow.OK()
+	if p == nil || p.ID == "" {
+		log.Error("Project or its project ID are empty")
+		return "", workflow.Terminate(workflow.Internal, "")
 	}
-
-	// Otherwise try to create it
-	p = &mongodbatlas.Project{
-		OrgID: connection.OrgID,
-		Name:  project.Spec.Name,
-	}
-
-	if p, _, err = client.Projects.Create(context.Background(), p); err != nil {
-		return "", workflow.Terminate(workflow.ProjectNotCreatedInAtlas, err.Error())
-	}
-	ctx.Log.Infow("Created Atlas Project", "name", project.Spec.Name, "id", p.ID)
-
 	return p.ID, workflow.OK()
 }
