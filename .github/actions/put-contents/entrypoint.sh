66c706ba78684cbe365f4b9634722fcf228f1524
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-30 12:56:32 +0300

    gh actions: shellcheck

--- .github/actions/put-contents/entrypoint.sh
+++ .github/actions/put-contents/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 #commit file to the destination branch
 
-export MESSAGE="generated $FILE_TO_COMMIT"
-export SHA=$(git rev-parse $DESTINATION_BRANCH:$FILE_TO_COMMIT)
-export CONTENT=$(base64 $FILE_TO_COMMIT)
+MESSAGE="generated $FILE_TO_COMMIT"
+SHA=$(git rev-parse "$DESTINATION_BRANCH:$FILE_TO_COMMIT")
+CONTENT=$(base64 "$FILE_TO_COMMIT")
 echo "$DESTINATION_BRANCH:$FILE_TO_COMMIT:$SHA"
 
 # Commit to the branch
 if [ "$SHA" = "$DESTINATION_BRANCH:$FILE_TO_COMMIT" ]; then
     echo "File does not exist"
-    gh api --method PUT /repos/:owner/:repo/contents/$FILE_TO_COMMIT \
+    gh api --method PUT "/repos/:owner/:repo/contents/$FILE_TO_COMMIT" \
         --field message="$MESSAGE" \
         --field content="$CONTENT" \
         --field encoding="base64" \
         --field branch="$DESTINATION_BRANCH"
 else
     echo "File exists"
-    gh api --method PUT /repos/:owner/:repo/contents/$FILE_TO_COMMIT \
+    gh api --method PUT "/repos/:owner/:repo/contents/$FILE_TO_COMMIT" \
         --field message="$MESSAGE" \
         --field content="$CONTENT" \
         --field encoding="base64" \
