5e840de608ba3347e6ab9f5e56fba1173e2e1e54
Anton <anton.lisovenko@10gen.com>
2021-01-19 10:16:23 +0000

    Remove slashes from the tag while pushing Operator (#39)

--- .github/actions/set-tag/entrypoint.sh
+++ .github/actions/set-tag/entrypoint.sh
@@ -5,5 +5,6 @@ branch_name=$GITHUB_HEAD_REF
 if [ -z "${branch_name}" ]; then
     branch_name=$(echo "$GITHUB_REF" | awk -F'/' '{print $3}')
 fi
+branch_name=$(echo "${branch_name}" | sed  's/\//-/g')
 tag="${branch_name}-${commit_id}"
 echo "::set-output name=tag::$tag"
