5100cdf5e9cd8ad43342d5e29c64327d1f2a010f
antonlisovenko <anton.lisovenko@mongodb.com>
2021-01-21 18:32:26 +0000

    fix

--- .github/actions/int-test/entrypoint.sh
+++ .github/actions/int-test/entrypoint.sh
@@ -4,5 +4,7 @@
 export ATLAS_ORG_ID="${INPUT_ATLAS_ORG_ID}"
 export ATLAS_PUBLIC_KEY="${INPUT_ATLAS_PUBLIC_KEY}"
 export ATLAS_PRIVATE_KEY="${INPUT_ATLAS_PRIVATE_KEY}"
+# otherwise we may get strange "Detected Programmatic Focus - setting exit status to 197"
+export GINKGO_EDITOR_INTEGRATION="true"
 
 ginkgo -v ./test/int
