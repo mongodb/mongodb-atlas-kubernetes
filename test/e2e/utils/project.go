33c07481981cb0ffba66344bff7d060a57184dc8
Svetlana <5437289+leo-ri@users.noreply.github.com>
2021-02-10 16:53:20 +0300

    Cloudp-80268 e2e initial (#84)

---
+++ test/e2e/utils/project.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	v1 "github.com/mongodb/mongodb-atlas-kubernetes/pkg/api/v1"
+)
+
+type ISpec interface {
+	ProjectName(string) ISpec
+	SecretRef(string) ISpec
+	// TODO WhiteIP
+	CompleteK8sConfig(string) []byte
+}
+
+type ProjectSpec v1.AtlasProjectSpec
+
+type ap struct {
+	metav1.TypeMeta `json:",inline"`
+	ObjectMeta      *metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec            ProjectSpec        `json:"spec,omitempty"`
+}
+
+func NewProject() ISpec {
+	return &ProjectSpec{}
+}
+
+func (s *ProjectSpec) ProjectName(name string) ISpec {
+	s.Name = name
+	return s
+}
+
+func (s *ProjectSpec) SecretRef(name string) ISpec {
+	s.ConnectionSecret = &v1.ResourceRef{Name: name}
+	return s
+}
+
+func (s ProjectSpec) CompleteK8sConfig(k8sname string) []byte {
+	var t ap
+	t.TypeMeta = metav1.TypeMeta{
+		APIVersion: "atlas.mongodb.com/v1",
+		Kind:       "AtlasProject",
+	}
+	t.ObjectMeta = &metav1.ObjectMeta{
+		Name: k8sname,
+	}
+	t.Spec = s
+	yamlConf, _ := JSONToYAMLConvert(t)
+	return yamlConf
+}
---
+++ test/e2e/utils/utils.go
@@ -0,0 +1,89 @@
+package utils
+
+import (
+	"encoding/json"
+
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
+	yaml "gopkg.in/yaml.v3"
+
+	v1 "github.com/mongodb/mongodb-atlas-kubernetes/pkg/api/v1"
+)
+
+// LoadUserProjectConfig load configuration into object
+func LoadUserProjectConfig(path string) *v1.AtlasProject {
+	var config v1.AtlasProject
+	ReadInYAMLFileAndConvert(path, &config)
+	return &config
+}
+
+// LoadUserClusterConfig load configuration into object
+func LoadUserClusterConfig(path string) AC {
+	var config AC
+	ReadInYAMLFileAndConvert(path, &config)
+	return config
+}
+
+func SaveToFile(path string, data []byte) {
+	ioutil.WriteFile(path, data, 0777) //nolint:gosec // kubectl apply (?)
+}
+
+func JSONToYAMLConvert(cnfg interface{}) ([]byte, error) {
+	var jsonI interface{}
+	j, _ := json.Marshal(cnfg)
+	err := yaml.Unmarshal(j, &jsonI)
+	if err != nil {
+		return nil, err
+	}
+	return yaml.Marshal(jsonI)
+}
+
+// ReadInYAMLFileAndConvert reads in the yaml file given by the path given
+func ReadInYAMLFileAndConvert(pathToYamlFile string, cnfg interface{}) interface{} {
+	// Read in the yaml file at the path given
+	yamlFile, err := ioutil.ReadFile(filepath.Clean(pathToYamlFile))
+	if err != nil {
+		log.Printf("Error while parsing YAML file %v, error: %s", filepath.Clean(pathToYamlFile), err)
+	}
+
+	// Map yamlFile to interface
+	var body interface{}
+	if err := yaml.Unmarshal(yamlFile, &body); err != nil {
+		panic(err)
+	}
+
+	// Convert yaml to its json counterpart
+	body = ConvertYAMLtoJSONHelper(body)
+
+	// Generate json string from data structure
+	jsonFormat, err := json.Marshal(body)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(jsonFormat, &cnfg); err != nil {
+		panic(err)
+	}
+
+	return cnfg
+}
+
+// ConvertYAMLtoJSONHelper converts the yaml to json recursively
+func ConvertYAMLtoJSONHelper(i interface{}) interface{} {
+	switch item := i.(type) {
+	case map[interface{}]interface{}:
+		document := map[string]interface{}{}
+		for k, v := range item {
+			document[k.(string)] = ConvertYAMLtoJSONHelper(v)
+		}
+		return document
+	case []interface{}:
+		for i, arr := range item {
+			item[i] = ConvertYAMLtoJSONHelper(arr)
+		}
+	}
+
+	return i
+}
