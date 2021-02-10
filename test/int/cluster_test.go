3a1e0cf3e4c1c16fbf5dc9a1b03f7e9c975854d8
Pavel <explosere@gmail.com>
2021-02-10 18:11:19 +0300

    Fix timeout after merge

--- test/int/cluster_test.go
+++ test/int/cluster_test.go
@@ -67,7 +67,7 @@ var _ = Describe("AtlasCluster", func() {
 			// This is a bit strange but the delete request right after the cluster is removed may fail with "Still active cluster" error
 			// UI shows the cluster being deleted though. Seems to be the issue only if removal is done using API,
 			// if the cluster is terminated using UI - it stays in "Deleting" state
-			Eventually(removeAtlasProject(createdProject.Status.ID), 20, interval).Should(BeTrue())
+			Eventually(removeAtlasProject(createdProject.Status.ID), 600, interval).Should(BeTrue())
 		}
 		removeControllersAndNamespace()
 	})
