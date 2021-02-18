11c112e24287cafe659c78c74deca43a9b3a535f
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2021-02-18 21:16:20 +0300

    change project wait

--- scripts/e2e_local.sh
+++ scripts/e2e_local.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-act -j build-push
+set -e
 
 public_key=$(grep "ATLAS_PUBLIC_KEY" .actrc | cut -d "=" -f 2)
 private_key=$(grep "ATLAS_PRIVATE_KEY" .actrc | cut -d "=" -f 2)
 org_id=$(grep "ATLAS_ORG_ID" .actrc | cut -d "=" -f 2)
-image=$(grep "DOCKER_REPO" .actrc | cut -d "=" -f 2):$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+image=$(grep "DOCKER_REPO" .env | cut -d "=" -f 2):$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 
 export MCLI_OPS_MANAGER_URL="https://cloud-qa.mongodb.com/"
 export MCLI_PUBLIC_API_KEY="${public_key}"
@@ -14,5 +14,7 @@ export INPUT_IMAGE_URL="${image}"
 export INPUT_ENV=dev
 
 ./.github/actions/gen-install-scripts/entrypoint.sh
+
+act -j build-push
 
 ginkgo -v -x test/e2e
