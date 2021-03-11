64946edbf58c9f687376d8ce8edb0e1bc1383e47
Anton <anton.lisovenko@10gen.com>
2021-03-11 15:14:47 +0000

    Split integration tests to different jobs (#163)

--- .github/actions/int-test/entrypoint.sh
+++ .github/actions/int-test/entrypoint.sh
@@ -7,4 +7,4 @@ export ATLAS_PRIVATE_KEY="${INPUT_ATLAS_PRIVATE_KEY}"
 # otherwise we may get strange "Detected Programmatic Focus - setting exit status to 197"
 export GINKGO_EDITOR_INTEGRATION="true"
 
-ginkgo -v -p -nodes=4 ./test/int
+ginkgo --focus "${INPUT_TEST_NAME}" -v -nodes=4 ./test/int
