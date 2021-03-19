79c2fcb6a362a777c3dc6cb795b74d405baf6511
antonlisovenko <anton.lisovenko@mongodb.com>
2021-03-19 16:27:33 +0000

    unit test

--- pkg/controller/atlasdatabaseuser/databaseuser_test.go
+++ pkg/controller/atlasdatabaseuser/databaseuser_test.go
@@ -1,7 +1,10 @@
 package atlasdatabaseuser
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/atlas/mongodbatlas"
@@ -13,7 +16,14 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	mdbv1 "github.com/mongodb/mongodb-atlas-kubernetes/pkg/api/v1"
+	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/controller/connectionsecret"
+	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/util/kube"
 )
+
+func init() {
+	logger, _ := zap.NewDevelopment()
+	zap.ReplaceGlobals(logger)
+}
 
 func TestFilterScopeClusters(t *testing.T) {
 	scopeSpecs := []mdbv1.ScopeSpec{{
@@ -45,6 +55,54 @@ func TestCheckUserExpired(t *testing.T) {
 
 		result = checkUserExpired(zap.S(), fakeClient, "", *mdbv1.DefaultDBUser("ns", "theuser", "").WithDeleteAfterDate("2021/11/30T15:04:05"))
 		assert.False(t, result.IsOk())
-		assert.Equal(t, reconcile.Result{}, result.ReconcileResult())
 	})
+	t.Run("User Marked Expired", func(t *testing.T) {
+		data := dataForSecret()
+		// Create a connection secret
+		_, err := connectionsecret.Ensure(fakeClient, "testNs", "project1", "603e7bf38a94956835659ae5", "cluster1", data)
+		assert.NoError(t, err)
+		// The secret for the other project
+		_, err = connectionsecret.Ensure(fakeClient, "testNs", "project2", "dsfsdf234234sdfdsf23423", "cluster1", data)
+		assert.NoError(t, err)
+
+		before := time.Now().Add(time.Minute * -1).Format("2006-01-02T15:04:05")
+		result := checkUserExpired(zap.S(), fakeClient, "603e7bf38a94956835659ae5", *mdbv1.DefaultDBUser("testNs", data.DBUserName, "").WithDeleteAfterDate(before))
+		assert.False(t, result.IsOk())
+		assert.Equal(t, reconcile.Result{}, result.ReconcileResult())
+
+		// The secret has been removed
+		secret := corev1.Secret{}
+		secretName := fmt.Sprintf("%s-%s-%s", "project1", "cluster1", kube.NormalizeIdentifier(data.DBUserName))
+		err = fakeClient.Get(context.Background(), kube.ObjectKey("testNs", secretName), &secret)
+		assert.Error(t, err)
+
+		// The other secret still exists
+		secretName = fmt.Sprintf("%s-%s-%s", "project2", "cluster1", kube.NormalizeIdentifier(data.DBUserName))
+		err = fakeClient.Get(context.Background(), kube.ObjectKey("testNs", secretName), &secret)
+		assert.NoError(t, err)
+	})
+	t.Run("No expiration happened", func(t *testing.T) {
+		data := dataForSecret()
+		// Create a connection secret
+		_, err := connectionsecret.Ensure(fakeClient, "testNs", "project1", "603e7bf38a94956835659ae5", "cluster1", data)
+		assert.NoError(t, err)
+		after := time.Now().Add(time.Minute * 1).Format("2006-01-02T15:04:05")
+		result := checkUserExpired(zap.S(), fakeClient, "603e7bf38a94956835659ae5", *mdbv1.DefaultDBUser("testNs", data.DBUserName, "").WithDeleteAfterDate(after))
+		assert.True(t, result.IsOk())
+
+		// The secret is still there
+		secret := corev1.Secret{}
+		secretName := fmt.Sprintf("%s-%s-%s", "project1", "cluster1", kube.NormalizeIdentifier(data.DBUserName))
+		err = fakeClient.Get(context.Background(), kube.ObjectKey("testNs", secretName), &secret)
+		assert.NoError(t, err)
+	})
+}
+
+func dataForSecret() connectionsecret.ConnectionData {
+	return connectionsecret.ConnectionData{
+		DBUserName: "admin",
+		ConnURL:    "mongodb://mongodb0.example.com:27017,mongodb1.example.com:27017/?authSource=admin",
+		SrvConnURL: "mongodb+srv://mongodb.example.com:27017/?authSource=admin",
+		Password:   "m@gick%",
+	}
 }
