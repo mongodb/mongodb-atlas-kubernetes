5a012ed40c2ea8eb74508b2a60a32ea1d7aee399
Anton <anton.lisovenko@10gen.com>
2021-03-25 16:08:05 +0000

    CLOUDP-85517: no connection secrets in db user status (#184)

--- pkg/api/v1/status/atlasdatabaseuser.go
+++ pkg/api/v1/status/atlasdatabaseuser.go
@@ -5,12 +5,6 @@ package status
 // AtlasDatabaseUserStatusOption is the option that is applied to Atlas Project Status
 type AtlasDatabaseUserStatusOption func(s *AtlasDatabaseUserStatus)
 
-func AtlasDatabaseUserSecretsOption(clusters2Secrets map[string]string) AtlasDatabaseUserStatusOption {
-	return func(s *AtlasDatabaseUserStatus) {
-		s.ConnectionSecrets = clusters2Secrets
-	}
-}
-
 func AtlasDatabaseUserPasswordVersion(passwordVersion string) AtlasDatabaseUserStatusOption {
 	return func(s *AtlasDatabaseUserStatus) {
 		s.PasswordVersion = passwordVersion
@@ -25,10 +19,6 @@ func AtlasDatabaseUserNameOption(name string) AtlasDatabaseUserStatusOption {
 // AtlasDatabaseUserStatus defines the observed state of AtlasProject
 type AtlasDatabaseUserStatus struct {
 	Common `json:",inline"`
-
-	// ConnectionSecrets defines all the Secrets for the current database user that were created by the Atlas Operator
-	// and which provide the Atlas clusters connectivity information
-	ConnectionSecrets map[string]string `json:"connectionSecrets,omitempty"`
 
 	// PasswordVersion is the 'ResourceVersion' of the password Secret that the Atlas Operator is aware of
 	PasswordVersion string `json:"passwordVersion,omitempty"`
