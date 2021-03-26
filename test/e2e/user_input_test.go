93c4b723553603d91925515c40b8bf00773ed940
Svetlana <5437289+leo-ri@users.noreply.github.com>
2021-03-26 12:18:48 +0300

    CLOUDP-82260. Application (#181)

--- test/e2e/user_input_test.go
+++ test/e2e/user_input_test.go
@@ -1,90 +1,56 @@
 package e2e_test
 
 import (
+	"os"
+	"path/filepath"
+
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
 	"go.mongodb.org/atlas/mongodbatlas"
 
 	kube "github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/cli/kube"
 	mongocli "github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/cli/mongocli"
+	"github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/model"
 	"github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/utils"
 )
 
-var (
-	ConfigAll         = "../../deploy/" // Released generated files
-	ClusterSample     = "data/atlascluster_basic.yaml"
-	DataFolder        = "data/"
-	defaultOperatorNS = "mongodb-atlas-system"
-)
-
-type userInputs struct {
-	projectName        string
-	projectID          string
-	keyName            string
-	namespace          string
-	k8sProjectName     string
-	k8sFullProjectName string
-	projectPath        string
-	clusters           []utils.AC
-	users              []utils.DBUser
-}
-
-// NewUsersInputs prepare users inputs
-func NewUserInputs(keyName string, users []utils.DBUser) userInputs {
-	uid := utils.GenUniqID()
-
-	input := userInputs{
-		projectName:        uid,
-		projectID:          "",
-		keyName:            keyName,
-		namespace:          "ns-" + uid,
-		k8sProjectName:     "k-" + uid,
-		k8sFullProjectName: "atlasproject.atlas.mongodb.com/k-" + uid,
-		projectPath:        DataFolder + uid + ".yaml",
-	}
-	for _, user := range users {
-		input.users = append(input.users, *user.WithProjectRef(input.k8sProjectName))
-	}
-	return input
-}
-
-func FilePathTo(name string) string {
-	return DataFolder + name + ".yaml"
-}
-
-func waitCluster(input userInputs, generation string) {
-	Eventually(kube.GetGeneration(input.namespace, input.clusters[0].GetClusterNameResource())).Should(Equal(generation))
+func waitCluster(input model.UserInputs, generation string) {
+	Eventually(kube.GetGeneration(input.Namespace, input.Clusters[0].GetClusterNameResource())).Should(Equal(generation))
 	Eventually(
-		kube.GetStatusCondition(input.namespace, input.clusters[0].GetClusterNameResource()),
+		kube.GetStatusCondition(input.Namespace, input.Clusters[0].GetClusterNameResource()),
 		"45m", "1m",
 	).Should(Equal("True"), "Kubernetes resource: Cluster status `Ready` should be True")
 
 	Eventually(kube.GetK8sClusterStateName(
-		input.namespace, input.clusters[0].GetClusterNameResource()),
+		input.Namespace, input.Clusters[0].GetClusterNameResource()),
 		"45m", "1m",
 	).Should(Equal("IDLE"), "Kubernetes resource: Cluster status should be IDLE")
 
 	Expect(
-		mongocli.GetClusterStateName(input.projectID, input.clusters[0].Spec.Name),
+		mongocli.GetClusterStateName(input.ProjectID, input.Clusters[0].Spec.Name),
 	).Should(Equal("IDLE"), "Atlas: Cluster status should be IDLE")
 }
 
-func waitProject(input userInputs, generation string) {
-	EventuallyWithOffset(1, kube.GetStatusCondition(input.namespace, input.k8sFullProjectName)).Should(Equal("True"), "Kubernetes resource: Project status `Ready` should be True")
-	EventuallyWithOffset(1, kube.GetGeneration(input.namespace, input.k8sFullProjectName)).Should(Equal(generation), "Kubernetes resource: Generation should be upgraded")
-	EventuallyWithOffset(1, kube.GetProjectResource(input.namespace, input.k8sFullProjectName).Status.ID).ShouldNot(BeNil(), "Kubernetes resource: Status has field with ProjectID")
+func waitProject(input model.UserInputs, generation string) {
+	EventuallyWithOffset(1, kube.GetStatusCondition(input.Namespace, input.K8sFullProjectName)).Should(Equal("True"), "Kubernetes resource: Project status `Ready` should be True")
+	EventuallyWithOffset(1, kube.GetGeneration(input.Namespace, input.K8sFullProjectName)).Should(Equal(generation), "Kubernetes resource: Generation should be upgraded")
+	EventuallyWithOffset(1, kube.GetProjectResource(input.Namespace, input.K8sFullProjectName).Status.ID).ShouldNot(BeNil(), "Kubernetes resource: Status has field with ProjectID")
 }
 
-func checkIfClusterExist(input userInputs) func() bool {
+func waitTestApplication(ns, label string) {
+	EventuallyWithOffset(1, kube.GetStatusPhase(ns, "pods", "-l", label)).Should(Equal("Running"), "Test application should be running")
+}
+
+func checkIfClusterExist(input model.UserInputs) func() bool {
 	return func() bool {
-		return mongocli.IsClusterExist(input.projectID, input.clusters[0].Spec.Name)
+		return mongocli.IsClusterExist(input.ProjectID, input.Clusters[0].Spec.Name)
 	}
 }
 
-func checkIfUsersExist(input userInputs) func() bool {
+func checkIfUsersExist(input model.UserInputs) func() bool {
 	return func() bool {
-		for _, user := range input.users {
-			if !mongocli.IsUserExist(user.Spec.Username, input.projectID) {
+		for _, user := range input.Users {
+			if !mongocli.IsUserExist(user.Spec.Username, input.ProjectID) {
 				return false
 			}
 		}
@@ -98,7 +64,7 @@ func checkIfUserExist(username, projecID string) func() bool {
 	}
 }
 
-func compareClustersSpec(requested utils.ClusterSpec, created mongodbatlas.Cluster) { // TODO
+func compareClustersSpec(requested model.ClusterSpec, created mongodbatlas.Cluster) { // TODO
 	ExpectWithOffset(1, created).To(MatchFields(IgnoreExtras, Fields{
 		"MongoURI":            Not(BeEmpty()),
 		"MongoURIWithOptions": Not(BeEmpty()),
@@ -122,13 +88,13 @@ func SaveK8sResources(resources []string, ns string) {
 	}
 }
 
-func checkUsersAttributes(input userInputs) {
-	for _, user := range input.users {
-		atlasUser := mongocli.GetUser(user.Spec.Username, input.projectID)
+func checkUsersAttributes(input model.UserInputs) {
+	for _, user := range input.Users {
+		atlasUser := mongocli.GetUser(user.Spec.Username, input.ProjectID)
 		// Required fields
 		ExpectWithOffset(1, atlasUser).To(MatchFields(IgnoreExtras, Fields{
 			"Username":     Equal(user.Spec.Username),
-			"GroupID":      Equal(input.projectID),
+			"GroupID":      Equal(input.ProjectID),
 			"DatabaseName": Or(Equal(user.Spec.DatabaseName), Equal("admin")),
 		}), "Users attributes should be the same as requested by the user")
 
@@ -140,4 +106,21 @@ func checkUsersAttributes(input userInputs) {
 			}), "Users roles attributes should be the same as requsted by the user")
 		}
 	}
+}
+
+// CopyKustomizeNamespaceOperator create copy of `/deploy/namespaced` folder with kustomization file for overriding namespace
+func CopyKustomizeNamespaceOperator(input model.UserInputs) {
+	// fullPath := filepath.Join("data", input.projectName, "operator")
+	fullPath := input.GetOperatorFolder()
+	// fullPath := filepath.Dir(projectPath)
+	os.Mkdir(fullPath, os.ModePerm)
+	utils.CopyFile("../../deploy/namespaced/crds.yaml", filepath.Join(fullPath, "crds.yaml"))
+	utils.CopyFile("../../deploy/namespaced/namespaced-config.yaml", filepath.Join(fullPath, "namespaced-config.yaml"))
+	data := []byte(
+		"namespace: " + input.Namespace + "\n" +
+			"resources:" + "\n" +
+			"- crds.yaml" + "\n" +
+			"- namespaced-config.yaml",
+	)
+	utils.SaveToFile(filepath.Join(fullPath, "kustomization.yaml"), data)
 }
