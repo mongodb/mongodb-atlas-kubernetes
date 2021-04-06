ea9a16d85d81f26c979e58dc8b50f34bdfc50eea
antonlisovenko <anton.lisovenko@mongodb.com>
2021-04-06 15:18:11 +0100

    wip

--- scripts/release-redhat-upstream.sh
+++ scripts/release-redhat-upstream.sh
@@ -6,7 +6,7 @@ version=${1:?"pass the version as the parameter, e.g \"0.5.0\""}
 
 cd "${RH_COMMUNITY_REPO_PATH}"
 cp -r community-operators/mongodb-atlas-kubernetes/"${version}" upstream-community-operators/mongodb-atlas-kubernetes
-git checkout -b "mongodb-atlas-operator-community-${version}"
+git checkout -b "mongodb-atlas-operator-upstream-${version}"
 git add upstream-community-operators/mongodb-atlas-kubernetes/"${version}"
 git commit -m "[community] MongoDB Atlas Operator ${version}" --signoff upstream-community-operators/mongodb-atlas-kubernetes/"${version}"
-git push origin mongodb-atlas-operator-community-"${version}"
+git push origin mongodb-atlas-operator-upstream-"${version}"
