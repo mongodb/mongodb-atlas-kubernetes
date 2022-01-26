2153b5fc9eef4c8b802cf29783be73397bcca95e
Cian Hatton <cianhatton@gmail.com>
2022-01-26 14:09:09 +0000

    Add user code path to post install hook

--- cmd/post-install/main.go
+++ cmd/post-install/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -35,6 +36,14 @@ func setupLogger() *zap.SugaredLogger {
 	return log.Sugar()
 }
 
+func validateHookType(hookType string) error {
+	hook := HookType(hookType)
+	if hook != ClusterType && hook != UserType {
+		return fmt.Errorf("hook type must be one of %s or %s", ClusterType, UserType)
+	}
+	return nil
+}
+
 // createK8sClient creates an in cluster client which can be used to fetch the current state of the AtlasCluster
 // resource.
 func createK8sClient() (client.Client, error) {
@@ -49,8 +58,20 @@ func createK8sClient() (client.Client, error) {
 		return nil, err
 	}
 
-	k8sClient.Scheme().AddKnownTypes(schema.GroupVersion{Group: "atlas.mongodb.com", Version: "v1"}, &mdbv1.AtlasCluster{}, &mdbv1.AtlasClusterList{})
+	k8sClient.Scheme().AddKnownTypes(schema.GroupVersion{Group: "atlas.mongodb.com", Version: "v1"}, &mdbv1.AtlasCluster{}, &mdbv1.AtlasClusterList{}, &mdbv1.AtlasDatabaseUser{}, &mdbv1.AtlasDatabaseUserList{})
 	return k8sClient, nil
+}
+
+type HookType string
+
+const (
+	ClusterType = "CLUSTER"
+	UserType    = "USER"
+)
+
+func getHookType() HookType {
+	hookType := os.Getenv("HOOK_TYPE")
+	return HookType(hookType)
 }
 
 // getNamespace returns the current namespace.
@@ -65,14 +86,45 @@ func getNamespace() (string, error) {
 	return defaultNamespace, nil
 }
 
-// isClusterReady returns a boolean indicating if the cluster has reached the ready state and is
-// ready to be used.
-func isClusterReady(logger *zap.SugaredLogger) (bool, error) {
-	k8sClient, err := createK8sClient()
+func isUserReady(k8sClient client.Client, logger *zap.SugaredLogger) (bool, error) {
+	ticker := time.NewTicker(pollingInterval)
+	defer ticker.Stop()
+
+	databaseUserName := os.Getenv("USER_NAME")
+
+	namespace, err := getNamespace()
 	if err != nil {
 		return false, err
 	}
 
+	totalTime := time.Duration(0)
+	for range ticker.C {
+		if totalTime > pollingDuration {
+			break
+		}
+		totalTime += pollingInterval
+
+		atlasDatabaseUser := mdbv1.AtlasDatabaseUser{}
+		if err := k8sClient.Get(context.TODO(), kube.ObjectKey(namespace, databaseUserName), &atlasDatabaseUser); err != nil {
+			return false, err
+		}
+
+		// the atlas user has reached the ready state.
+		for _, cond := range atlasDatabaseUser.Status.Conditions {
+			if cond.Type == status.DatabaseUserReadyType {
+				if cond.Status == corev1.ConditionTrue {
+					return true, nil
+				}
+				logger.Infof("Atlas Database User %s is not yet ready", atlasDatabaseUser.Name)
+			}
+		}
+	}
+	return false, nil
+}
+
+// isClusterReady returns a boolean indicating if the cluster has reached the ready state and is
+// ready to be used.
+func isClusterReady(k8sClient client.Client, logger *zap.SugaredLogger) (bool, error) {
 	ticker := time.NewTicker(pollingInterval)
 	defer ticker.Stop()
 
@@ -110,15 +162,30 @@ func isClusterReady(logger *zap.SugaredLogger) (bool, error) {
 
 func main() {
 	logger := setupLogger()
+	if err := validateHookType(os.Getenv("HOOK_TYPE")); err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
 
-	clusterIsReady, err := isClusterReady(logger)
+	handler := isClusterReady
+	if getHookType() == UserType {
+		handler = isUserReady
+	}
+
+	k8sClient, err := createK8sClient()
+	if err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
+
+	isReady, err := handler(k8sClient, logger)
 	if err != nil {
 		logger.Error(err)
 		os.Exit(1)
 	}
 
 	exitCode := 1
-	if clusterIsReady {
+	if !isReady {
 		exitCode = 0
 	}
 	os.Exit(exitCode)
