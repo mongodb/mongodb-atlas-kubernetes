c61851eaed0cadafc32a7dab89a61d82c87eb7ab
Igor Karpukhin <karpukhinigor@gmail.com>
2022-01-26 09:30:59 +0100

    Removed unused script

--- scripts/deploy_to_openshift.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eou pipefail
-
-: ${TARGET_NAMESPACE:="mongodb-altas-system"}
-
-if [[ -z $(oc get project ${TARGET_NAMESPACE} 2> /dev/null) ]]; then
-	oc create project ${TARGET_NAMESPACE}
-fi
