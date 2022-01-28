e362bb5fde64c8def5570daad8a323b3ece8d0a1
Igor Karpukhin <karpukhinigor@gmail.com>
2022-01-28 16:35:05 +0100

    Added openshift-namespaced env

--- .github/actions/gen-install-scripts/entrypoint.sh
+++ .github/actions/gen-install-scripts/entrypoint.sh
@@ -10,6 +10,7 @@ crds_dir="${target_dir}/crds"
 mkdir -p "${clusterwide_dir}"
 mkdir -p "${namespaced_dir}"
 mkdir -p "${crds_dir}"
+mkdir -p "${openshift_namespaced}"
 
 # Generate configuration and save it to `all-in-one`
 controller-gen crd:crdVersions=v1 rbac:roleName=manager-role webhook paths="./..." output:crd:artifacts:config=config/crd/bases
@@ -28,6 +29,11 @@ echo "Created all-in-one config"
 kustomize build --load-restrictor LoadRestrictionsNone "config/release/${INPUT_ENV}/clusterwide" > "${clusterwide_dir}/clusterwide-config.yaml"
 kustomize build "config/crd" > "${clusterwide_dir}/crds.yaml"
 echo "Created clusterwide config"
+
+# base-openshift-namespace-scoped
+kustomize build --load-restrictor LoadRestrictionsNone "config/release/${INPUT_ENV}/namespaced" > "${openshift_namespaced}/clusterwide-config.yaml"
+kustomize build "config/crd" > "${openshift_namespaced}/crds.yaml"
+echo "Created openshift namespaced config"
 
 # namespaced
 kustomize build --load-restrictor LoadRestrictionsNone "config/release/${INPUT_ENV}/namespaced" > "${namespaced_dir}/namespaced-config.yaml"
