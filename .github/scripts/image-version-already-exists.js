82861fc01bb373c1284f6bc2394e2bd5b1a601ce
Cian Hatton <cianhatton@gmail.com>
2022-01-31 10:25:21 +0000

    updated js script

--- .github/scripts/image-version-already-exists.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-module.exports = async () => {
-    const {image, version} = process.env;
-    const resp = await axios.get(`https://quay.io/api/v1/repository/mongodb/${image}`);
-    return resp.data.tags[version] !== undefined;
-};
