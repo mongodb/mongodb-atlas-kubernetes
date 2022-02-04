ab5a6995d9a010e88db1a824072b85613404d79d
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2022-02-04 22:47:55 +0300

    update cleaner

--- .github/actions/cleanup/entrypoint.sh
+++ .github/actions/cleanup/entrypoint.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-# For Deleting empty(!) PROJECTs which live more then 9 hours
+# For Deleting empty(!) PROJECTs which live more then (9) hours
 # It deletes all if INPUT_CLEAN_ALL is true
 
-set -eou pipefail
 MAX_PROJECT_LIFETIME=9 # in hours
+mongocli config set skip_update_check true
 
 delete_endpoints_for_project() {
     projectID=$1
     provider=$2
-    endpoints=$(mongocli atlas privateEndpoints "$provider" list --projectId "$projectID" | awk 'NR!=1{print $1}')
+
+    endpoints=$(mongocli atlas privateEndpoints "$provider" list --projectId "$projectID" -o json | jq -c . )
+    [[ "$provider" == "aws" ]] && field=".interfaceEndpoints" || field=".privateEndpoints"
+
     # shellcheck disable=SC2068
     # multiline
-    for endpoint in ${endpoints[@]}; do
-        echo "deleting endpoint $endpoint in $projectID"
-        mongocli atlas privateEndpoints "$provider" delete "$endpoint" --projectId "$projectID" --force
+    for endpoint in $(echo "$endpoints" | jq -cr '.[]'); do
+        # echo $endpoint
+        service_id=$(echo "$endpoint" | jq -r '.id' )
+        points=$(echo "$endpoint" | jq -r "$field")
+
+        if [[ $points != "null" ]]; then
+            for pe in $(echo "$points" | jq -r '.[]'); do
+                echo "----Delete private endpoint: $pe from $projectID will be deleted"
+                pe=$(echo "$pe" | sed 's/\//%2F/g')
+                mongocli atlas privateEndpoints "$provider" interfaces delete "$pe" --endpointServiceId "$service_id" --projectId "$projectID" --force
+                mongocli atlas privateEndpoints "$provider" interfaces describe "$pe" --endpointServiceId "$service_id" --projectId "$projectID" | awk 'NR!=1{print $1}'
+                until [[ "$(mongocli atlas privateEndpoints "$provider" interfaces describe "$pe" --endpointServiceId "$service_id" --projectId "$projectID" | awk 'NR!=1{print $1}')" == "" ]]; do
+                    echo "wait..."
+                    sleep 1
+                done
+            done
+        fi
+
+        echo "--Private endpoint service will be deleted: $service_id in $projectID"
+        # we do not wait PE service deletion - long operation
+        mongocli atlas privateEndpoints "$provider" delete "$service_id" --projectId "$projectID" --force
     done
 }
 
@@ -32,27 +53,29 @@ delete_clusters() {
 # delete only old projects (older than 9 hours)
 delete_old_project() {
     if [[ -z "${count:-}" ]] || [[ ${count:-} == "null"  ]] && [[ "$existance_hours" -gt $MAX_PROJECT_LIFETIME ]]; then
-        echo "deleting-$id"
+        echo "====== Cleaning Project: $id"
         delete_endpoints_for_project "$id" "aws"
         delete_endpoints_for_project "$id" "azure"
-        mongocli iam projects delete "$id" --force
+        isReady=$(mongocli atlas privateEndpoints aws list --projectId "$projectID" | awk 'NR!=1{print $1}')$(mongocli atlas privateEndpoints azure list --projectId "$projectID" | awk 'NR!=1{print $1}')
+        [[ $isReady == "" ]] && mongocli iam projects delete "$id" --force
     fi
 }
 
 # delete private endpoints, terminate clusters, delete empty project
 delete_all() {
+    echo "====== Cleaning Project: $id"
     delete_endpoints_for_project "$id" "aws"
     delete_endpoints_for_project "$id" "azure"
     if [[ -z ${count:-} ]] || [[ ${count:-} == "null" ]]; then
-        echo "deleting-$id"
-        mongocli iam projects delete "$id" --force
+        isReady=$(mongocli atlas privateEndpoints aws list --projectId "$projectID" | awk 'NR!=1{print $1}')$(mongocli atlas privateEndpoints azure list --projectId "$projectID" | awk 'NR!=1{print $1}')
+        [[ $isReady == "" ]] && mongocli iam projects delete "$id" --force
     else
         echo "delete only cluster (will not wait)"
         delete_clusters "$id"
     fi
 }
 
-projects=$(mongocli iam projects list -o json)
+projects=$(mongocli iam projects list -o json | jq -c .)
 if [[ $projects == *"error"* ]]; then
     echo "Error: $projects"
     exit 1
