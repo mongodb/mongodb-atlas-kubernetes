b99be02010025679eb0e68c6e489ea55d1be93c0
fabritsius <anton_fabritsius@epam.com>
2022-03-05 18:32:34 +0300

    Update the release script for RedHat

--- scripts/release-redhat.sh
+++ scripts/release-redhat.sh
@@ -4,18 +4,17 @@ set -eou pipefail
 
 version=${1:?"pass the version as the parameter, e.g \"0.5.0\""}
 repo="${RH_COMMUNITY_OPERATORHUB_REPO_PATH}/operators/mongodb-atlas-kubernetes"
-cd "${repo}"
+mkdir "${repo}/${version}"
+cp -r bundle.Dockerfile bundle/manifests bundle/metadata bundle/tests "${repo}/${version}"
 
+cd "${repo}"
 git fetch upstream main
 git reset --hard upstream/main
 
-mkdir "${version}"
-cp -r bundle.Dockerfile bundle/manifests bundle/metadata bundle/tests "${repo}/${version}"
-
 # replace the move instructions in the docker file
-sed -i .bak 's/COPY bundle\/manifests/COPY manifests/' "${version}/bundle.Dockerfile"
-sed -i .bak 's/COPY bundle\/metadata/COPY metadata/' "${version}/bundle.Dockerfile"
-sed -i .bak 's/COPY bundle\/tests\/scorecard/COPY tests\/scorecard/' "${version}/bundle.Dockerfile"
+sed -i.bak 's/COPY bundle\/manifests/COPY manifests/' "${version}/bundle.Dockerfile"
+sed -i.bak 's/COPY bundle\/metadata/COPY metadata/' "${version}/bundle.Dockerfile"
+sed -i.bak 's/COPY bundle\/tests\/scorecard/COPY tests\/scorecard/' "${version}/bundle.Dockerfile"
 rm "${version}/bundle.Dockerfile.bak"
 
 # commit
