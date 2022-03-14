7601121343b70aa04e3f7922c8c2df9a209e3545
Igor Karpukhin <karpukhinigor@gmail.com>
2022-03-14 18:49:17 +0100

    Truncate branch name

--- .github/actions/set-tag/entrypoint.sh
+++ .github/actions/set-tag/entrypoint.sh
@@ -8,6 +8,6 @@ branch_name=${GITHUB_HEAD_REF-}
 if [ -z "${branch_name}" ]; then
     branch_name=$(echo "$GITHUB_REF" | awk -F'/' '{print $3}')
 fi
-branch_name=$(echo "${branch_name}" | sed  's/\//-/g')
+branch_name=$(echo "${branch_name}" | awk '{print substr($0, 1, 15)}' | sed 's/\//-/g')
 tag="${branch_name}-${commit_id}"
 echo "::set-output name=tag::$tag"
