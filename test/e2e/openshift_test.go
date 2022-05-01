6aadafcd3feded713498236cd6d113c019427c15
Igor Karpukhin <karpukhinigor@gmail.com>
2022-05-01 21:52:56 +0200

    Increased timeout for the Openshift test

--- test/e2e/openshift_test.go
+++ test/e2e/openshift_test.go
@@ -54,9 +54,9 @@ var _ = Describe("Openshift UI test", Label("openshift"), func() {
 		if CurrentSpecReport().Failed() && !strings.Contains(page.URL(), "token") {
 			pagereport.MakeScreenshot(page, "error")
 		}
-		oc.Delete(path) // we delete it all the time, because of shared space
-		closeBrowser(pw, browser, page)
+		oc.Delete(path)
 		kubecli.DeleteResource("configmap", lockNamespace, lockNamespace) // clean lockConfig Map
+		closeBrowser(pw, browser, page)
 	})
 
 	It("User can deploy Atlas Kubernetes operator from openshift", func() {
