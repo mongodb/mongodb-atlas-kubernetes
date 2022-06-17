69e7001b5e0b13b43ff0c1aa3f5804125bd8f829
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2022-06-17 18:43:08 +0300

    clean serverless

--- .github/actions/cleanup/entrypoint.sh
+++ .github/actions/cleanup/entrypoint.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
+# shellcheck shell=bash disable=SC1091
 
 # For Deleting empty(!) PROJECTs which live more then (MAX_PROJECT_LIFETIME_INPUT) hours
 # It deletes all if INPUT_CLEAN_ALL is true
 
 mongocli config set skip_update_check true
+set -e
+
+# ------------------------------------------------------------------------------
+# delete global API key by API request (mongocli does not support it yet)
+BASE_URL="https://cloud-qa.mongodb.com/api/atlas/v1.0"
+
+get_api_keys() {
+    curl -s -u "${MCLI_PUBLIC_API_KEY}:${MCLI_PRIVATE_API_KEY}" --digest "${BASE_URL}/orgs/${MCLI_ORG_ID}/apiKeys"
+}
+
+delete_test_apikeys() {
+    API_KEY_ID=$1
+    curl -s -u "${MCLI_PUBLIC_API_KEY}:${MCLI_PRIVATE_API_KEY}" --digest --request DELETE "${BASE_URL}/orgs/${MCLI_ORG_ID}/apiKeys/${API_KEY_ID}"
+}
+
+get_all_serverless() {
+    curl -s -u "${MCLI_PUBLIC_API_KEY}:${MCLI_PRIVATE_API_KEY}" --digest "${BASE_URL}/groups/${projectID}/serverless"
+}
+
+delete_serverless_request() {
+    instance=$1
+    curl -s -u "${MCLI_PUBLIC_API_KEY}:${MCLI_PRIVATE_API_KEY}" --digest --request DELETE "${BASE_URL}/groups/${projectID}/serverless/${instance}"
+}
+
+get_serverless_instance_request() {
+    instance=$1
+    curl -s -u "${MCLI_PUBLIC_API_KEY}:${MCLI_PRIVATE_API_KEY}" --digest "${BASE_URL}/groups/${projectID}/serverless/${instance}"
+}
+
+# ------------------------------------------------------------------------------
 
 command_result_waiter() {
     command=$1
@@ -75,6 +106,35 @@ delete_clusters() {
     done
 }
 
+delete_serverless() {
+    projectID=$1
+    echo "====== Cleaning Serverless Instances in Project: $projectID"
+    all=$(get_all_serverless)
+    echo "all instances: $all"
+    serverless=$(echo "$all" | jq -r '.results | .[] | .name')
+    echo "list name: ${serverless[*]}"
+    total_serverless=$(echo "$all" | jq -r '.totalCount // 0')
+    echo "total serverless instances: $total_serverless"
+    if [[ $total_serverless -gt 0 ]]; then
+        # shellcheck disable=SC2068
+        # multiline
+        for instance in ${serverless[@]}; do
+            echo "== Cleaning Serverless instance: $instance"
+            instance_atlas=$(get_serverless_instance_request "$instance")
+            echo "inside: $instance_atlas"
+
+            state=$(get_serverless_instance_request "$instance" | jq -r '.stateName')
+            echo "Current instance: $instance. State: $state"
+            if [[ -n $state ]] && [[ $state != "DELETING" ]]; then
+                echo "Deleting instance $instance in $projectID"
+                delete_serverless_request "$instance"
+                command="get_serverless_instance_request $instance"
+                command_result_waiter "$command" "providerSettings" 10 20 # if no providerSetting - no serverless instance
+            fi
+        done
+    fi
+}
+
 delete_project() {
     peDeleted=$(mongocli atlas privateEndpoints aws list --projectId "$projectID" | awk 'NR!=1{print $1}')$(mongocli atlas privateEndpoints azure list --projectId "$projectID" | awk 'NR!=1{print $1}')
     [[ $peDeleted == "" ]] && mongocli iam projects delete "$id" --force
@@ -85,7 +145,7 @@ delete_old_project() {
     output=$(
         exist=is_project_exist
         if [[ -n ${exist:-} ]] || [[ -z "${count:-}" ]] || [[ ${count:-} == "null"  ]] && [[ "$existance_hours" -gt $MAX_PROJECT_LIFETIME_INPUT ]]; then
-            echo "====== Cleaning Project: $id"
+            echo "======================= Cleaning Project: $id"
             delete_endpoints_for_project "$id" "aws"
             delete_endpoints_for_project "$id" "azure"
             delete_endpoints_for_project "$id" "gcp"
@@ -105,31 +165,17 @@ delete_all() {
     output=$(
         exist=is_project_exist
         if [[ -n ${exist:-} ]]; then
-            echo "====== Cleaning Project: $id"
+            echo "======================= Cleaning Project: $id"
             delete_endpoints_for_project "$id" "aws"
             delete_endpoints_for_project "$id" "azure"
             delete_endpoints_for_project "$id" "gcp"
+            delete_serverless "$id"
             delete_clusters "$id"
             delete_project
         fi
     )
     echo "${output[@]}"
 }
-
-# ------------------------------------------------------------------------------
-# delete global API key by API request (mongocli does not support it yet)
-BASE_URL="https://cloud-qa.mongodb.com/api/atlas/v1.0"
-
-get_api_keys() {
-    curl -s -u "${MCLI_PUBLIC_API_KEY}:${MCLI_PRIVATE_API_KEY}" --digest "${BASE_URL}/orgs/${MCLI_ORG_ID}/apiKeys"
-}
-
-delete_test_apikeys() {
-    API_KEY_ID=$1
-    curl -s -u "${MCLI_PUBLIC_API_KEY}:${MCLI_PRIVATE_API_KEY}" --digest --request DELETE "${BASE_URL}/orgs/${MCLI_ORG_ID}/apiKeys/${API_KEY_ID}"
-}
-
-# ------------------------------------------------------------------------------
 
 echo "The process could take a while. Please, wait..."
 
@@ -175,5 +221,6 @@ if [[ "${INPUT_CLEAN_ALL:-}" == "true" ]]; then
         fi
     done
 fi
+# ------------------------------------------------------------------------------
 
 echo "Job Done"
