f7b93dd95cedf6e1a52c8fbc0240743214a9e92b
Julien-Ben <julien.benhaim@mongodb.com>
2022-06-24 11:43:12 +0100

    Input validation

--- pkg/api/v1/project/maintenancewindow.go
+++ pkg/api/v1/project/maintenancewindow.go
@@ -10,10 +10,14 @@ type MaintenanceWindow struct {
 	// Day of the week when you would like the maintenance window to start as a 1-based integer.
 	// Sunday 1, Monday 2, Tuesday 3, Wednesday 4, Thursday 5, Friday 6, Saturday 7
 	// +optional
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=7
 	DayOfWeek int `json:"dayOfWeek,omitempty"`
 	// Hour of the day when you would like the maintenance window to start.
 	// This parameter uses the 24-hour clock, where midnight is 0, noon is 12.
 	// +optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=23
 	HourOfDay int `json:"hourOfDay,omitempty"`
 	// Flag indicating whether any scheduled project maintenance should be deferred automatically for one week.
 	// +optional
