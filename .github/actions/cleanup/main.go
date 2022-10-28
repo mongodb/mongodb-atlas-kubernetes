067d8afc076f86aa208f67ad3e54f170ec2f04e2
Timur Akhmadiev <42980811+Sugar-pack@users.noreply.github.com>
2022-10-28 17:53:04 +0400

    CLOUDP-141796: Remake cleaner (#743)

---
+++ .github/actions/cleanup/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"actions/cleanup/atlasclient"
+	"actions/cleanup/project"
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/atlas/mongodbatlas"
+)
+
+func main() {
+	config, err := NewConfig()
+	if err != nil {
+		log.Fatalf("error getting config: %s", err)
+	}
+
+	client, err := atlasclient.SetupClient(config.PublicKey, config.PrivateKey, config.ManagerUrl)
+	if err != nil {
+		log.Fatalf("error creating atlas client: %s", err)
+	}
+
+	projects, _, err := client.Projects.GetAllProjects(context.Background(), &mongodbatlas.ListOptions{
+		ItemsPerPage: 250,
+	})
+	if err != nil {
+		log.Fatalf("error getting projects: %s", err)
+	}
+	projectList, err := project.PrepareListToDelete(projects, config.DeleteAll, config.Lifetime)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	ok := project.DeleteProjects(ctx, client, projectList)
+	if !ok {
+		log.Printf("Not all project deleted. Please run cleaner again later")
+		os.Exit(1)
+	}
+}
---
+++ .github/actions/cleanup/project/project.go
@@ -0,0 +1,60 @@
+package project
+
+import (
+	"actions/cleanup/deployment"
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/atlas/mongodbatlas"
+)
+
+func PrepareListToDelete(projects *mongodbatlas.Projects, deleteAll bool, lifetimeInHours int) ([]*mongodbatlas.Project, error) {
+	var projectList []*mongodbatlas.Project
+	if deleteAll {
+		projectList = projects.Results
+	} else {
+		for _, project := range projects.Results {
+			createdTime, err := time.Parse(time.RFC3339, project.Created) // check format
+			if err != nil {
+				return nil, fmt.Errorf("error parsing project creation time: %w", err)
+			}
+			if time.Since(createdTime) < time.Duration(lifetimeInHours)*time.Hour {
+				projectList = append(projectList, project)
+			}
+		}
+	}
+	return projectList, nil
+}
+
+func DeleteProjects(ctx context.Context, client mongodbatlas.Client, projectList []*mongodbatlas.Project) bool {
+	ok := true
+	for _, project := range projectList {
+		log.Printf("Deleting project with name %s and id %s", project.Name, project.ID)
+		err := deleteAllPE(ctx, client.PrivateEndpoints, project.ID)
+		if err != nil {
+			log.Printf("error deleting private endpoints: %s", err)
+		}
+		err = DeleteAllNetworkPeers(ctx, client.Peers, project.ID)
+		if err != nil {
+			log.Printf("error deleting network peers: %s", err)
+		}
+		err = deployment.DeleteAllDeployments(ctx, client.Clusters, project.ID)
+		if err != nil {
+			log.Printf("error deleting deployments: %s", err)
+		}
+		err = deployment.DeleteAllServerless(ctx, client.ServerlessInstances, project.ID)
+		if err != nil {
+			log.Printf("error deleting serverless: %s", err)
+		}
+		_, err = client.Projects.Delete(context.Background(), project.ID)
+		if err != nil {
+			ok = false
+			log.Printf("error deleting project: %s", err)
+		} else {
+			log.Printf("Succesfully delete")
+		}
+	}
+	return ok
+}
