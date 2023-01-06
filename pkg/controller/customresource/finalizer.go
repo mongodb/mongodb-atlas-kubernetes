462d40e0a893195455b4a23e3387f14362513a66
Helder Santana <helder.santana@mongodb.com>
2023-01-06 16:26:09 -0300

    fix lint

--- pkg/controller/customresource/finalizer.go
+++ pkg/controller/customresource/finalizer.go
@@ -1,12 +1,7 @@
 package customresource
 
 import (
-	"context"
-	"fmt"
-
 	mdbv1 "github.com/mongodb/mongodb-atlas-kubernetes/pkg/api/v1"
-	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/util/kube"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func HaveFinalizer(resource mdbv1.AtlasCustomResource, finalizer string) bool {
@@ -37,32 +32,4 @@ func UnsetFinalizer(resource mdbv1.AtlasCustomResource, finalizer string) {
 	}
 
 	resource.SetFinalizers(finalizers)
-}
-
-func AddFinalizer(ctx context.Context, client client.Client, resource mdbv1.AtlasCustomResource, finalizer string) error {
-	err := client.Get(ctx, kube.ObjectKeyFromObject(resource), resource)
-	if err != nil {
-		return fmt.Errorf("cannot get resource to add finalizer: %w", err)
-	}
-
-	SetFinalizer(resource, finalizer)
-
-	if err = client.Update(ctx, resource); err != nil {
-		return fmt.Errorf("failed to add deletion finalizer from %s: %w", resource, err)
-	}
-	return nil
-}
-
-func DeleteFinalizer(ctx context.Context, client client.Client, resource mdbv1.AtlasCustomResource, finalizer string) error {
-	err := client.Get(ctx, kube.ObjectKeyFromObject(resource), resource)
-	if err != nil {
-		return fmt.Errorf("cannot get resource to delete finalizer: %w", err)
-	}
-
-	UnsetFinalizer(resource, finalizer)
-
-	if err = client.Update(ctx, resource); err != nil {
-		return fmt.Errorf("failed to remove deletion finalizer from %s: %w", resource, err)
-	}
-	return nil
 }
