62b26e01b0bac7086d00c4798ad20dffaed731fa
Helder Santana <helder.santana@mongodb.com>
2023-02-06 15:15:04 -0300

    fix action to certify container images

--- .github/actions/certify-openshift-images/entrypoint.sh
+++ .github/actions/certify-openshift-images/entrypoint.sh
@@ -2,19 +2,24 @@
 
 set -eou pipefail
 
-docker login -u unused -p "${QUAY_PASSWORD}" quay.io
+docker login -u mongodb+mongodb_atlas_kubernetes -p "${QUAY_PASSWORD}" quay.io
 
 DIGESTS=$(docker manifest inspect "${REPOSITORY}:${VERSION}" | jq -r .manifests[].digest)
 
 for DIGEST in $DIGESTS; do
   echo "Checking image $DIGEST"
   # Do the preflight check first
-  preflight check container "${DIGEST}" --docker-config="${HOME}/.docker/config.json"
+  preflight check container "${REPOSITORY}@${DIGEST}" --artifacts "${DIGEST}" --docker-config="${HOME}/.docker/config.json"
 
-  # Send results to RedHat if preflight finished without errors
-  preflight check container "${DIGEST}" \
-    --submit \
-    --pyxis-api-token="${RHCC_TOKEN}" \
-    --certification-project-id="${RHCC_PROJECT}" \
-    --docker-config="${HOME}/.docker/config.json"
+  if [ "$SUBMIT" = "true" ]; then
+    rm -rf "${DIGEST}"
+    echo "Submitting result to RedHat Connect"
+    # Send results to RedHat if preflight finished wthout errors
+    preflight check container "${REPOSITORY}@${DIGEST}" \
+      --artifacts "${DIGEST}" \
+      --pyxis-api-token="${RHCC_TOKEN}" \
+      --certification-project-id="${RHCC_PROJECT}" \
+      --docker-config="${HOME}/.docker/config.json" \
+      --submit
+  fi
 done
