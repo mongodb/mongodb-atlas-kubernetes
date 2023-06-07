70a8c427fe0377f69e468a32a681d0e17e157581
Igor Karpukhin <karpukhinigor@gmail.com>
2023-06-07 14:46:14 +0200

    Fixed encryption at rest aws.roleID

--- pkg/api/v1/atlasdatafederation_types.go
+++ pkg/api/v1/atlasdatafederation_types.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 MongoDB.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1
 
 import (
@@ -115,10 +131,7 @@ func (pe DataFederationPE) Identifier() interface{} {
 // +kubebuilder:subresource:status
 // +groupName:=atlas.mongodb.com
 
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// +kubebuilder:object:root=true
-// +kubebuilder:subresource:status
-
+// AtlasDataFederation is the Schema for the Atlas Data Federation API
 type AtlasDataFederation struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
