94bb692efa21da22d8888f281c0dd9886f9c4857
Jose Vazquez <jose.vazquez@mongodb.com>
2023-07-04 14:27:51 +0200

    CLOUDP-186825: Add deletion protection to deployments
    Signed-off-by: Jose Vazquez <jose.vazquez@mongodb.com>

--- pkg/api/v1/atlasdeployment_types.go
+++ pkg/api/v1/atlasdeployment_types.go
@@ -210,6 +210,13 @@ type ServerlessSpec struct {
 	PrivateEndpoints []ServerlessPrivateEndpoint `json:"privateEndpoints,omitempty"`
 }
 
+// ToAtlas converts the ServerlessSpec to native Atlas client Cluster format.
+func (s *ServerlessSpec) ToAtlas() (*mongodbatlas.Cluster, error) {
+	result := &mongodbatlas.Cluster{}
+	err := compat.JSONCopy(result, s)
+	return result, err
+}
+
 // BiConnector specifies BI Connector for Atlas configuration on this deployment.
 type BiConnector struct {
 	Enabled        *bool  `json:"enabled,omitempty"`
