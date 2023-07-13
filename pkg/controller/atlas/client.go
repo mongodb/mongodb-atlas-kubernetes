0d639a3050ea6d4ccd95fbdde5febd7ca355bdba
josvaz <jose.vazquez@mongodb.com>
2023-07-13 14:17:29 +0200

    Support Atlas client transport customization (#1029)
    * Support Atlas client customization & logging
    
    Signed-off-by: Jose Vazquez <jose.vazquez@mongodb.com>
    
    * Update pkg/controller/atlas/client_test.go
    
    Co-authored-by: Gustavo Bazan <gssbzn@gmail.com>
    
    * Update pkg/controller/atlas/client_test.go
    
    Co-authored-by: Gustavo Bazan <gssbzn@gmail.com>
    
    ---------
    
    Signed-off-by: Jose Vazquez <jose.vazquez@mongodb.com>
    Co-authored-by: Gustavo Bazan <gssbzn@gmail.com>

--- pkg/controller/atlas/client.go
+++ pkg/controller/atlas/client.go
@@ -15,11 +15,13 @@ import (
 
 // Client is the central place to create a client for Atlas using specified API keys and a server URL.
 // Note, that the default HTTP transport is reused globally by Go so all caching, keep-alive etc will be in action.
-func Client(atlasDomain string, connection Connection, log *zap.SugaredLogger) (mongodbatlas.Client, error) {
+func Client(atlasDomain string, connection Connection, log *zap.SugaredLogger, opts ...httputil.ClientOpt) (mongodbatlas.Client, error) {
 	withDigest := httputil.Digest(connection.PublicKey, connection.PrivateKey)
 	withLogging := httputil.LoggingTransport(log)
+	allOptions := []httputil.ClientOpt{withDigest, withLogging}
+	allOptions = append(allOptions, opts...)
 
-	httpClient, err := httputil.DecorateClient(basicClient(), withDigest, withLogging)
+	httpClient, err := httputil.DecorateClient(basicClient(), allOptions...)
 	if err != nil {
 		return mongodbatlas.Client{}, err
 	}
--- pkg/controller/atlas/client_test.go
+++ pkg/controller/atlas/client_test.go
@@ -1,13 +1,17 @@
 package atlas_test
 
 import (
+	"context"
+	"net/http"
 	"testing"
 
-	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/version"
-
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.mongodb.org/atlas/mongodbatlas"
 
 	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/controller/atlas"
+	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/util/httputil"
+	"github.com/mongodb/mongodb-atlas-kubernetes/pkg/version"
 )
 
 func TestClientUserAgent(t *testing.T) {
@@ -16,4 +20,23 @@ func TestClientUserAgent(t *testing.T) {
 	c, err := atlas.Client("https://cloud.mongodb.com", atlas.Connection{}, nil)
 	r.NoError(err)
 	r.Contains(c.UserAgent, version.Version)
+}
+
+// RoundTrip implements http.RoundTripper registering if it got used
+type testTransport struct {
+	used bool
+}
+
+func (tt *testTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	tt.used = true
+	return nil, nil
+}
+
+func TestCustomTransport(t *testing.T) {
+	tt := &testTransport{used: false}
+
+	client, err := atlas.Client("https://cloud.mongodb.com", atlas.Connection{}, nil, httputil.CustomTransport(tt))
+	require.NoError(t, err)
+	client.Projects.GetAllProjects(context.Background(), &mongodbatlas.ListOptions{})
+	assert.True(t, tt.used)
 }
