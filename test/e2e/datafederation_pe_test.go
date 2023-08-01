945b190aa6baca82b040e35b8922c6d9d587db7a
Igor Karpukhin <karpukhinigor@gmail.com>
2023-08-01 23:20:49 +0200

    Fixed E2E test label for DataFederation test

--- test/e2e/datafederation_pe_test.go
+++ test/e2e/datafederation_pe_test.go
@@ -28,7 +28,7 @@ import (
 
 // AWS NOTES: reserved VPC in eu-west-2, eu-south-1, us-east-1 (due to limitation no more 4 VPC per region)
 
-var _ = Describe("UserLogin", Label("datafederation"), func() {
+var _ = Describe("UserLogin", Label("data-federation"), func() {
 	var testData *model.TestDataProvider
 	var providerAction cloud.Provider
 
