440f2b043b53b41150836c4d3ec5bbc8dd3be478
Igor Karpukhin <karpukhinigor@gmail.com>
2023-11-17 18:29:54 +0100

    Fixed local deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ docker build --rm -t "${image}" .
 docker push "${image}"
 
 #Prepare CRDs
-controller-gen crd:crdVersions=v1 rbac:roleName=manager-role webhook paths="./pkg/api/..." output:crd:artifacts:config=config/crd/bases
+controller-gen crd:crdVersions=v1,ignoreUnexportedFields=true rbac:roleName=manager-role webhook paths="./pkg/api/..." output:crd:artifacts:config=config/crd/bases
 
 #Installing the CRD,Operator,Role
 ns=mongodb-atlas-system
