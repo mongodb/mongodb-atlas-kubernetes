7aced5fbbc6edf6c4475cc9c28f1607bd4e7278d
Helder Santana <helder.santana@mongodb.com>
2024-01-12 14:15:22 +0100

    switch to sdk v20231001002.0.0

--- test/int/databaseuser_unprotected_test.go
+++ test/int/databaseuser_unprotected_test.go
@@ -21,8 +21,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/kube"
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/timeutil"
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/toptr"
 	mdbv1 "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1/project"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1/status"
