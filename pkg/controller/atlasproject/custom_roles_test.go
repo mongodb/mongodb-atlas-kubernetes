b6c8e637508e190ea2ff1be92a2d7cc4d44ef1c8
Roo Thorp <roo.thorp@mongodb.com>
2024-02-14 10:20:00 +0000

    fmt & lint

--- pkg/controller/atlasproject/custom_roles_test.go
+++ pkg/controller/atlasproject/custom_roles_test.go
@@ -403,7 +403,7 @@ func TestCanCustomRolesReconcile(t *testing.T) {
 		akoProject.WithAnnotations(map[string]string{customresource.AnnotationLastAppliedConfiguration: `{"customRoles":[{"name":"testRole1","actions":[{"name":"INSERT","resources":[{"database":"testDB","collection":"testCollection"}]}]}]}`})
 		workflowCtx := &workflow.Context{
 			Client:  &atlasClient,
-			Log: zaptest.NewLogger(t).Sugar(),
+			Log:     zaptest.NewLogger(t).Sugar(),
 			Context: context.Background(),
 		}
 		result, err := canCustomRolesReconcile(workflowCtx, true, akoProject)
