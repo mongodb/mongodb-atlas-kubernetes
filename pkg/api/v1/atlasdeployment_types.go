5b8ef20739a4ec8b39d464f6bbc2e43aca43407a
Igor Karpukhin <karpukhinigor@gmail.com>
2024-02-20 21:51:16 +0100

    Added search nodes implementation

--- pkg/api/v1/atlasdeployment_types.go
+++ pkg/api/v1/atlasdeployment_types.go
@@ -17,11 +17,14 @@ limitations under the License.
 package v1
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"reflect"
 	"regexp"
 	"strconv"
+
+	"go.mongodb.org/atlas-sdk/v20231115004/admin"
 
 	"go.mongodb.org/atlas/mongodbatlas"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -68,6 +71,14 @@ type AtlasDeploymentSpec struct {
 	// ProcessArgs allows to modify Advanced Configuration Options
 	// +optional
 	ProcessArgs *ProcessArgs `json:"processArgs,omitempty"`
+}
+
+type SearchNode struct {
+	// +kubebuilder:validation:Enum:=S20_HIGHCPU_NVME;S30_HIGHCPU_NVME;S40_HIGHCPU_NVME;S50_HIGHCPU_NVME;S60_HIGHCPU_NVME;S70_HIGHCPU_NVME;S80_HIGHCPU_NVME;S30_LOWCPU_NVME;S40_LOWCPU_NVME;S50_LOWCPU_NVME;S60_LOWCPU_NVME;S80_LOWCPU_NVME;S90_LOWCPU_NVME;S100_LOWCPU_NVME;S110_LOWCPU_NVME
+	InstanceSize string `json:"instanceSize,omitempty"`
+	// +kubebuilder:validation:Mininum:=2
+	// +kubebuilder:validation:Maximum:=32
+	NodeCount uint8 `json:"nodeCount,omitempty"`
 }
 
 type AdvancedDeploymentSpec struct {
@@ -130,6 +141,9 @@ type AdvancedDeploymentSpec struct {
 	// Flag that indicates whether termination protection is enabled on the cluster. If set to true, MongoDB Cloud won't delete the cluster. If set to false, MongoDB Cloud will delete the cluster.
 	// +kubebuilder:default:=false
 	TerminationProtectionEnabled bool `json:"terminationProtectionEnabled,omitempty"`
+	// List of settings that configure the search nodes for your cluster
+	// +optional
+	SearchNodes []SearchNode `json:"searchNodes,omitempty"`
 }
 
 // ToAtlas converts the AdvancedDeploymentSpec to native Atlas client ToAtlas format.
@@ -137,6 +151,26 @@ func (s *AdvancedDeploymentSpec) ToAtlas() (*mongodbatlas.AdvancedCluster, error
 	result := &mongodbatlas.AdvancedCluster{}
 	err := compat.JSONCopy(result, s)
 	return result, err
+}
+
+func (s *AdvancedDeploymentSpec) SearchNodesToAtlas() *[]admin.ApiSearchDeploymentSpec {
+	if s.SearchNodes == nil {
+		return nil
+	}
+	result := make([]admin.ApiSearchDeploymentSpec, len(s.SearchNodes))
+
+	o, _ := json.MarshalIndent(s.SearchNodes, "", " ")
+	fmt.Println("AKO SEARCH NODES", string(o))
+
+	for i := 0; i < len(s.SearchNodes); i++ {
+		result[i] = admin.ApiSearchDeploymentSpec{
+			InstanceSize: s.SearchNodes[i].InstanceSize,
+			NodeCount:    int(s.SearchNodes[i].NodeCount),
+		}
+	}
+	d, _ := json.MarshalIndent(result, "", " ")
+	fmt.Println("SEARCH NODES", string(d))
+	return &result
 }
 
 func LessAD(a, b interface{}) bool {
