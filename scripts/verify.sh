b001eb6cee16a14f2ffeb027101dfd5f868e6634
jose.vazquez <jose.vazquez@mongodb.com>
2024-02-22 12:26:46 +0100

    Remove comment

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -9,4 +9,4 @@ SIGNATURE_REPO=${SIGNATURE_REPO:-$REPO}
 KEY_FILE=${KEY_FILE:-ako.pem}
 
 COSIGN_REPOSITORY="${SIGNATURE_REPO}" cosign verify \
-  --key="${KEY_FILE}" "${img_to_verify}" # --insecure-ignore-tlog 
+  --key="${KEY_FILE}" "${img_to_verify}"
