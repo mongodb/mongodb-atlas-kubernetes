49496afc35ec671fc6bd8aea04aff503a7affcf3
jose.vazquez <jose.vazquez@mongodb.com>
2024-02-26 15:31:25 +0100

    Cleanup orphan GCP private links
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- tools/clean/provider/gcp.go
+++ tools/clean/provider/gcp.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path"
 	"strings"
+	"time"
 
 	compute "cloud.google.com/go/compute/apiv1"
 	"cloud.google.com/go/compute/apiv1/computepb"
@@ -100,6 +102,79 @@ func (gcp *GCP) DeletePrivateEndpoint(ctx context.Context, groupName, sAttachmen
 	return nil
 }
 
+func (gcp *GCP) DeleteOrphanPrivateEndpoints(ctx context.Context, lifetime int, region string, subnet string) ([]string, []string, error) {
+	addresses := gcp.addressClient.List(ctx, &computepb.ListAddressesRequest{
+		Project: gcp.projectID,
+		Region:  region,
+	})
+	done := []string{}
+	skipped := []string{}
+	for {
+		addr, err := addresses.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed iterating addresses in project %v region %v: %w",
+				gcp.projectID, region, err)
+		}
+		suffix := fmt.Sprintf("subnetworks/%s", subnet)
+		if !strings.HasSuffix(addr.GetSubnetwork(), suffix) {
+			skipped = append(skipped,
+				fmt.Sprintf("Address %s(%s) skipped, not in %s\n", addr.GetName(), addr.GetAddress(), subnet))
+			continue
+		}
+		createdAt, err := asTime(addr.GetCreationTimestamp())
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed parsing Address creation timestamp %q: %w",
+				addr.GetCreationTimestamp(), err)
+		}
+		if time.Since(createdAt) < time.Duration(lifetime)*time.Hour {
+			skipped = append(skipped, fmt.Sprintf("Address %s(%s) skipped once created less than %d hours ago\n",
+				addr.GetName(), addr.GetAddress(), lifetime))
+		}
+		frName, err := expectForwardingRule(addr.GetUsers())
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if frName != "" {
+			if err := gcp.deleteForwardingRule(ctx, frName, region); err != nil {
+				return nil, nil, fmt.Errorf("failed deleting Forwarding Rule %q in region %q: %w", region, frName, err)
+			}
+			done = append(done, fmt.Sprintf("Deleted Forwarding Rule %s for %s\n",
+				frName, addr.GetAddress()))
+		} else {
+			skipped = append(skipped,
+				fmt.Sprintf("No forwarding rule using Address %s(%s)", addr.GetName(), addr.GetAddress()))
+		}
+		if err := gcp.deleteIPAddress(ctx, addr.GetName(), region); err != nil {
+			return nil, nil, fmt.Errorf("error deleting Address %s(%s) in region %q: %w",
+				region, addr.GetName(), addr.GetAddress(), err)
+		}
+		done = append(done, fmt.Sprintf("Released orphan Address %s(%s)\n", addr.GetName(), addr.GetAddress()))
+	}
+
+	return done, skipped, nil
+}
+
+func asTime(rfc3339time string) (time.Time, error) {
+	return time.Parse(time.RFC3339, rfc3339time)
+}
+
+func expectForwardingRule(usersOfEndpointAddress []string) (string, error) {
+	if len(usersOfEndpointAddress) == 0 {
+		return "", nil
+	}
+	if len(usersOfEndpointAddress) > 1 {
+		return "", fmt.Errorf("expected a single user of an Endpoint Address, but got %v", usersOfEndpointAddress)
+	}
+	if strings.Contains(usersOfEndpointAddress[0], "/forwardingRules/") {
+		return path.Base(usersOfEndpointAddress[0]), nil
+	}
+	return "", fmt.Errorf("expected a Forwarding Rule user for Endpoint Address but got %s", usersOfEndpointAddress[0])
+}
+
 func (gcp *GCP) DeleteCryptoKey(ctx context.Context, keyName string) error {
 	_, err := gcp.keyManagementClient.GetCryptoKeyVersion(ctx, &kmspb.GetCryptoKeyVersionRequest{
 		Name: keyName,
@@ -135,7 +210,7 @@ func (gcp *GCP) deleteForwardRules(ctx context.Context, region, groupName string
 
 	it := gcp.forwardRuleClient.List(ctx, request)
 	for {
-		resp, err := it.Next()
+		fwr, err := it.Next()
 		if errors.Is(err, iterator.Done) {
 			break
 		}
@@ -143,24 +218,25 @@ func (gcp *GCP) deleteForwardRules(ctx context.Context, region, groupName string
 			return err
 		}
 
-		frRequest := &computepb.DeleteForwardingRuleRequest{
-			ForwardingRule: resp.GetName(),
-			Project:        gcp.projectID,
-			Region:         resp.GetRegion(),
-		}
-
-		op, err := gcp.forwardRuleClient.Delete(ctx, frRequest)
-		if err != nil {
-			return err
-		}
-
-		err = op.Wait(ctx)
-		if err != nil {
-			return err
+		if err := gcp.deleteForwardingRule(ctx, fwr.GetName(), fwr.GetRegion()); err != nil {
+			return os.ErrClosed
 		}
 	}
 
 	return nil
+}
+
+func (gcp *GCP) deleteForwardingRule(ctx context.Context, name, region string) error {
+	op, err := gcp.forwardRuleClient.Delete(ctx, &computepb.DeleteForwardingRuleRequest{
+		ForwardingRule: name,
+		Project:        gcp.projectID,
+		Region:         region,
+	})
+	if err != nil {
+		return err
+	}
+
+	return op.Wait(ctx)
 }
 
 func (gcp *GCP) deleteIPAddresses(ctx context.Context, region, groupName string) error {
@@ -174,7 +250,7 @@ func (gcp *GCP) deleteIPAddresses(ctx context.Context, region, groupName string)
 
 	it := gcp.addressClient.List(ctx, request)
 	for {
-		resp, err := it.Next()
+		addr, err := it.Next()
 		if errors.Is(err, iterator.Done) {
 			break
 		}
@@ -182,24 +258,25 @@ func (gcp *GCP) deleteIPAddresses(ctx context.Context, region, groupName string)
 			return err
 		}
 
-		aRequest := &computepb.DeleteAddressRequest{
-			Address: resp.GetName(),
-			Project: gcp.projectID,
-			Region:  resp.GetRegion(),
-		}
-
-		op, err := gcp.addressClient.Delete(ctx, aRequest)
-		if err != nil {
-			return err
-		}
-
-		err = op.Wait(ctx)
-		if err != nil {
+		if err := gcp.deleteIPAddress(ctx, addr.GetName(), addr.GetRegion()); err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func (gcp *GCP) deleteIPAddress(ctx context.Context, name, region string) error {
+	op, err := gcp.addressClient.Delete(ctx, &computepb.DeleteAddressRequest{
+		Address: name,
+		Project: gcp.projectID,
+		Region:  region,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete address %s at %s: %v", name, region, err)
+	}
+
+	return op.Wait(ctx)
 }
 
 func NewGCPCleaner(ctx context.Context) (*GCP, error) {
