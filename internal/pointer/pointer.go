3700f5d6b4b18f53bb74437ed393dcf0691bfba6
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2024-03-04 14:41:51 +0100

    migrate private endpoints to new Atlas SDK

--- internal/pointer/pointer.go
+++ internal/pointer/pointer.go
@@ -1,5 +1,12 @@
 package pointer
 
+func GetOrNilIfEmpty[T any](val []T) *[]T {
+	if len(val) == 0 {
+		return nil
+	}
+	return &val
+}
+
 // SetOrNil returns the address of the given value or nil if it equals defaultValue
 func SetOrNil[T comparable](val T, defaultValue T) *T {
 	if val == defaultValue {
