425ff8ce1612ebeb0e1e7bf7c7f4f08c7b6aa8d7
Helder Santana <helder.santana@mongodb.com>
2024-03-26 16:06:24 +0100

    Add Atlas Stream types

---
+++ pkg/api/v1/status/atlasstreaminstance.go
@@ -0,0 +1,20 @@
+package status
+
+import "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1/common"
+
+type AtlasStreamInstanceStatus struct {
+	Common `json:",inline"`
+	// Unique 24-hexadecimal character string that identifies the instance
+	ID string `json:"id,omitempty"`
+	// List that contains the hostnames assigned to the stream instance.
+	Hostnames []string `json:"hostnames,omitempty"`
+	// List of connections configured in the stream instance.
+	Connections []StreamConnection `json:"connections"`
+}
+
+type StreamConnection struct {
+	// Human-readable label that uniquely identifies the stream connection
+	Name string `json:"name,omitempty"`
+	// Reference for the resource that contains connection configuration
+	ResourceRef common.ResourceRefNamespaced `json:"resourceRef,omitempty"`
+}
