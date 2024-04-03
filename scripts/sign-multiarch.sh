58ad5df90487e39da494a956aade5e3c2a3b5a9a
jose.vazquez <jose.vazquez@mongodb.com>
2024-04-03 11:50:45 +0200

    CLOUDP-240887: Fix CI signatures
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- scripts/sign-multiarch.sh
+++ scripts/sign-multiarch.sh
@@ -9,14 +9,18 @@ action=${1:-sign}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker pull "${img}"
+docker inspect "${img}"
 MULTIARCH_IMG_SHA=$(docker inspect "${img}" |jq -rc '.[0].Id')
 IMG_PLATFORMS_SHAS=$(docker manifest inspect "${img}" | \
   jq -rc '.manifests[] | select(.platform.os != "unknown" and .platform.architecture != "unknown") | .digest')
-
-echo "${action} parent multiarch image ${img}@${MULTIARCH_IMG_SHA}..."
-IMG="${img}@${MULTIARCH_IMG_SHA}" "${SCRIPT_DIR}/${action}.sh"
 
 for platform_sha in ${IMG_PLATFORMS_SHAS}; do
   echo "${action} platform image ${img}@${platform_sha}..."
   IMG="${img}@${platform_sha}" "${SCRIPT_DIR}/${action}.sh"
 done
+
+echo "${action} default image ${img}..."
+IMG="${img}" "${SCRIPT_DIR}/${action}.sh"
+
+echo "${action} parent multiarch image ${img}@${MULTIARCH_IMG_SHA}..."
+IMG="${img}@${MULTIARCH_IMG_SHA}" "${SCRIPT_DIR}/${action}.sh"
