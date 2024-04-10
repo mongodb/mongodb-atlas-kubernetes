2545dbf5575341d0851bab874a0f15d5b83fb517
josvaz <jose.vazquez@mongodb.com>
2024-04-10 18:55:25 +0200

    Update generate_upload_sbom.sh
    Add platform support

--- scripts/generate_upload_sbom.sh
+++ scripts/generate_upload_sbom.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -Eeou pipefail
 
+platforms=("linux/arm64" "linux/amd64")
 image_pull_spec=""
 image_name=""
 tag_name=""
@@ -11,13 +12,16 @@ s3_path=""
 
 function usage() {
   echo "Generates and uploads an SBOM to an S3 bucket.
+
 Usage:
   generate_upload_sbom.sh [-h]
   generate_upload_sbom.sh -i <image_name> -b <bucket_name>
+
 Options:
   -h                   (optional) Shows this screen.
   -i <image_name>      (required) Image to be processed.
   -b                   (required) S3 bucket name.
+  -p                   (optional) An array of platforms, for example 'linux/arm64,linux/amd64'. The script **doesn't** fail if a particular architecture is not found.
 "
 }
 
@@ -36,13 +40,29 @@ function validate() {
       echo "Missing bucket name"
       usage
       exit 1
-  fi
+    fi
 }
 
-while getopts ':i:b:h' opt; do
+function generate_sbom() {
+  local image_pull_spec=$1
+  local platform=$2
+  local file_name=$3
+  set +Ee
+  docker sbom --platform "$platform" -o "$file_name" --format "cyclonedx-json" "$image_pull_spec"
+  docker_sbom_return_code=$?
+  set -Ee
+  if ((docker_sbom_return_code != 0)); then
+      echo "Image $image_pull_spec with platform $platform doesn't exist. Ignoring."
+      return 1
+  fi
+  return 0
+}
+
+while getopts ':p:i:b:h' opt; do
   case $opt in
     i) image_pull_spec=$OPTARG ;;
     b) bucket_name=$OPTARG ;;
+    p) IFS=',' read -ra platforms <<< "$OPTARG" ;;
     h) usage && exit 0;;
     *) usage && exit 0;;
   esac
@@ -63,7 +83,6 @@ repo_name="${repo_name##*/}"
 
 tag_name=${image_pull_spec##*:}
 
-file_name="${image_name}_${tag_name}.json"
 s3_path="s3://${bucket_name}/sboms/$registry_name/$repo_name/$image_name/$tag_name"
 
 echo "Generating and uploading SBOM for $image_pull_spec"
@@ -72,17 +91,20 @@ echo "Registry: $registry_name"
 echo "Repo: $repo_name"
 echo "Image: $image_name"
 echo "Tag: $tag_name"
+echo "Platforms:" "${platforms[@]}"
 echo "S3 Path: $s3_path"
-echo "File name: $file_name"
 
-echo "Generating SBOM for $image_pull_spec"
-docker sbom -o "$file_name" --format "cyclonedx-json" "$image_pull_spec"
+for platform in "${platforms[@]}"; do
+  s3_path_platform_dependent="$s3_path/${platform////_}"
+  file_name="${image_name}_${tag_name}_${platform////_}.json"
+  echo "Generating SBOM for $image_pull_spec ($platform) and uploading to $s3_path_platform_dependent"
 
-echo "Enabling S3 Bucket ($bucket_name) versioning"
-aws s3api put-bucket-versioning --bucket "${bucket_name}" --versioning-configuration Status=Enabled
+  if generate_sbom "$image_pull_spec" "$platform" "$file_name"; then
+    echo "Enabling S3 Bucket ($bucket_name) versioning"
+    aws s3api put-bucket-versioning --bucket "${bucket_name}" --versioning-configuration Status=Enabled
 
-echo "Copying SBOM to $s3_path"
-aws s3 cp "$file_name" "$s3_path"
-
-echo "Done generating and uploading SBOM for $image_pull_spec"
-
+    echo "Copying SBOM file $file_name to $s3_path_platform_dependent"
+    aws s3 cp "$file_name" "$s3_path_platform_dependent"
+  fi
+  echo "Done generating and uploading SBOM for $image_pull_spec"
+done
