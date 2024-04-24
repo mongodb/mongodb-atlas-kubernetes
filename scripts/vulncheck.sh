ef89b0a1c549e2608bb6fd847a6bd3b51ca81c2f
jose.vazquez <jose.vazquez@mongodb.com>
2024-04-24 09:12:26 +0200

    CLOUDP-242965: Run govulncheck at lint time

---
+++ scripts/vulncheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ignore_file=${1:-./vuln-ignore}
+
+ignore_lines=$(grep -v '^#' "${ignore_file}")
+check_cmd='govulncheck ./... |grep "Vulnerability #"'
+while IFS= read -r line; do
+  if [ "${line}" != "" ]; then
+    check_cmd+="|grep -v \"${line}\""
+  fi
+done <<< "${ignore_lines}"
+
+echo "${check_cmd}"
+vulns=$(eval "${check_cmd}")
+count=$(echo "${vulns}" |grep -c "\S")
+echo "govulncheck found ${count} non ignored vulnerabilities"
+if (( count != 0 )); then
+  echo "${vulns}"
+  echo "FAIL"
+  exit 1
+fi
+echo "PASS"
