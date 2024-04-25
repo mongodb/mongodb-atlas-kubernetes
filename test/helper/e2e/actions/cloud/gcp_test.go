d4f403344d2c3b74a9fe155492555fd35f1fd3cc
josvaz <jose.vazquez@mongodb.com>
2024-04-25 10:09:52 +0200

    Fix test to use correct sample IP (#1542)

--- test/helper/e2e/actions/cloud/gcp_test.go
+++ test/helper/e2e/actions/cloud/gcp_test.go
@@ -18,9 +18,9 @@ func TestCreateUsedVirtualAddress(t *testing.T) {
 	ga, err := NewGCPAction(gt, GoogleProjectID)
 	require.NoError(t, err)
 
-	err = ga.createVirtualAddress(ctx, "10.3.0.55", "name1", Subnet2Name, GCPRegion)
+	err = ga.createVirtualAddress(ctx, "10.0.0.155", "name1", Subnet2Name, GCPRegion)
 	require.NoError(t, err)
 	defer ga.deleteVirtualAddress(ctx, "name1", GCPRegion)
-	expectedErr := ga.createVirtualAddress(ctx, "10.3.0.55", "name2", Subnet2Name, GCPRegion)
+	expectedErr := ga.createVirtualAddress(ctx, "10.0.0.155", "name2", Subnet2Name, GCPRegion)
 	require.ErrorContains(t, expectedErr, "IP_IN_USE_BY_ANOTHER_RESOURCE")
 }
