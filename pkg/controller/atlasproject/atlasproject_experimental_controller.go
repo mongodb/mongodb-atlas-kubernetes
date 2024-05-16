46a612f8dca8a7fd960d2ee36b956264e625c0a1
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2024-05-16 18:32:48 +0200

    remove label handling

--- pkg/controller/atlasproject/atlasproject_experimental_controller.go
+++ pkg/controller/atlasproject/atlasproject_experimental_controller.go
@@ -18,17 +18,15 @@ package atlasproject
 
 import (
 	"context"
-	"fmt"
 	"reflect"
-	"strings"
 	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
 
 	"k8s.io/apimachinery/pkg/types"
 
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	"k8s.io/apimachinery/pkg/labels"
 
 	"go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -43,10 +41,6 @@ import (
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/atlas"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/customresource"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/watch"
-)
-
-const (
-	ExperimentalProject = "atlas.mongodb.com/experimentalproject"
 )
 
 type AtlasProjectExperimentalReconciler struct {
@@ -80,7 +74,7 @@ func (r *AtlasProjectExperimentalReconciler) SetupWithManager(mgr ctrl.Manager) 
 		Watches(
 			&akov2.AtlasProject{},
 			handler.EnqueueRequestsFromMapFunc(r.findExperimentalProjectForStableProject),
-			builder.WithPredicates(predicate.GenerationChangedPredicate{}),
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{}),
 		).
 		Complete(r)
 }
@@ -90,54 +84,47 @@ func (r *AtlasProjectExperimentalReconciler) findExperimentalProjectForStablePro
 	if !ok {
 		return nil
 	}
-	value, ok := stableProject.Labels[ExperimentalProject]
-	if !ok {
-		return nil
-	}
-
-	name := strings.TrimPrefix(strings.TrimPrefix(value, stableProject.Namespace), "-")
-	if len(name) == 0 {
-		return nil
-	}
 
 	return []reconcile.Request{{
 		NamespacedName: types.NamespacedName{
-			Name:      name,
+			Name:      stableProject.Name,
 			Namespace: stableProject.Namespace,
 		},
 	}}
 }
 
 func (r *AtlasProjectExperimentalReconciler) reconcile(ctx context.Context, experimentalProject *akov1alpha1.AtlasProject) (ctrl.Result, error) {
-	var stableProjects akov2.AtlasProjectList
-	if err := r.Client.List(ctx, &stableProjects, &client.ListOptions{
-		LabelSelector: mustParseLabelSelector(ExperimentalProject + "=" + experimentalProject.Namespace + "-" + experimentalProject.Name),
-	}); err != nil {
+	var stableProject akov2.AtlasProject
+	err := r.Client.Get(ctx, client.ObjectKey{Namespace: experimentalProject.GetNamespace(), Name: experimentalProject.GetName()}, &stableProject)
+
+	switch {
+	case errors.IsNotFound(err):
+		// no stable project is present, create one
+		return r.create(ctx, experimentalProject)
+	case err != nil:
+		// some error occured
 		return r.terminate(err)
 	}
 
-	switch {
-	// no stable project is present, create one
-	case len(stableProjects.Items) == 0:
-		return r.create(ctx, experimentalProject)
 	// one stable project is present, update it
-	case len(stableProjects.Items) == 1:
-		return r.update(ctx, experimentalProject, stableProjects.Items[0].DeepCopy())
+	updateResult, err := r.update(ctx, experimentalProject, &stableProject)
+	if err != nil {
+		return r.terminate(err)
 	}
 
-	return r.terminate(fmt.Errorf("expected none or one AtlasProject but found %v", len(stableProjects.Items)))
+	if err := r.Client.SubResource("status").Update(ctx, experimentalProject, &client.SubResourceUpdateOptions{}); err != nil {
+		return r.terminate(err)
+	}
+
+	return updateResult, nil
 }
 
 func (r *AtlasProjectExperimentalReconciler) create(ctx context.Context, experimentalProject *akov1alpha1.AtlasProject) (ctrl.Result, error) {
 	var stableProject akov2.AtlasProject
 
-	// overwrite spec
 	stableProject.Spec = experimentalProject.Spec.AtlasProjectSpec
 	stableProject.Namespace = experimentalProject.Namespace
-	stableProject.GenerateName = experimentalProject.Name
-	stableProject.Labels = map[string]string{
-		ExperimentalProject: experimentalProject.Namespace + "-" + experimentalProject.Name,
-	}
+	stableProject.Name = experimentalProject.Name
 
 	if err := r.Client.Create(ctx, &stableProject); err != nil {
 		return r.terminate(err)
@@ -161,10 +148,6 @@ func (r *AtlasProjectExperimentalReconciler) update(ctx context.Context, experim
 
 	// then, always update the experimental project status based on the stable project status
 	experimentalProject.Status = stableProject.Status
-	if err := r.Client.SubResource("status").Update(ctx, experimentalProject, &client.SubResourceUpdateOptions{}); err != nil {
-		return r.terminate(err)
-	}
-
 	return r.idle()
 }
 
@@ -178,12 +161,4 @@ func (r *AtlasProjectExperimentalReconciler) idle() (ctrl.Result, error) {
 
 func (r *AtlasProjectExperimentalReconciler) terminate(err error) (ctrl.Result, error) {
 	return ctrl.Result{}, err
-}
-
-func mustParseLabelSelector(label string) labels.Selector {
-	sel, err := labels.Parse(label)
-	if err != nil {
-		panic(err)
-	}
-	return sel
 }
