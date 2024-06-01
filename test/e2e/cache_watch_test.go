d9ead9f112760222782855d21c31eb155e5540a6
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2024-06-01 12:20:04 +0200

    test/e2e/cache_watch_test.go: improve e2e test

--- test/e2e/cache_watch_test.go
+++ test/e2e/cache_watch_test.go
@@ -10,6 +10,7 @@ import (
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -21,32 +22,38 @@ import (
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/data"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/k8s"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/model"
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/utils"
 )
+
+type tc struct {
+	namespaces      []string
+	watchNamespaces []string
+	wantToFind      []string
+}
 
 var _ = Describe("Kubernetes cache watch test:", Label("cache-watch"), func() {
 	DescribeTable("Cache gets", Label("watch-gets"),
-		func(name string, namespaceIndexesToWatch []int, expectedNamespaceFound []bool) {
+		func(ctx context.Context, testCase *tc) {
 			testData := model.DataProvider(
-				fmt.Sprintf("cache-%s", name),
+				fmt.Sprintf("cache-%s", CurrentSpecReport().LeafNodeLabels[0]),
 				model.NewEmptyAtlasKeyType().UseDefaultFullAccess(),
 				30008,
 				[]func(*model.TestDataProvider){},
 			)
-			namespaces := setupNamespaces(testData, name, len(expectedNamespaceFound))
-			defer clearNamespaces(testData, namespaces)
-			setupSecrets(testData, namespaces)
-			defer clearSecrets(testData, namespaces)
+			namespaces := setupNamespaces(ctx, testData, testCase.namespaces...)
+			defer clearNamespaces(ctx, testData, namespaces)
+			setupSecrets(ctx, testData, namespaces)
+			defer clearSecrets(ctx, testData, namespaces)
 
-			mgrHandle := setupManager(testData, namespaces, namespaceIndexesToWatch)
-			defer mgrHandle.stop()
+			mgr, stop := setupManager(ctx, namespaces, testCase.watchNamespaces)
+			defer stop()
 
+			wantToFindSet := sets.NewString(testCase.wantToFind...)
 			By("Using the manager cache to get all secrets in all namespaces and checking the expected results", func() {
-				cache := mgrHandle.mgr.GetCache()
-				for i, ns := range namespaces {
-					secret := &corev1.Secret{}
-					err := cache.Get(testData.Context, types.NamespacedName{Name: config.DefaultOperatorGlobalKey, Namespace: ns}, secret)
-					if expectedNamespaceFound[i] {
+				cache := mgr.GetCache()
+				for _, ns := range namespaces {
+					err := cache.Get(ctx, types.NamespacedName{Name: config.DefaultOperatorGlobalKey, Namespace: ns.GetName()}, &corev1.Secret{})
+
+					if wantToFindSet.Has(ns.GetGenerateName()) {
 						Expect(err).To(Succeed())
 					} else {
 						Expect(err).NotTo(Succeed())
@@ -55,36 +62,50 @@ var _ = Describe("Kubernetes cache watch test:", Label("cache-watch"), func() {
 				}
 			})
 		},
-		Entry("From all namespaces when no namespace config is set", Label("gets-all"),
-			"gets-all", []int{}, []bool{true, true, true}),
-		Entry("One namespace when only one namespace is configured", Label("gets-one"),
-			"gets-one", []int{0}, []bool{true, false, false}),
-		Entry("Two namespaces when only those two are configured", Label("gets-two"),
-			"gets-two", []int{0, 1}, []bool{true, true, false}),
+
+		Entry("From all namespaces when no namespace config is set", Label("gets-all"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: nil,
+			wantToFind:      []string{"ns1", "ns2", "ns3"},
+		}),
+
+		Entry("One namespace when only one namespace is configured", Label("gets-all"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: []string{"ns1"},
+			wantToFind:      []string{"ns1"},
+		}),
+
+		Entry("Two namespaces when only those two are configured", Label("gets-all"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: []string{"ns1", "n2"},
+			wantToFind:      []string{"ns1", "n2"},
+		}),
 	)
 
 	DescribeTable("Cache lists", Label("watch-lists"),
-		func(name string, namespaceIndexesToWatch []int, expectedNamespaceFound []bool) {
+		func(ctx context.Context, testCase *tc) {
 			testData := model.DataProvider(
-				fmt.Sprintf("cache-%s", name),
+				fmt.Sprintf("cache-%s", CurrentSpecReport().LeafNodeLabels[0]),
 				model.NewEmptyAtlasKeyType().UseDefaultFullAccess(),
 				30009,
 				[]func(*model.TestDataProvider){},
 			)
-			namespaces := setupNamespaces(testData, name, len(expectedNamespaceFound))
-			defer clearNamespaces(testData, namespaces)
-			setupSecrets(testData, namespaces)
-			defer clearSecrets(testData, namespaces)
 
-			mgrHandle := setupManager(testData, namespaces, namespaceIndexesToWatch)
-			defer mgrHandle.stop()
+			namespaces := setupNamespaces(ctx, testData, testCase.namespaces...)
+			defer clearNamespaces(ctx, testData, namespaces)
+			setupSecrets(ctx, testData, namespaces)
+			defer clearSecrets(ctx, testData, namespaces)
 
+			mgr, stop := setupManager(ctx, namespaces, testCase.watchNamespaces)
+			defer stop()
+
+			wantToFindSet := sets.NewString(testCase.wantToFind...)
 			By("Using the manager cache to list all secrets in all namespaces and checking the expected results", func() {
-				cache := mgrHandle.mgr.GetCache()
-				for i, ns := range namespaces {
-					secrets := &corev1.SecretList{}
-					err := cache.List(testData.Context, secrets, &client.ListOptions{Namespace: ns})
-					if expectedNamespaceFound[i] {
+				cache := mgr.GetCache()
+				for _, ns := range namespaces {
+					err := cache.List(ctx, &corev1.SecretList{}, &client.ListOptions{Namespace: ns.GetName()})
+
+					if wantToFindSet.Has(ns.GetGenerateName()) {
 						Expect(err).To(Succeed())
 					} else {
 						Expect(err).NotTo(Succeed())
@@ -93,12 +114,24 @@ var _ = Describe("Kubernetes cache watch test:", Label("cache-watch"), func() {
 				}
 			})
 		},
-		Entry("From all namespaces when no namespace config is set", Label("list-all"),
-			"list-all", []int{}, []bool{true, true, true}),
-		Entry("One namespace when only one namespace is configured", Label("list-one"),
-			"list-one", []int{0}, []bool{true, false, false}),
-		Entry("Two namespaces when only those two are configured", Label("list-two"),
-			"list-two", []int{0, 1}, []bool{true, true, false}),
+
+		Entry("From all namespaces when no namespace config is set", Label("list-all"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: nil,
+			wantToFind:      []string{"ns1", "ns2", "ns3"},
+		}),
+
+		Entry("One namespace when only one namespace is configured", Label("list-one"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: []string{"ns1"},
+			wantToFind:      []string{"ns1"},
+		}),
+
+		Entry("Two namespaces when only those two are configured", Label("list-two"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: []string{"ns1", "n2"},
+			wantToFind:      []string{"ns1", "n2"},
+		}),
 	)
 })
 
@@ -112,45 +145,47 @@ var _ = Describe("Kubernetes cache watch test:", Label("cache-watch"), func() {
 
 var _ = Describe("Reconciles test:", func() {
 	DescribeTable("Reconciles",
-		func(name string, namespaceIndexesToWatch []int, expectedNamespaceFound []bool) {
+		func(ctx context.Context, testCase *tc) {
 			testData := model.DataProvider(
-				name,
+				fmt.Sprintf("reconcile-%s", CurrentSpecReport().LeafNodeLabels[0]),
 				model.NewEmptyAtlasKeyType().UseDefaultFullAccess(),
 				30010,
 				[]func(*model.TestDataProvider){},
 			)
-			namespaces := setupNamespaces(testData, name, len(expectedNamespaceFound))
-			defer clearNamespaces(testData, namespaces)
-			setupSecrets(testData, namespaces)
-			defer clearSecrets(testData, namespaces)
+			namespaces := setupNamespaces(ctx, testData, testCase.namespaces...)
+			defer clearNamespaces(ctx, testData, namespaces)
+			setupSecrets(ctx, testData, namespaces)
+			defer clearSecrets(ctx, testData, namespaces)
 
-			mgrHandle := setupManager(testData, namespaces, namespaceIndexesToWatch)
-			defer mgrHandle.stop()
+			_, stop := setupManager(ctx, namespaces, testCase.watchNamespaces)
+			defer stop()
 
 			By("Launching an atlas project on each namespace, expect only listened namespaces to update status", func() {
-				projectNames := make([]string, len(expectedNamespaceFound))
+				projectNames := make([]string, len(namespaces))
+				wantToFindSet := sets.NewString(testCase.wantToFind...)
+
 				for i, ns := range namespaces {
 					project := data.DefaultProject()
 					By("Create project", func() {
-						project.Name = utils.RandomName(fmt.Sprintf("test-project-%s-ns%d", name, i))
-						project.Namespace = ns
+						project.Name = ""
+						project.GenerateName = "test-project-" + CurrentSpecReport().LeafNodeLabels[0]
+						project.Namespace = ns.GetName()
 						project.Spec.Name = "" // Set empty name to force validation error
-						projectNames[i] = project.Name
-
 						Expect(testData.K8SClient.Create(context.Background(), project)).ToNot(HaveOccurred())
+						projectNames[i] = project.Name
 					})
 
 					prj := &akov2.AtlasProject{}
 					By("Wait project to be present in Kubernetes", func() {
 						Eventually(func(g Gomega) bool {
 							return g.Expect(
-								testData.K8SClient.Get(testData.Context,
-									types.NamespacedName{Name: project.Name, Namespace: ns}, prj),
+								testData.K8SClient.Get(ctx,
+									types.NamespacedName{Name: project.Name, Namespace: ns.GetName()}, prj),
 							).To(Succeed())
 						}).WithTimeout(time.Minute).Should(BeTrue())
 					})
 
-					if expectedNamespaceFound[i] {
+					if wantToFindSet.Has(ns.GetGenerateName()) {
 						expectedCondition := api.Condition{
 							Type:    "ProjectReady",
 							Status:  "False",
@@ -160,8 +195,8 @@ var _ = Describe("Reconciles test:", func() {
 						By("Verify Kubernetes status got the expected error", func() {
 							Eventually(func(g Gomega) bool {
 								g.Expect(
-									testData.K8SClient.Get(testData.Context,
-										types.NamespacedName{Name: project.Name, Namespace: ns}, prj),
+									testData.K8SClient.Get(ctx,
+										types.NamespacedName{Name: project.Name, Namespace: ns.GetName()}, prj),
 								).To(Succeed())
 								match, err := ContainElement(conditions.MatchCondition(expectedCondition)).Match(prj.GetStatus().GetConditions())
 								g.Expect(err).To(Succeed())
@@ -174,8 +209,8 @@ var _ = Describe("Reconciles test:", func() {
 						By("Verify Kubernetes status remains untouched", func() {
 							Eventually(func(g Gomega) bool {
 								g.Expect(
-									testData.K8SClient.Get(testData.Context,
-										types.NamespacedName{Name: project.Name, Namespace: ns}, prj),
+									testData.K8SClient.Get(ctx,
+										types.NamespacedName{Name: project.Name, Namespace: ns.GetName()}, prj),
 								).To(Succeed())
 								if prj.Status.Common.ObservedGeneration == expectedObservedGeneration {
 									verifications += 1
@@ -186,123 +221,140 @@ var _ = Describe("Reconciles test:", func() {
 						})
 					}
 					By("Delete project", func() {
-						Expect(testData.K8SClient.Delete(context.Background(), project)).ToNot(HaveOccurred())
+						Expect(testData.K8SClient.Delete(ctx, project)).ToNot(HaveOccurred())
 					})
 				}
 			})
 		},
-		Entry("All namespaces when no namespace config is set", Label("reconcile-all"),
-			"reconcile-all", []int{}, []bool{true, true, true}),
-		Entry("One namespace when only one namespace is configured", Label("reconcile-one"),
-			"reconcile-one", []int{0}, []bool{true, false, false}),
-		Entry("Two namespaces when only those two are configured", Label("reconcile-two"),
-			"reconcile-two", []int{0, 1}, []bool{true, true, false}),
+
+		Entry("All namespaces when no namespace config is set", Label("reconcile-all"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: nil,
+			wantToFind:      []string{"ns1", "ns2", "ns3"},
+		}),
+
+		Entry("One namespace when only one namespace is configured", Label("reconcile-one"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: []string{"ns1"},
+			wantToFind:      []string{"ns1"},
+		}),
+
+		Entry("Two namespaces when only those two are configured", Label("reconcile-two"), &tc{
+			namespaces:      []string{"ns1", "ns2", "ns3"},
+			watchNamespaces: []string{"ns1", "n2"},
+			wantToFind:      []string{"ns1", "n2"},
+		}),
 	)
 })
 
-func setupNamespaces(testData *model.TestDataProvider, name string, size int) []string {
-	namespaces := make([]string, size)
+func setupNamespaces(ctx context.Context, testData *model.TestDataProvider, generateNames ...string) []*corev1.Namespace {
+	result := make([]*corev1.Namespace, 0, len(generateNames))
 	By("Setting up test namespaces (and wait for them to be created)", func() {
-		for i := 0; i < size; i++ {
-			namespaces[i] = utils.RandomName(fmt.Sprintf("%s-ns%d", name, i))
-			Expect(k8s.CreateNamespace(testData.Context, testData.K8SClient, namespaces[i])).To(Succeed())
+		for i := 0; i < len(generateNames); i++ {
+			namespace, err := k8s.CreateRandomNamespace(ctx, testData.K8SClient, generateNames[i])
+			Expect(err).To(BeNil())
+			result = append(result, namespace)
 		}
-		for _, ns := range namespaces {
+		for _, ns := range result {
 			Eventually(func(g Gomega) bool {
 				namespace := &corev1.Namespace{}
 				return g.Expect(
-					testData.K8SClient.Get(testData.Context, types.NamespacedName{Name: ns}, namespace),
+					testData.K8SClient.Get(ctx, types.NamespacedName{Name: ns.GetName()}, namespace),
 				).To(Succeed())
 			}).WithTimeout(time.Minute).Should(BeTrue())
 		}
 	})
-	return namespaces
+	return result
 }
 
-func setupSecrets(testData *model.TestDataProvider, namespaces []string) {
+func setupSecrets(ctx context.Context, testData *model.TestDataProvider, namespaces []*corev1.Namespace) {
 	By("Setting up test secrets, one on each namespace (and wait for them)", func() {
 		for _, ns := range namespaces {
-			k8s.CreateDefaultSecret(testData.Context, testData.K8SClient, config.DefaultOperatorGlobalKey, ns)
+			k8s.CreateDefaultSecret(ctx, testData.K8SClient, config.DefaultOperatorGlobalKey, ns.GetName())
 		}
 		for _, ns := range namespaces {
 			Eventually(func(g Gomega) bool {
 				secret := &corev1.Secret{}
 				return g.Expect(
-					testData.K8SClient.Get(testData.Context, types.NamespacedName{
-						Name: config.DefaultOperatorGlobalKey, Namespace: ns}, secret),
+					testData.K8SClient.Get(ctx, types.NamespacedName{
+						Name: config.DefaultOperatorGlobalKey, Namespace: ns.GetName()}, secret),
 				).To(Succeed())
 			}).WithTimeout(time.Minute).Should(BeTrue())
 		}
 	})
 }
 
-func setupManager(testData *model.TestDataProvider, namespaces []string, namespaceIndexesToWatch []int) *managerHandle {
-	mgrHandle := managerHandle{}
-	mgrCtx, cancelFn := context.WithCancel(context.Background())
-	mgrHandle.cancelFn = cancelFn
+type stopper func()
+
+func setupManager(ctx context.Context, namespaces []*corev1.Namespace, wantToWatch []string) (manager.Manager, stopper) {
+	var (
+		wg  sync.WaitGroup
+		mgr manager.Manager
+	)
+
+	wantToWatchSet := sets.NewString(wantToWatch...)
+	watchedNamespaces := make(map[string]bool)
+	for _, ns := range namespaces {
+		if wantToWatchSet.Has(ns.GetGenerateName()) {
+			watchedNamespaces[ns.GetName()] = true
+		}
+	}
+
+	mgrCtx, cancelFn := context.WithCancel(ctx)
 	By("Setting up the manager in the first namespace to watch on the given namespaces", func() {
 		managerConfig := &k8s.Config{
 			GlobalAPISecret: client.ObjectKey{
-				Namespace: namespaces[0],
+				Namespace: namespaces[0].GetName(),
 				Name:      config.DefaultOperatorGlobalKey,
 			},
 			FeatureFlags: featureflags.NewFeatureFlags(func() []string { return []string{} }),
 		}
-		watchedNamespaces := map[string]bool{}
-		for _, idx := range namespaceIndexesToWatch {
-			watchedNamespaces[namespaces[idx]] = true
-		}
+
 		if len(watchedNamespaces) > 0 {
 			managerConfig.WatchedNamespaces = watchedNamespaces
 		}
+
 		var err error
-		mgrHandle.mgr, err = k8s.BuildManager(managerConfig)
+		mgr, err = k8s.BuildManager(managerConfig)
 		Expect(err).NotTo(HaveOccurred())
 
-		mgrHandle.wg.Add(1)
+		wg.Add(1)
 		go func(ctx context.Context) {
-			defer mgrHandle.wg.Done()
-			err := mgrHandle.mgr.Start(ctx)
+			defer wg.Done()
+			err := mgr.Start(ctx)
 			Expect(err).NotTo(HaveOccurred())
 		}(mgrCtx)
 	})
 
 	By("wait for cache to be started", func() {
 		// the first namespace should always be cache-accessible
-		cache := mgrHandle.mgr.GetCache()
 		Eventually(func(g Gomega) bool {
-			namespace := &corev1.Namespace{}
 			return g.Expect(
-				cache.Get(testData.Context, types.NamespacedName{Name: namespaces[0]}, namespace),
+				mgr.GetCache().Get(ctx, types.NamespacedName{Name: namespaces[0].GetName()}, &corev1.Namespace{}),
 			).To(Succeed())
 		}).WithTimeout(time.Minute).Should(BeTrue())
 	})
-	return &mgrHandle
+
+	stopper := func() {
+		cancelFn()
+		wg.Wait()
+	}
+
+	return mgr, stopper
 }
 
-type managerHandle struct {
-	mgr      manager.Manager
-	wg       sync.WaitGroup
-	cancelFn context.CancelFunc
-}
-
-func (mgrHandle *managerHandle) stop() {
-	mgrHandle.cancelFn()
-	mgrHandle.wg.Wait()
-}
-
-func clearSecrets(testData *model.TestDataProvider, namespaces []string) {
+func clearSecrets(ctx context.Context, testData *model.TestDataProvider, namespaces []*corev1.Namespace) {
 	By("Clearing the secrets", func() {
 		for _, ns := range namespaces {
-			Expect(k8s.DeleteKey(testData.Context, testData.K8SClient, config.DefaultOperatorGlobalKey, ns)).To(Succeed())
+			Expect(k8s.DeleteKey(ctx, testData.K8SClient, config.DefaultOperatorGlobalKey, ns.GetName())).To(Succeed())
 		}
 	})
 }
 
-func clearNamespaces(testData *model.TestDataProvider, namespaces []string) {
+func clearNamespaces(ctx context.Context, testData *model.TestDataProvider, namespaces []*corev1.Namespace) {
 	By("Clearing namespaces", func() {
 		for _, ns := range namespaces {
-			Expect(k8s.DeleteNamespace(testData.Context, testData.K8SClient, ns)).To(Succeed())
+			Expect(k8s.DeleteNamespace(ctx, testData.K8SClient, ns.GetName())).To(Succeed())
 		}
 	})
 }
