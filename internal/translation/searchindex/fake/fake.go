aad736dbb67df385a1de1ad3d2c3cadd3e945516
jose.vazquez <jose.vazquez@mongodb.com>
2024-06-14 10:02:57 +0200

    Simplify fakes, they panic anyways

--- internal/translation/searchindex/fake/fake.go
+++ internal/translation/searchindex/fake/fake.go
@@ -14,29 +14,17 @@ type FakeAtlasSearch struct {
 }
 
 func (fas *FakeAtlasSearch) GetIndex(ctx context.Context, projectID, clusterName, indexName, indexID string) (*searchindex.SearchIndex, error) {
-	if fas.GetIndexFunc == nil {
-		panic("unimplemented")
-	}
 	return fas.GetIndexFunc(ctx, projectID, clusterName, indexName, indexID)
 }
 
 func (fas *FakeAtlasSearch) CreateIndex(ctx context.Context, projectID, clusterName string, index *searchindex.SearchIndex) (*searchindex.SearchIndex, error) {
-	if fas.CreateIndexFunc == nil {
-		panic("unimplemented")
-	}
 	return fas.CreateIndexFunc(ctx, projectID, clusterName, index)
 }
 
 func (fas *FakeAtlasSearch) DeleteIndex(ctx context.Context, projectID, clusterName, indexID string) error {
-	if fas.DeleteIndexFunc == nil {
-		panic("unimplemented")
-	}
 	return fas.DeleteIndexFunc(ctx, projectID, clusterName, indexID)
 }
 
 func (fas *FakeAtlasSearch) UpdateIndex(ctx context.Context, projectID, clusterName string, index *searchindex.SearchIndex) (*searchindex.SearchIndex, error) {
-	if fas.UpdateIndexFunc == nil {
-		panic("unimplemented")
-	}
 	return fas.UpdateIndexFunc(ctx, projectID, clusterName, index)
 }
