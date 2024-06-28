e7665af4e193e692aff3204148fe3fa3cd7643cf
Roo Thorp <roo.thorp@mongodb.com>
2024-06-28 12:07:54 +0100

    Update Project Settings to use new SDK

--- pkg/controller/atlasproject/project_settings.go
+++ pkg/controller/atlasproject/project_settings.go
@@ -45,24 +45,20 @@ func areSettingsInSync(atlas, spec *akov2.ProjectSettings) bool {
 }
 
 func patchSettings(ctx *workflow.Context, projectID string, spec *akov2.ProjectSettings) error {
-	specAsAtlas, err := spec.ToAtlas()
-	if err != nil {
-		return err
-	}
+	specAsAtlas := spec.ToAtlas()
 
-	_, _, err = ctx.Client.Projects.UpdateProjectSettings(ctx.Context, projectID, specAsAtlas)
+	_, _, err := ctx.SdkClient.ProjectsApi.UpdateProjectSettings(ctx.Context, projectID, specAsAtlas).Execute()
 	return err
 }
 
 func fetchSettings(ctx *workflow.Context, projectID string) (*akov2.ProjectSettings, error) {
-	data, _, err := ctx.Client.Projects.GetProjectSettings(ctx.Context, projectID)
+	data, _, err := ctx.SdkClient.ProjectsApi.GetProjectSettings(ctx.Context, projectID).Execute()
 	if err != nil {
 		return nil, err
 	}
 	ctx.Log.Debugw("Got Project Settings", "data", data)
 
-	settings := akov2.ProjectSettings(*data)
-	return &settings, nil
+	return akov2.ProjectSettingsFromAtlas(data), nil
 }
 
 func isOneContainedInOther(one, other *akov2.ProjectSettings) bool {
