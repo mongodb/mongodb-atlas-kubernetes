6b8f28d6dd212770242a1cbbc95df8967e32948b
Roo Thorp <roo.thorp@mongodb.com>
2024-06-28 15:05:10 +0100

    CLOUDP-228590: Migrate Project Settings to new Atlas SDK (#1666)
    * Update Project Settings to use new SDK
    
    * Add unit tests

--- pkg/api/v1/project_settings.go
+++ pkg/api/v1/project_settings.go
@@ -1,9 +1,7 @@
 package v1
 
 import (
-	"go.mongodb.org/atlas/mongodbatlas"
-
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/compat"
+	"go.mongodb.org/atlas-sdk/v20231115008/admin"
 )
 
 type ProjectSettings struct {
@@ -15,8 +13,28 @@ type ProjectSettings struct {
 	IsSchemaAdvisorEnabled                      *bool `json:"isSchemaAdvisorEnabled,omitempty"`
 }
 
-func (s ProjectSettings) ToAtlas() (*mongodbatlas.ProjectSettings, error) {
-	result := &mongodbatlas.ProjectSettings{}
-	err := compat.JSONCopy(result, s)
-	return result, err
+func (s ProjectSettings) ToAtlas() *admin.GroupSettings {
+	atlas := &admin.GroupSettings{}
+
+	atlas.IsCollectDatabaseSpecificsStatisticsEnabled = s.IsCollectDatabaseSpecificsStatisticsEnabled
+	atlas.IsDataExplorerEnabled = s.IsDataExplorerEnabled
+	atlas.IsExtendedStorageSizesEnabled = s.IsExtendedStorageSizesEnabled
+	atlas.IsPerformanceAdvisorEnabled = s.IsPerformanceAdvisorEnabled
+	atlas.IsRealtimePerformancePanelEnabled = s.IsRealtimePerformancePanelEnabled
+	atlas.IsSchemaAdvisorEnabled = s.IsSchemaAdvisorEnabled
+
+	return atlas
+}
+
+func ProjectSettingsFromAtlas(atlas *admin.GroupSettings) *ProjectSettings {
+	ps := &ProjectSettings{}
+
+	ps.IsCollectDatabaseSpecificsStatisticsEnabled = atlas.IsCollectDatabaseSpecificsStatisticsEnabled
+	ps.IsDataExplorerEnabled = atlas.IsDataExplorerEnabled
+	ps.IsExtendedStorageSizesEnabled = atlas.IsExtendedStorageSizesEnabled
+	ps.IsPerformanceAdvisorEnabled = atlas.IsPerformanceAdvisorEnabled
+	ps.IsRealtimePerformancePanelEnabled = atlas.IsRealtimePerformancePanelEnabled
+	ps.IsSchemaAdvisorEnabled = atlas.IsSchemaAdvisorEnabled
+
+	return ps
 }
