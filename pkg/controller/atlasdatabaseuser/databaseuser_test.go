8374effd240f126f4377b59787db54a6edbed293
Igor Karpukhin <karpukhinigor@gmail.com>
2024-07-24 10:25:54 +0200

    Fixed imports ordering

--- pkg/controller/atlasdatabaseuser/databaseuser_test.go
+++ pkg/controller/atlasdatabaseuser/databaseuser_test.go
@@ -6,8 +6,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/mocks/atlas"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
