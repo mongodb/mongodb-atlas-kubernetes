1b981f8b1b62b239f5fe60e2a51bc5a7dc1ac8a2
jose.vazquez <jose.vazquez@mongodb.com>
2024-07-29 13:46:41 +0200

    Make CI preflight check the test images
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- .github/actions/certify-openshift-images/entrypoint.sh
+++ .github/actions/certify-openshift-images/entrypoint.sh
@@ -2,22 +2,27 @@
 
 set -eou pipefail
 
-docker login -u mongodb+mongodb_atlas_kubernetes -p "${QUAY_PASSWORD}" quay.io
+docker login -u mongodb+mongodb_atlas_kubernetes -p "${REGISTRY_PASSWORD}" "${REGISTRY}"
 
-DIGESTS=$(docker manifest inspect "quay.io/${REPOSITORY}:${VERSION}" | jq -r '.manifests[] | select(.platform.os!="unknown") | .digest')
-mapfile -t PLATFORMS < <(docker manifest inspect "quay.io/${REPOSITORY}:${VERSION}" | jq -r '.manifests[] | select(.platform.os!="unknown") | .platform.architecture')
+DIGESTS=$(docker manifest inspect "${REGISTRY}/${REPOSITORY}:${VERSION}" | jq -r '.manifests[] | select(.platform.os!="unknown") | .digest')
+mapfile -t PLATFORMS < <(docker manifest inspect "${REGISTRY}/${REPOSITORY}:${VERSION}" | jq -r '.manifests[] | select(.platform.os!="unknown") | .platform.architecture')
+
+submit_flag=--submit
+if [ "${SUBMIT}" == "false" ]; then
+  submit_flag=
+fi
 
 INDEX=0
 for DIGEST in $DIGESTS; do
     echo "Check and Submit result to RedHat Connect"
     # Send results to RedHat if preflight finished wthout errors
-    preflight check container "quay.io/${REPOSITORY}@${DIGEST}" \
+    preflight check container "${REGISTRY}/${REPOSITORY}@${DIGEST}" \
       --artifacts "${DIGEST}" \
       --platform "${PLATFORMS[$INDEX]}" \
       --pyxis-api-token="${RHCC_TOKEN}" \
       --certification-project-id="${RHCC_PROJECT}" \
       --docker-config="${HOME}/.docker/config.json" \
-      --submit
+      ${submit_flag}
 
   (( INDEX++ )) || true
 done
