ad30ace1931dae5ff8cd55b6fa61ac3df92bc7d0
filipcirtog <filip.cirtog@mongodb.com>
2024-07-29 14:37:41 +0100

    helm

--- shell.nix
+++ shell.nix
@@ -23,12 +23,14 @@ pkgs.mkShell {
     pkgs.jq
     pkgs.go
     pkgs.kubectl
+    pkgs.docker
     pkgs.kubernetes-controller-tools
     pkgs.kustomize_4
     pkgs.git
     pkgs.envsubst
     pkgs.wget
     pkgs.cosign
+    pkgs.kubernetes-helm
     pkgs.govulncheck
     pkgs.gotools
     pkgs.go-licenses
@@ -41,6 +43,7 @@ pkgs.mkShell {
     export PATH=$PATH:${pkgs.go}/bin
     export PATH=$PATH:${pkgs.cosign}/bin
     export PATH=$PATH:${pkgs.go-licenses}/bin
+    export PATH=$PATH:${pkgs.docker}/bin
     export PATH=$PATH:${pkgs.yq-go}/bin
     export PATH=$PATH:${pkgs.wget}/bin
     export PATH=$PATH:${pkgs.envsubst}/bin
@@ -49,6 +52,7 @@ pkgs.mkShell {
     export PATH=$PATH:${pkgs.jq}/bin
     export PATH=$PATH:${pkgs.kubernetes-controller-tools}/bin
     export PATH=$PATH:${pkgs.kubectl}/bin
+     export PATH=$PATH:${pkgs.kubernetes-helm}/bin
     export PATH=$PATH:${pkgs.kustomize}/bin
     export PATH=$PATH:${pkgs.govulncheck}/bin
     export PATH=$PATH:${pkgs.gotools}/bin
