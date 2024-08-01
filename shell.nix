a8b3eeb9b0e2838bcade6d1cb0d09c595d461681
filipcirtog <filip.cirtog@mongodb.com>
2024-08-01 15:44:01 +0100

    nix-improvments

--- shell.nix
+++ shell.nix
@@ -20,8 +20,10 @@ pkgs.mkShell {
   buildInputs = [
     golangci-lint-private
     pkgs.yq-go
+    pkgs.kubebuilder
     pkgs.jq
     pkgs.go
+    phgs.act
     pkgs.kubectl
     pkgs.docker
     pkgs.kubernetes-controller-tools
@@ -36,11 +38,13 @@ pkgs.mkShell {
     pkgs.go-licenses
     pkgs.ginkgo
     pkgs.operator-sdk
+    pkgs.shellcheck
   ];
 
   shellHook = ''
     export PATH=$PATH:${pkgs.go}/bin
     export PATH=$PATH:${pkgs.cosign}/bin
+    export PATH=$PATH:${pkgs.kubebuilder}/bin
     export PATH=$PATH:${pkgs.go-licenses}/bin
     export PATH=$PATH:${pkgs.docker}/bin
     export PATH=$PATH:${pkgs.yq-go}/bin
@@ -48,9 +52,11 @@ pkgs.mkShell {
     export PATH=$PATH:${pkgs.envsubst}/bin
     export PATH=$PATH:${pkgs.git}/bin
     export PATH=$PATH:${pkgs.jq}/bin
+    export PATH=$PATH:${pkgs.shellcheck}/bin
     export PATH=$PATH:${pkgs.kubernetes-controller-tools}/bin
     export PATH=$PATH:${pkgs.kubectl}/bin
-     export PATH=$PATH:${pkgs.kubernetes-helm}/bin
+    export PATH=$PATH:${pkgs.kubernetes-helm}/bin
+    export PATH=$PATH:${pkgs.act}/bin
     export PATH=$PATH:${pkgs.kustomize}/bin
     export PATH=$PATH:${pkgs.govulncheck}/bin
     export PATH=$PATH:${pkgs.gotools}/bin
