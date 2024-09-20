a80ff061dc117300cb4372353a56d01c2690e204
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2024-09-20 11:03:42 +0200

    datafederation: switch to versioned SDK, add translation layer

--- pkg/controller/atlasdatafederation/datafederation.go
+++ pkg/controller/atlasdatafederation/datafederation.go
@@ -1,42 +1,29 @@
 package atlasdatafederation
 
 import (
-	"net/http"
+	"errors"
 
-	"go.mongodb.org/atlas/mongodbatlas"
-
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
-	"go.uber.org/zap"
-
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/compat"
+	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/translation/datafederation"
 	akov2 "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1"
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1/common"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/workflow"
 )
 
-func (r *AtlasDataFederationReconciler) ensureDataFederation(ctx *workflow.Context, project *akov2.AtlasProject, dataFederation *akov2.AtlasDataFederation) workflow.Result {
-	log := ctx.Log
-
+func (r *AtlasDataFederationReconciler) ensureDataFederation(ctx *workflow.Context, project *akov2.AtlasProject, dataFederation *akov2.AtlasDataFederation, federationService datafederation.DataFederationService) workflow.Result {
 	projectID := project.ID()
 	operatorSpec := &dataFederation.Spec
 
-	dataFederationToAtlas, err := dataFederation.ToAtlas()
+	akoDataFederation, err := datafederation.NewDataFederation(&dataFederation.Spec, projectID, nil)
 	if err != nil {
-		return workflow.Terminate(workflow.Internal, "can not convert DataFederation (operator -> atlas)")
+		return workflow.Terminate(workflow.Internal, err.Error())
 	}
 
-	atlasSpec, resp, err := ctx.Client.DataFederation.Get(ctx.Context, projectID, operatorSpec.Name)
+	atlasDataFederation, err := federationService.Get(ctx.Context, projectID, operatorSpec.Name)
 	if err != nil {
-		if resp == nil {
+		if !errors.Is(err, datafederation.ErrorNotFound) {
 			return workflow.Terminate(workflow.Internal, err.Error())
 		}
 
-		if resp.StatusCode != http.StatusNotFound {
-			return workflow.Terminate(workflow.DataFederationNotCreatedInAtlas, err.Error())
-		}
-
-		_, _, err = ctx.Client.DataFederation.Create(ctx.Context, projectID, dataFederationToAtlas)
+		err = federationService.Create(ctx.Context, akoDataFederation)
 		if err != nil {
 			return workflow.Terminate(workflow.DataFederationNotCreatedInAtlas, err.Error())
 		}
@@ -44,57 +31,14 @@ func (r *AtlasDataFederationReconciler) ensureDataFederation(ctx *workflow.Conte
 		return workflow.InProgress(workflow.DataFederationCreating, "Data Federation is being created")
 	}
 
-	dfFromAtlas, err := DataFederationFromAtlas(atlasSpec)
-	if err != nil {
-		return workflow.Terminate(workflow.Internal, "can not convert DataFederation (atlas -> operator)")
-	}
-
-	if areEqual, _ := dataFederationEqual(*dfFromAtlas, *operatorSpec, log); areEqual {
+	if akoDataFederation.SpecEqualsTo(atlasDataFederation) {
 		return workflow.OK()
 	}
 
-	_, _, err = ctx.Client.DataFederation.Update(ctx.Context, projectID, dataFederation.Spec.Name, dataFederationToAtlas, nil)
+	err = federationService.Update(ctx.Context, akoDataFederation)
 	if err != nil {
 		return workflow.Terminate(workflow.DataFederationNotUpdatedInAtlas, err.Error())
 	}
 
 	return workflow.InProgress(workflow.DataFederationUpdating, "Data Federation is being updated")
-}
-
-func DataFederationFromAtlas(atlasDF *mongodbatlas.DataFederationInstance) (*akov2.DataFederationSpec, error) {
-	dfSpec := &akov2.DataFederationSpec{}
-	err := compat.JSONCopy(dfSpec, atlasDF)
-	return dfSpec, err
-}
-
-func dataFederationEqual(atlasSpec, operatorSpec akov2.DataFederationSpec, log *zap.SugaredLogger) (areEqual bool, diff string) {
-	mergedSpec, err := getMergedSpec(atlasSpec, operatorSpec)
-	if err != nil {
-		log.Errorf("failed to merge Data Federation specs: %s", err.Error())
-		return false, ""
-	}
-
-	d := cmp.Diff(atlasSpec, mergedSpec, cmpopts.EquateEmpty())
-	if d != "" {
-		log.Debugf("Data Federation diff: \n%s", d)
-	}
-
-	return d == "", d
-}
-
-func getMergedSpec(atlasSpec, operatorSpec akov2.DataFederationSpec) (akov2.DataFederationSpec, error) {
-	mergedSpec := akov2.DataFederationSpec{}
-
-	operatorSpec.PrivateEndpoints = []akov2.DataFederationPE{}
-
-	if err := compat.JSONCopy(&mergedSpec, atlasSpec); err != nil {
-		return mergedSpec, err
-	}
-	if err := compat.JSONCopy(&mergedSpec, operatorSpec); err != nil {
-		return mergedSpec, err
-	}
-
-	mergedSpec.Project = common.ResourceRefNamespaced{}
-
-	return mergedSpec, nil
 }
