b810e6b69881c4a49df9b4092ae233d40748f70a
jose.vazquez <jose.vazquez@mongodb.com>
2024-10-04 17:44:17 +0200

    CLOUDP-277184: Fix skipped e2e tests
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

---
+++ scripts/launch-ci-e2e.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+helm version
+go version
+cd test/e2e
+
+# no `long-run`, no `broken` tests. `Long-run` tests run as a separate job
+if [[ $TEST_NAME == "long-run" ]]; then
+	filter="long-run && !broken";
+else 
+	filter="$TEST_NAME && !long-run && !broken";
+fi
+
+AKO_E2E_TEST=1 ginkgo --output-interceptor-mode=none --label-filter="${filter}" --timeout 120m --nodes=10 \
+  --flake-attempts=1 --race --cover --v --coverpkg=github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/...
+
