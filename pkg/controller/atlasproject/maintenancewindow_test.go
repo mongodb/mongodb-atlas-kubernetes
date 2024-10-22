54056ad7c3994c54ce040c6b4735c0843ca2a737
Roo Thorp <roo.thorp@mongodb.com>
2024-10-22 17:17:10 +0100

    add and update tests

--- pkg/controller/atlasproject/maintenancewindow_test.go
+++ pkg/controller/atlasproject/maintenancewindow_test.go
@@ -1,11 +1,22 @@
 package atlasproject
 
 import (
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+
+	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/mocks/translation"
+	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/translation/maintenancewindow"
+	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api"
+	akov2 "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1"
+	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/workflow"
 
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1/project"
+	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1/status"
 )
 
 func TestValidateMaintenanceWindow(t *testing.T) {
@@ -117,6 +128,323 @@ func TestValidateMaintenanceWindow(t *testing.T) {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestEnsureMaintenanceWindow(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+
+		maintenanceWindow  project.MaintenanceWindow
+		maintenanceService maintenancewindow.MaintenanceWindowService
+
+		isOK             bool
+		wantStatus       string
+		conditionMissing bool
+	}{
+		{
+			name: "maintenance window is the same in atlas and kube",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 2,
+							HourOfDay: 14,
+						},
+					},
+					nil,
+				)
+				return service
+			}(),
+			isOK:       true,
+			wantStatus: "True",
+		},
+		{
+			name: "validation fails",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				StartASAP: true,
+				Defer:     true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				return nil
+			}(),
+			isOK:       false,
+			wantStatus: "False",
+		},
+		{
+			name: "get request errors",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{},
+					errors.New("TEST GET ERROR"),
+				)
+				return service
+			}(),
+			isOK:       false,
+			wantStatus: "False",
+		},
+		{
+			name: "maintenance window is different (update)",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 4,
+							HourOfDay: 18,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().Update(context.Background(), "testid123", mock.AnythingOfType("*maintenancewindow.MaintenanceWindow")).Return(nil)
+				return service
+			}(),
+			isOK:       true,
+			wantStatus: "True",
+		},
+		{
+			name: "start maintenance ASAP",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				StartASAP: true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 2,
+							HourOfDay: 14,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().Update(context.Background(), "testid123", mock.AnythingOfType("*maintenancewindow.MaintenanceWindow")).Return(nil)
+				return service
+			}(),
+			isOK:       true,
+			wantStatus: "True",
+		},
+		{
+			name: "update request fails",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				StartASAP: true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 4,
+							HourOfDay: 18,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().Update(context.Background(), "testid123", mock.AnythingOfType("*maintenancewindow.MaintenanceWindow")).
+					Return(errors.New("TEST UPDATE ERROR"))
+				return service
+			}(),
+			isOK:       false,
+			wantStatus: "False",
+		},
+		{
+			name:              "maintenance window not in AKO (delete/reset)",
+			maintenanceWindow: project.MaintenanceWindow{},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Reset(context.Background(), "testid123").Return(nil)
+				return service
+			}(),
+			isOK:             true,
+			wantStatus:       "False",
+			conditionMissing: true,
+		},
+		{
+			name:              "reset request errors",
+			maintenanceWindow: project.MaintenanceWindow{},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Reset(context.Background(), "testid123").Return(errors.New("TEST RESET ERROR"))
+				return service
+			}(),
+			isOK:       false,
+			wantStatus: "False",
+		},
+		{
+			name: "auto defer toggled",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				AutoDefer: true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 2,
+							HourOfDay: 14,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().ToggleAutoDefer(context.Background(), "testid123").Return(nil)
+				return service
+			}(),
+			isOK:       true,
+			wantStatus: "True",
+		},
+
+		{
+			name: "auto defer toggle errors",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				AutoDefer: true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 2,
+							HourOfDay: 14,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().ToggleAutoDefer(context.Background(), "testid123").Return(errors.New("TEST AUTO DEFER ERROR"))
+				return service
+			}(),
+			isOK:       false,
+			wantStatus: "False",
+		},
+		{
+			name: "defer maintenance",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				Defer:     true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 2,
+							HourOfDay: 14,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().Defer(context.Background(), "testid123").Return(nil)
+				return service
+			}(),
+			isOK:       true,
+			wantStatus: "True",
+		},
+		{
+			name: "defer maintenance and update",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				Defer:     true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 4,
+							HourOfDay: 18,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().Update(context.Background(), "testid123", mock.AnythingOfType("*maintenancewindow.MaintenanceWindow")).Return(nil)
+				service.EXPECT().Defer(context.Background(), "testid123").Return(nil)
+				return service
+			}(),
+			isOK:       true,
+			wantStatus: "True",
+		},
+		{
+			name: "defer maintenance errors",
+			maintenanceWindow: project.MaintenanceWindow{
+				DayOfWeek: 2,
+				HourOfDay: 14,
+				Defer:     true,
+			},
+			maintenanceService: func() maintenancewindow.MaintenanceWindowService {
+				service := translation.NewMaintenanceWindowServiceMock(t)
+				service.EXPECT().Get(context.Background(), "testid123").Return(
+					&maintenancewindow.MaintenanceWindow{
+						MaintenanceWindow: &project.MaintenanceWindow{
+							DayOfWeek: 2,
+							HourOfDay: 14,
+						},
+					},
+					nil,
+				)
+				service.EXPECT().Defer(context.Background(), "testid123").Return(errors.New("TEST DEFER ERROR"))
+				return service
+			}(),
+			isOK:       false,
+			wantStatus: "False",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			proj := &akov2.AtlasProject{
+				Status: status.AtlasProjectStatus{
+					ID: "testid123",
+				},
+				Spec: akov2.AtlasProjectSpec{
+					MaintenanceWindow: tc.maintenanceWindow,
+				},
+			}
+
+			ctx := &workflow.Context{
+				Context: context.Background(),
+				Log:     zap.S(),
+			}
+			ctx.SetConditionTrue(api.MaintenanceWindowReadyType)
+
+			r := AtlasProjectReconciler{
+				maintenanceService: tc.maintenanceService,
+			}
+
+			result := r.ensureMaintenanceWindow(ctx, proj)
+
+			assert.Equal(t, tc.isOK, result.IsOk())
+
+			con, ok := ctx.GetCondition(api.MaintenanceWindowReadyType)
+			if tc.conditionMissing {
+				assert.False(t, ok)
+			} else {
+				assert.True(t, ok)
+				assert.Equal(t, tc.wantStatus, string(con.Status))
 			}
 		})
 	}
