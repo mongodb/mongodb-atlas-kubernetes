f705c4126fab9bae331956b5ac3b58a6a6a76ae6
Roo Thorp <roo.thorp@mongodb.com>
2024-10-22 17:16:44 +0100

    switch to versioned sdk

--- pkg/api/v1/project/maintenancewindow.go
+++ pkg/api/v1/project/maintenancewindow.go
@@ -1,9 +1,5 @@
 package project
 
-import (
-	"go.mongodb.org/atlas/mongodbatlas"
-)
-
 type MaintenanceWindow struct {
 	// Day of the week when you would like the maintenance window to start as a 1-based integer.
 	// Sunday 1, Monday 2, Tuesday 3, Wednesday 4, Thursday 5, Friday 6, Saturday 7
@@ -28,47 +24,4 @@ type MaintenanceWindow struct {
 	// Cannot be specified if startASAP is true
 	// +optional
 	Defer bool `json:"defer,omitempty"`
-}
-
-// ToAtlas converts the MaintenanceWindow to native Atlas client format.
-func (m MaintenanceWindow) ToAtlas() *mongodbatlas.MaintenanceWindow {
-	return &mongodbatlas.MaintenanceWindow{
-		DayOfWeek:            m.DayOfWeek,
-		HourOfDay:            &m.HourOfDay,
-		StartASAP:            &m.StartASAP,
-		NumberOfDeferrals:    0,
-		AutoDeferOnceEnabled: &m.AutoDefer,
-	}
-}
-
-// ************************************ Builder methods *************************************************
-// Note, that we don't use pointers here as the AtlasProject uses this without pointers
-
-func NewMaintenanceWindow() MaintenanceWindow {
-	return MaintenanceWindow{}
-}
-
-func (m MaintenanceWindow) WithDay(day int) MaintenanceWindow {
-	m.DayOfWeek = day
-	return m
-}
-
-func (m MaintenanceWindow) WithHour(hour int) MaintenanceWindow {
-	m.HourOfDay = hour
-	return m
-}
-
-func (m MaintenanceWindow) WithAutoDefer(autoDefer bool) MaintenanceWindow {
-	m.AutoDefer = autoDefer
-	return m
-}
-
-func (m MaintenanceWindow) WithStartASAP(startASAP bool) MaintenanceWindow {
-	m.StartASAP = startASAP
-	return m
-}
-
-func (m MaintenanceWindow) WithDefer(isDefer bool) MaintenanceWindow {
-	m.Defer = isDefer
-	return m
 }
