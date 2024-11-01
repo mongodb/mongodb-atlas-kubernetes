9c8d3d50cd2707e2c41d2441d3ee48e860629bea
Roo Thorp <roo.thorp@mongodb.com>
2024-11-01 10:28:02 +0000

    add translation layer

---
+++ internal/translation/encryptionatrest/conversion.go
@@ -0,0 +1,177 @@
+package encryptionatrest
+
+import (
+	"reflect"
+
+	"go.mongodb.org/atlas-sdk/v20231115008/admin"
+
+	akov2 "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1"
+)
+
+type EncryptionAtRest struct {
+	AWS   AwsKms
+	Azure AzureKeyVault
+	GCP   GoogleCloudKms
+}
+
+type AwsKms struct {
+	akov2.AwsKms
+	AccessKeyID                  string
+	SecretAccessKey              string
+	CustomerMasterKeyID          string
+	RoleID                       string
+	CloudProviderIntegrationRole string
+}
+
+func (a *AwsKms) SetSecrets(customerMasterKeyID, roleID string) {
+	a.CustomerMasterKeyID = customerMasterKeyID
+	a.RoleID = roleID
+}
+
+func (a *AwsKms) ToAtlas() *admin.AWSKMSConfiguration {
+	if a == nil {
+		return nil
+	}
+
+	result := &admin.AWSKMSConfiguration{
+		Enabled: a.AwsKms.Enabled,
+		Region:  &a.AwsKms.Region,
+		Valid:   a.AwsKms.Valid,
+
+		RoleId:              &a.RoleID,
+		CustomerMasterKeyID: &a.CustomerMasterKeyID,
+	}
+
+	if result.RoleId == nil && a.CloudProviderIntegrationRole != "" {
+		result.RoleId = &a.CloudProviderIntegrationRole
+	}
+
+	return result
+}
+
+type AzureKeyVault struct {
+	akov2.AzureKeyVault
+	SubscriptionID string
+	KeyVaultName   string
+	KeyIdentifier  string
+	Secret         string
+}
+
+func (az *AzureKeyVault) SetSecrets(subscriptionID, keyVaultName, keyIdentifier, secret string) {
+	az.SubscriptionID = subscriptionID
+	az.KeyVaultName = keyVaultName
+	az.KeyIdentifier = keyIdentifier
+	az.Secret = secret
+}
+
+func (az *AzureKeyVault) ToAtlas() *admin.AzureKeyVault {
+	if az == nil {
+		return nil
+	}
+
+	return &admin.AzureKeyVault{
+		Enabled:           az.AzureKeyVault.Enabled,
+		ClientID:          &az.AzureKeyVault.ClientID,
+		AzureEnvironment:  &az.AzureKeyVault.AzureEnvironment,
+		ResourceGroupName: &az.AzureKeyVault.ResourceGroupName,
+		TenantID:          &az.AzureKeyVault.TenantID,
+
+		SubscriptionID: &az.SubscriptionID,
+		KeyVaultName:   &az.KeyVaultName,
+		KeyIdentifier:  &az.KeyIdentifier,
+		Secret:         &az.Secret,
+	}
+}
+
+type GoogleCloudKms struct {
+	akov2.GoogleCloudKms
+	ServiceAccountKey    string
+	KeyVersionResourceID string
+}
+
+func (g *GoogleCloudKms) SetSecrets(serviceAccountKey, keyVersionResourceID string) {
+	g.ServiceAccountKey = serviceAccountKey
+	g.KeyVersionResourceID = keyVersionResourceID
+}
+
+func (g *GoogleCloudKms) ToAtlas() *admin.GoogleCloudKMS {
+	if g == nil {
+		return nil
+	}
+
+	return &admin.GoogleCloudKMS{
+		Enabled: g.GoogleCloudKms.Enabled,
+
+		ServiceAccountKey:    &g.ServiceAccountKey,
+		KeyVersionResourceID: &g.KeyVersionResourceID,
+	}
+}
+
+func NewEncryptionAtRest(project *akov2.AtlasProject) *EncryptionAtRest {
+	ear := &EncryptionAtRest{}
+	spec := project.Spec.EncryptionAtRest
+	if spec == nil {
+		return nil
+	}
+
+	if *spec.AwsKms.Enabled {
+		ear.AWS.AwsKms = spec.AwsKms
+		for _, role := range project.Status.CloudProviderIntegrations {
+			if role.ProviderName == "AWS" {
+				ear.AWS.CloudProviderIntegrationRole = role.RoleID
+			}
+		}
+	}
+	if *spec.AzureKeyVault.Enabled {
+		ear.Azure.AzureKeyVault = spec.AzureKeyVault
+	}
+	if *spec.GoogleCloudKms.Enabled {
+		ear.GCP.GoogleCloudKms = spec.GoogleCloudKms
+	}
+	return ear
+}
+
+func toAtlas(spec *EncryptionAtRest) *admin.EncryptionAtRest {
+	if spec == nil {
+		return nil
+	}
+
+	return &admin.EncryptionAtRest{
+		AwsKms:         spec.AWS.ToAtlas(),
+		AzureKeyVault:  spec.Azure.ToAtlas(),
+		GoogleCloudKms: spec.GCP.ToAtlas(),
+	}
+}
+
+func fromAtlas(ear *admin.EncryptionAtRest) *EncryptionAtRest {
+	out := &EncryptionAtRest{}
+	if ear.HasAwsKms() {
+		out.AWS.AwsKms = akov2.AwsKms{
+			Enabled: ear.AwsKms.Enabled,
+			Region:  ear.AwsKms.GetRegion(),
+			Valid:   ear.AwsKms.Valid,
+		}
+	}
+	if ear.HasAzureKeyVault() {
+		out.Azure.AzureKeyVault = akov2.AzureKeyVault{
+			Enabled:           ear.AzureKeyVault.Enabled,
+			AzureEnvironment:  ear.AzureKeyVault.GetAzureEnvironment(),
+			ClientID:          ear.AzureKeyVault.GetClientID(),
+			ResourceGroupName: ear.AzureKeyVault.GetResourceGroupName(),
+			TenantID:          ear.AzureKeyVault.GetTenantID(),
+			Valid:             ear.AzureKeyVault.Valid,
+		}
+	}
+	if ear.HasGoogleCloudKms() {
+		out.GCP.GoogleCloudKms = akov2.GoogleCloudKms{
+			Enabled: ear.GoogleCloudKms.Enabled,
+			Valid:   ear.GoogleCloudKms.Valid,
+		}
+	}
+	return out
+}
+
+func EqualSpecs(spec, atlas *EncryptionAtRest) bool {
+	// Retracted secrets mean that this will never be equal
+	return reflect.DeepEqual(atlas, spec)
+}
---
+++ internal/translation/encryptionatrest/encryptionatrest.go
@@ -0,0 +1,39 @@
+package encryptionatrest
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/atlas-sdk/v20231115008/admin"
+)
+
+type EncryptionAtRestService interface {
+	Get(context.Context, string) (*EncryptionAtRest, error)
+	Update(context.Context, string, EncryptionAtRest) error
+}
+
+type EncryptionAtRestAPI struct {
+	encryptionAtRestAPI admin.EncryptionAtRestUsingCustomerKeyManagementApi
+}
+
+func NewEncryptionAtRestAPI(api admin.EncryptionAtRestUsingCustomerKeyManagementApi) *EncryptionAtRestAPI {
+	return &EncryptionAtRestAPI{encryptionAtRestAPI: api}
+}
+
+func (e *EncryptionAtRestAPI) Get(ctx context.Context, projectID string) (*EncryptionAtRest, error) {
+	result, _, err := e.encryptionAtRestAPI.GetEncryptionAtRest(ctx, projectID).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get encryption at rest from Atlas: %w", err)
+	}
+
+	return fromAtlas(result), nil
+}
+
+func (e *EncryptionAtRestAPI) Update(ctx context.Context, projectID string, ear EncryptionAtRest) error {
+	_, _, err := e.encryptionAtRestAPI.UpdateEncryptionAtRest(ctx, projectID, toAtlas(&ear)).Execute()
+	if err != nil {
+		return fmt.Errorf("failed to update encryption at rest in Atlas: %w", err)
+	}
+
+	return nil
+}
