72f3aeced7b5997f0bdf6f3480bde668b030ef69
jose.vazquez <jose.vazquez@mongodb.com>
2024-11-11 15:59:55 +0100

    Improve contract test helper
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- test/helper/contract/contract.go
+++ test/helper/contract/contract.go
@@ -2,84 +2,111 @@ package contract
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
 	"time"
 
 	"go.mongodb.org/atlas-sdk/v20231115008/admin"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/stretchr/testify/require"
 
+	akov2 "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api/v1"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/control"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/utils"
 )
 
-const (
-	operatorInstallName = "test-atlas-operator"
-	akoTestNamespace    = "ako-test"
-)
-
-type ContractTest struct {
-	t            *testing.T
-	akoInstalled bool
-	namespace    string
-	resources    []client.Object
-
-	Ctx         context.Context
-	AtlasClient *admin.APIClient
-	K8sClient   client.Client
+type ContractHelper interface {
+	AtlasClient() *admin.APIClient
+	AddResources(ctx context.Context, timeout time.Duration, resources ...client.Object) error
+	ProjectID(ctx context.Context, projectName string) (string, error)
 }
 
-func (ct *ContractTest) cleanup() {
+type contractTest struct {
+	credentials bool
+	namespace   string
+	resources   []client.Object
+	k8sClient   client.Client
+	atlasClient *admin.APIClient
+}
+
+func (ct *contractTest) cleanup(ctx context.Context) error {
 	for i := len(ct.resources) - 1; i >= 0; i-- {
 		resource := ct.resources[i]
-		require.NoError(ct.t, ct.K8sClient.Delete(ct.Ctx, resource))
+		if err := ct.k8sClient.Delete(ctx, resource); err != nil {
+			return fmt.Errorf("failed to delete contract test pre-set resource: %w", err)
+		}
 	}
 	ct.resources = []client.Object{}
 	if ct.namespace != "" {
-		require.NoError(ct.t, ct.K8sClient.Delete(ct.Ctx, defaultNamespace(ct.namespace)))
+		if err := ct.k8sClient.Delete(ctx, defaultNamespace(ct.namespace)); err != nil {
+			return fmt.Errorf("failed to delete namespace %q: %w", ct.namespace, err)
+		}
 	}
+	return nil
 }
 
-func RunContractTest(t *testing.T, name string, ctFn func(ct *ContractTest)) {
-	t.Helper()
+func RunGoContractTest(ctx context.Context, t *testing.T, name string, contractTest func(ch ContractHelper)) {
 	if !control.Enabled("AKO_CONTRACT_TEST") {
 		t.Skip("Skipping contract test as AKO_CONTRACT_TEST is unset")
 		return
 	}
-	ctx := context.Background()
-	ct := &ContractTest{
-		t:            t,
-		Ctx:          ctx,
-		K8sClient:    mustCreateK8sClient(),
-		akoInstalled: false,
-		resources:    []client.Object{},
-		AtlasClient:  mustCreateVersionedAtlasClient(ctx),
-	}
-	defer ct.cleanup()
+	ct := newContractTest(ctx)
+	defer func() {
+		require.NoError(t, ct.cleanup(ctx))
+	}()
 	t.Run(name, func(t *testing.T) {
-		ctFn(ct)
+		contractTest(ct)
 	})
 }
 
-func (ct *ContractTest) AddResources(timeout time.Duration, resources ...client.Object) {
-	if !ct.akoInstalled {
-		require.NoError(ct.t, ensureTestAtlasOperator(akoTestNamespace))
-		require.NoError(ct.t, k8sRecreate(ct.Ctx, ct.K8sClient, globalSecret(akoTestNamespace)))
-		ct.akoInstalled = true
+func newContractTest(ctx context.Context) *contractTest {
+	return &contractTest{
+		k8sClient:   mustCreateK8sClient(),
+		credentials: false,
+		resources:   []client.Object{},
+		atlasClient: mustCreateVersionedAtlasClient(ctx),
+	}
+}
+
+func (ct *contractTest) AtlasClient() *admin.APIClient {
+	return ct.atlasClient
+}
+
+func (ct *contractTest) AddResources(ctx context.Context, timeout time.Duration, resources ...client.Object) error {
+	if !ct.credentials {
+		akoTestNamespace := os.Getenv("HELM_AKO_NAMESPACE")
+		if err := k8sRecreate(ctx, ct.k8sClient, globalSecret(akoTestNamespace)); err != nil {
+			return fmt.Errorf("failed to set AKO namespace: %w", err)
+		}
+		ct.credentials = true
 	}
 	if ct.namespace == "" {
 		ct.namespace = utils.RandomName("test-ns")
-		require.NoError(ct.t, ct.K8sClient.Create(ct.Ctx, defaultNamespace(ct.namespace)))
+		if err := ct.k8sClient.Create(ctx, defaultNamespace(ct.namespace)); err != nil {
+			return fmt.Errorf("failed to create test namespace: %w", err)
+		}
 	}
 	for _, resource := range resources {
 		resource.SetNamespace(ct.namespace)
-		require.NoError(ct.t, ct.K8sClient.Create(ct.Ctx, resource))
+		if err := ct.k8sClient.Create(ctx, resource); err != nil {
+			return fmt.Errorf("failed to create resource: %w", err)
+		}
 	}
 	ct.resources = append(ct.resources, resources...)
-	require.NoError(ct.t, waitForReadyStatus(ct.K8sClient, resources, timeout))
+	if err := waitForReadyStatus(ct.k8sClient, resources, timeout); err != nil {
+		return fmt.Errorf("failed to reach READY status: %w", err)
+	}
+	return nil
 }
 
-func (ct *ContractTest) Namespace() string {
-	return ct.namespace
+func (ct *contractTest) ProjectID(ctx context.Context, projectName string) (string, error) {
+	project := akov2.AtlasProject{}
+	key := types.NamespacedName{Namespace: ct.namespace, Name: projectName}
+	if err := ct.k8sClient.Get(ctx, key, &project); err != nil {
+		return "", fmt.Errorf("failed to get project ID: %w", err)
+	}
+	return project.Status.ID, nil
 }
