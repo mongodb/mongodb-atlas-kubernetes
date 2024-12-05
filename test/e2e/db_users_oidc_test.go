7f3df99af20eee112e06d999a7686d787dfe7366
Roo Thorp <roo.thorp@mongodb.com>
2024-12-05 02:52:56 +0000

    update e2e tests

--- test/e2e/db_users_oidc_test.go
+++ test/e2e/db_users_oidc_test.go
@@ -2,13 +2,15 @@ package e2e
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/featureflags"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/api"
@@ -19,13 +21,40 @@ import (
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/actions/deploy"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/config"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/data"
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/k8s"
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/model"
-	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/resources"
 )
 
-var _ = Describe("Operator to run db-user with the OIDC feature flags", Ordered, Label("users-oidc"), func() {
+var _ = Describe("Operator to run db-user with the OIDC feature flags disabled", Ordered, Label("users-oidc"), func() {
 	var testData *model.TestDataProvider
+
+	_ = BeforeEach(func() {
+		project := data.DefaultProject()
+
+		deployment := data.CreateBasicDeployment("dbusers-operator-global")
+
+		testData = model.DataProvider(
+			"dbusers-operator-global",
+			model.NewEmptyAtlasKeyType().UseDefaultFullAccess(),
+			30008,
+			[]func(*model.TestDataProvider){},
+		).WithProject(project).
+			WithInitialDeployments(deployment).
+			WithUsers(
+				data.BasicUser(
+					"reader1",
+					"reader1",
+					data.WithSecretRef("dbuser-secret-u1"),
+					data.WithReadWriteRole(),
+					data.WithNamespace(project.Namespace),
+					data.WithLabels([]common.LabelSpec{
+						{Key: "type", Value: "e2e-test"},
+						{Key: "context", Value: "cloud"},
+					}),
+				),
+			)
+
+		actions.CreateNamespaceAndSecrets(testData)
+	})
 
 	_ = AfterEach(func() {
 		if CurrentSpecReport().Failed() {
@@ -39,71 +68,14 @@ var _ = Describe("Operator to run db-user with the OIDC feature flags", Ordered,
 	})
 
 	It("Operator run on global namespace with the OIDC feature disabled", func() {
-		By("Setting up test data with the DB User OIDC disabled", func() {
-			project := data.DefaultProject()
-			project.Namespace = config.DefaultOperatorNS
-
-			deployment := data.CreateBasicDeployment("dbusers-operator-global")
-			deployment.Namespace = config.DefaultOperatorNS
-
-			testData = model.DataProvider(
-				"dbusers-operator-global",
-				model.NewEmptyAtlasKeyType().UseDefaultFullAccess(),
-				30008,
-				[]func(*model.TestDataProvider){},
-			).WithProject(project).
-				WithInitialDeployments(deployment).
-				WithUsers(
-					data.BasicUser(
-						"reader1",
-						"reader1",
-						data.WithSecretRef("dbuser-secret-u1"),
-						data.WithReadWriteRole(),
-						data.WithNamespace(config.DefaultOperatorNS),
-						data.WithLabels([]common.LabelSpec{
-							{Key: "type", Value: "e2e-test"},
-							{Key: "context", Value: "cloud"},
-						}),
-					),
-				)
-			testData.Resources.Namespace = config.DefaultOperatorNS
-		})
-
 		By("Running operator watching global namespace with OIDC disabled", func() {
-			Eventually(k8s.CreateNamespace(testData.Context, testData.K8SClient, config.DefaultOperatorNS)).WithTimeout(5 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
-			k8s.CreateDefaultSecret(testData.Context, testData.K8SClient, config.DefaultOperatorGlobalKey, config.DefaultOperatorNS)
-
-			mgr, err := k8s.BuildManager(&k8s.Config{
-				GlobalAPISecret: client.ObjectKey{
-					Namespace: config.DefaultOperatorNS,
-					Name:      config.DefaultOperatorGlobalKey,
-				},
-				WatchedNamespaces: map[string]bool{config.DefaultOperatorNS: true},
-				FeatureFlags:      featureflags.NewFeatureFlags(func() []string { return []string{} }),
-			})
-			Expect(err).NotTo(HaveOccurred())
-
-			go func(ctx context.Context) context.Context {
-				err := mgr.Start(ctx)
-				Expect(err).NotTo(HaveOccurred())
-				return ctx
-			}(testData.Context)
+			actions.ProjectCreationFlow(testData)
 		})
-		By("Creating project and database users resources", func() {
-			deploy.CreateProject(testData)
+
+		By("Creating database users resource", func() {
 			deploy.CreateUsers(testData)
-
-			Eventually(func(g Gomega) bool {
-				for i := range testData.Users {
-					dbUser := testData.Users[i]
-
-					g.Expect(testData.K8SClient.Get(testData.Context, client.ObjectKeyFromObject(dbUser), dbUser)).To(Succeed())
-					g.Expect(resources.CheckCondition(testData.K8SClient, dbUser, api.TrueCondition(api.ReadyType))).To(BeTrue())
-				}
-
-				return true
-			}).WithTimeout(5 * time.Minute).WithPolling(10 * time.Second).Should(BeTrue())
 		})
+
 		By("Try to enabled the OIDC feature for the user", func() {
 			currentUser := &akov2.AtlasDatabaseUser{}
 			Expect(testData.K8SClient.Get(context.Background(),
@@ -134,95 +106,98 @@ var _ = Describe("Operator to run db-user with the OIDC feature flags", Ordered,
 		})
 	})
 
-	// TODO: Enable this test as soon as API for configuring OpenID providers becomes available
-	// It("Operator run on global namespace with the OIDC feature enabled", func() {
-	// 	By("Setting up test data with the DB User OIDC enabled", func() {
-	// 		project := data.DefaultProject()
-	// 		project.Namespace = config.DefaultOperatorNS
+	It("Operator run on global namespace with the OIDC feature enabled", func() {
+		By("Running operator watching global namespace with OIDC enabled", func() {
+			os.Setenv(featureflags.FeatureOIDC, "true")
+			actions.ProjectCreationFlow(testData)
+		})
+		By("Creating database users resource", func() {
+			deploy.CreateUsers(testData)
+		})
+		By("Creating a FedAuth resource and verify it is ready", func() {
+			fedAuth := &akov2.AtlasFederatedAuth{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-fedauth", testData.Project.Name),
+					Namespace: testData.Resources.Namespace,
+				},
+				Spec: akov2.AtlasFederatedAuthSpec{
+					Enabled:                     true,
+					DataAccessIdentityProviders: &[]string{"abc"},
+					ConnectionSecretRef: common.ResourceRefNamespaced{
+						Namespace: testData.Resources.Namespace,
+						Name:      config.DefaultOperatorGlobalKey,
+					},
+				},
+			}
+			Expect(testData.K8SClient.Create(testData.Context, fedAuth)).Should(Succeed())
+			Eventually(func(g Gomega) {
+				currentFedAuth := &akov2.AtlasFederatedAuth{}
+				g.Expect(testData.K8SClient.Get(context.Background(),
+					types.NamespacedName{
+						Name:      fedAuth.Name,
+						Namespace: fedAuth.Namespace,
+					}, currentFedAuth)).NotTo(HaveOccurred())
+				for _, condition := range currentFedAuth.Status.Conditions {
+					if condition.Type == api.ReadyType {
+						g.Expect(condition.Status).Should(Equal(corev1.ConditionTrue))
+					}
+				}
+			}).WithTimeout(1 * time.Minute).WithPolling(20 * time.Second).Should(Succeed())
+		})
+		By("Try to enabled the OIDC Group feature for the user", func() {
+			currentUser := &akov2.AtlasDatabaseUser{}
+			Expect(testData.K8SClient.Get(context.Background(),
+				types.NamespacedName{
+					Name:      testData.Users[0].Name,
+					Namespace: testData.Users[0].Namespace,
+				}, currentUser)).NotTo(HaveOccurred())
 
-	// 		deployment := data.CreateBasicDeployment("dbusers-operator-global")
-	// 		deployment.Namespace = config.DefaultOperatorNS
+			currentUser.Spec.OIDCAuthType = "IDP_GROUP"
+			currentUser.Spec.PasswordSecret = nil
+			Expect(testData.K8SClient.Update(context.Background(), currentUser)).NotTo(HaveOccurred())
+		})
 
-	// 		testData = model.DataProvider(
-	// 			"dbusers-operator-global",
-	// 			model.NewEmptyAtlasKeyType().UseDefaultFullAccess(),
-	// 			30008,
-	// 			[]func(*model.TestDataProvider){},
-	// 		).WithProject(project).
-	// 			WithInitialDeployments(deployment).
-	// 			WithUsers(
-	// 				data.BasicUser(
-	// 					"reader1",
-	// 					"reader1",
-	// 					data.WithReadWriteRole(),
-	// 					data.WithNamespace(config.DefaultOperatorNS),
-	// 					data.WithOIDCEnabled(),
-	// 				),
-	// 			)
-	// 		testData.Resources.Namespace = config.DefaultOperatorNS
-	// 	})
+		By("Verify if user is ready", func() {
+			currentUser := &akov2.AtlasDatabaseUser{}
+			Eventually(func(g Gomega) {
+				g.Expect(testData.K8SClient.Get(context.Background(),
+					types.NamespacedName{
+						Name:      testData.Users[0].Name,
+						Namespace: testData.Users[0].Namespace,
+					}, currentUser)).NotTo(HaveOccurred())
+				for _, condition := range currentUser.Status.Conditions {
+					if condition.Type == api.ReadyType {
+						g.Expect(condition.Status).Should(Equal(corev1.ConditionTrue))
+					}
+				}
+			}).WithTimeout(1 * time.Minute).WithPolling(20 * time.Second).Should(Succeed())
+		})
+		By("Try to enabled the OIDC User feature for the user", func() {
+			currentUser := &akov2.AtlasDatabaseUser{}
+			Expect(testData.K8SClient.Get(context.Background(),
+				types.NamespacedName{
+					Name:      testData.Users[0].Name,
+					Namespace: testData.Users[0].Namespace,
+				}, currentUser)).NotTo(HaveOccurred())
 
-	// 	By("Running operator watching global namespace with OIDC enabled", func() {
-	// 		Eventually(k8s.CreateNamespace(testData.Context, testData.K8SClient, config.DefaultOperatorNS)).WithTimeout(5 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
-	// 		k8s.CreateDefaultSecret(testData.Context, testData.K8SClient, config.DefaultOperatorGlobalKey, config.DefaultOperatorNS)
-	// 		k8s.CreateNamespace(testData.Context, testData.K8SClient, secondNamespace)
+			currentUser.Spec.OIDCAuthType = "USER"
+			Expect(testData.K8SClient.Update(context.Background(), currentUser)).NotTo(HaveOccurred())
+		})
 
-	// 		mgr, err := k8s.BuildManager(&k8s.Config{
-	// 			GlobalAPISecret: client.ObjectKey{
-	// 				Namespace: config.DefaultOperatorNS,
-	// 				Name:      config.DefaultOperatorGlobalKey,
-	// 			},
-	// 			FeatureFlags: featureflags.NewFeatureFlags(func() []string { return []string{featureflags.FeatureOIDC} }),
-	// 		})
-	// 		Expect(err).NotTo(HaveOccurred())
-
-	// 		go func(ctx context.Context) context.Context {
-	// 			err := mgr.Start(ctx)
-	// 			Expect(err).NotTo(HaveOccurred())
-	// 			return ctx
-	// 		}(testData.Context)
-	// 	})
-	// 	By("Creating project and database users resources", func() {
-	// 		deploy.CreateProject(testData)
-	// 		deploy.CreateUsers(testData)
-
-	// 		Eventually(func(g Gomega) bool {
-	// 			for i := range testData.Users {
-	// 				dbUser := testData.Users[i]
-
-	// 				g.Expect(testData.K8SClient.Get(testData.Context, client.ObjectKeyFromObject(dbUser), dbUser)).To(Succeed())
-	// 				g.Expect(resources.CheckCondition(testData.K8SClient, dbUser, status.TrueCondition(api.ReadyType))).To(BeTrue())
-	// 			}
-
-	// 			return true
-	// 		}).WithTimeout(5 * time.Minute).WithPolling(10 * time.Second).Should(BeTrue())
-	// 	})
-	// 	By("Try to enabled the OIDC feature for the user", func() {
-	// 		currentUser := &akov2.AtlasDatabaseUser{}
-	// 		Expect(testData.K8SClient.Get(context.Background(),
-	// 			types.NamespacedName{
-	// 				Name:      testData.Users[0].Name,
-	// 				Namespace: testData.Users[0].Namespace,
-	// 			}, currentUser)).NotTo(HaveOccurred())
-
-	// 		currentUser.Spec.OIDCAuthType = "IDP_GROUP"
-	// 		Expect(testData.K8SClient.Update(context.Background(), currentUser)).NotTo(HaveOccurred())
-	// 	})
-
-	// 	By("Verify if user is ready", func() {
-	// 		currentUser := &akov2.AtlasDatabaseUser{}
-	// 		Eventually(func(g Gomega) {
-	// 			g.Expect(testData.K8SClient.Get(context.Background(),
-	// 				types.NamespacedName{
-	// 					Name:      testData.Users[0].Name,
-	// 					Namespace: testData.Users[0].Namespace,
-	// 				}, currentUser)).NotTo(HaveOccurred())
-	// 			for _, condition := range currentUser.Status.Conditions {
-	// 				if condition.Type == api.ReadyType {
-	// 					g.Expect(condition.Status).Should(Equal(corev1.ConditionTrue))
-	// 				}
-	// 			}
-	// 		}).WithTimeout(1 * time.Minute).WithPolling(20 * time.Second).Should(Succeed())
-	// 	})
-	// })
+		By("Verify if user is ready", func() {
+			currentUser := &akov2.AtlasDatabaseUser{}
+			Eventually(func(g Gomega) {
+				g.Expect(testData.K8SClient.Get(context.Background(),
+					types.NamespacedName{
+						Name:      testData.Users[0].Name,
+						Namespace: testData.Users[0].Namespace,
+					}, currentUser)).NotTo(HaveOccurred())
+				for _, condition := range currentUser.Status.Conditions {
+					if condition.Type == api.ReadyType {
+						g.Expect(condition.Status).Should(Equal(corev1.ConditionTrue))
+					}
+				}
+			}).WithTimeout(1 * time.Minute).WithPolling(20 * time.Second).Should(Succeed())
+		})
+	})
 })
