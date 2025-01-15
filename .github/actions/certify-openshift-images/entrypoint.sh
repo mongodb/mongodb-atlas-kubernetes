c1fbf14e5d9b9fbd8a72298bcb0bffbba5f9d63f
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2025-01-15 12:07:34 +0100

    preflight: check manifest image only (#2047)

--- .github/actions/certify-openshift-images/entrypoint.sh
+++ .github/actions/certify-openshift-images/entrypoint.sh
@@ -4,25 +4,15 @@ set -eou pipefail
 
 docker login -u mongodb+mongodb_atlas_kubernetes -p "${REGISTRY_PASSWORD}" "${REGISTRY}"
 
-DIGESTS=$(docker manifest inspect "${REGISTRY}/${REPOSITORY}:${VERSION}" | jq -r '.manifests[] | select(.platform.os!="unknown") | .digest')
-mapfile -t PLATFORMS < <(docker manifest inspect "${REGISTRY}/${REPOSITORY}:${VERSION}" | jq -r '.manifests[] | select(.platform.os!="unknown") | .platform.architecture')
-
 submit_flag=--submit
 if [ "${SUBMIT}" == "false" ]; then
   submit_flag=
 fi
 
-INDEX=0
-for DIGEST in $DIGESTS; do
-    echo "Check and Submit result to RedHat Connect"
-    # Send results to RedHat if preflight finished wthout errors
-    preflight check container "${REGISTRY}/${REPOSITORY}@${DIGEST}" \
-      --artifacts "${DIGEST}" \
-      --platform "${PLATFORMS[$INDEX]}" \
-      --pyxis-api-token="${RHCC_TOKEN}" \
-      --certification-project-id="${RHCC_PROJECT}" \
-      --docker-config="${HOME}/.docker/config.json" \
-      ${submit_flag}
-
-  (( INDEX++ )) || true
-done
+echo "Check and Submit result to RedHat Connect"
+# Send results to RedHat if preflight finished wthout errors
+preflight check container "${REGISTRY}/${REPOSITORY}:${VERSION}" \
+  --pyxis-api-token="${RHCC_TOKEN}" \
+  --certification-project-id="${RHCC_PROJECT}" \
+  --docker-config="${HOME}/.docker/config.json" \
+  ${submit_flag}
