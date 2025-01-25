1e1dcfa5bc45813482de14b5342e963b834cf567
jose.vazquez <jose.vazquez@mongodb.com>
2025-01-25 12:23:31 +0100

    add controller draft
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

---
+++ internal/controller/atlasnetworkcontainer_controller_test.go
@@ -0,0 +1,24 @@
+/*
+Copyright (C) MongoDB, Inc. 2020-present.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+package controller
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("AtlasNetworkContainer Controller", func() {
+	Context("When reconciling a resource", func() {
+
+		It("should successfully reconcile the resource", func() {
+
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})
