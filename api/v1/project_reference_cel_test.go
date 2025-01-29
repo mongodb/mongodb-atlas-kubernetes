19f1e990931ef523331819aa5ced8cb896e3b85d
josvaz <jose.vazquez@mongodb.com>
2025-01-29 14:49:21 +0100

    Refactor dual ref CEL validations (#2071)
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- api/v1/project_reference_cel_test.go
+++ api/v1/project_reference_cel_test.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,119 +14,156 @@ import (
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/cel"
 )
 
-type CELTestObjectGenFunc func(*ProjectDualReference) AtlasCustomResource
+const (
+	crdBasePath = "../../config/crd/bases/"
+)
 
-type celValidationTestCase struct {
+type projectReferrerObject interface {
+	runtime.Object
+	ProjectDualRef() *ProjectDualReference
+}
+
+var dualRefCRDs = []struct {
+	obj      projectReferrerObject
+	filename string
+}{
+	{
+		obj:      &AtlasDatabaseUser{},
+		filename: "atlas.mongodb.com_atlasdatabaseusers.yaml",
+	},
+	{
+		obj:      &AtlasDeployment{},
+		filename: "atlas.mongodb.com_atlasdeployments.yaml",
+	},
+	{
+		obj:      &AtlasIPAccessList{},
+		filename: "atlas.mongodb.com_atlasipaccesslists.yaml",
+	},
+	{
+		obj:      &AtlasPrivateEndpoint{},
+		filename: "atlas.mongodb.com_atlasprivateendpoints.yaml",
+	},
+	{
+		obj:      &AtlasIPAccessList{},
+		filename: "atlas.mongodb.com_atlasipaccesslists.yaml",
+	},
+}
+
+var testCases = []struct {
 	title          string
-	obj, old       AtlasCustomResource
+	ref            *ProjectDualReference
 	expectedErrors []string
+}{
+	{
+		title:          "no project reference is set",
+		ref:            &ProjectDualReference{},
+		expectedErrors: []string{"spec: Invalid value: \"object\": must define only one project reference through externalProjectRef or projectRef"},
+	},
+	{
+		title: "both project references are set",
+		ref: &ProjectDualReference{
+			ProjectRef: &common.ResourceRefNamespaced{
+				Name: "my-project",
+			},
+			ExternalProjectRef: &ExternalProjectReference{
+				ID: "my-project-id",
+			},
+		},
+		expectedErrors: []string{
+			"spec: Invalid value: \"object\": must define only one project reference through externalProjectRef or projectRef",
+			"spec: Invalid value: \"object\": must define a local connection secret when referencing an external project",
+		},
+	},
+	{
+		title: "external project references is set",
+		ref: &ProjectDualReference{
+			ExternalProjectRef: &ExternalProjectReference{
+				ID: "my-project-id",
+			},
+		},
+		expectedErrors: []string{
+			"spec: Invalid value: \"object\": must define a local connection secret when referencing an external project",
+		},
+	},
+	{
+		title: "kubernetes project references is set",
+		ref: &ProjectDualReference{
+			ProjectRef: &common.ResourceRefNamespaced{
+				Name: "my-project",
+			},
+		},
+	},
+	{
+		title: "external project references is set without connection secret",
+		ref: &ProjectDualReference{
+			ExternalProjectRef: &ExternalProjectReference{
+				ID: "my-project-id",
+			},
+		},
+		expectedErrors: []string{
+			"spec: Invalid value: \"object\": must define a local connection secret when referencing an external project",
+		},
+	},
+	{
+		title: "external project references is set with connection secret",
+		ref: &ProjectDualReference{
+			ExternalProjectRef: &ExternalProjectReference{
+				ID: "my-project-id",
+			},
+			ConnectionSecret: &api.LocalObjectReference{
+				Name: "my-dbuser-connection-secret",
+			},
+		},
+	},
+	{
+		title: "kubernetes project references is set without connection secret",
+		ref: &ProjectDualReference{
+			ProjectRef: &common.ResourceRefNamespaced{
+				Name: "my-project",
+			},
+		},
+	},
+	{
+		title: "kubernetes project references is set with connection secret",
+		ref: &ProjectDualReference{
+			ProjectRef: &common.ResourceRefNamespaced{
+				Name: "my-project",
+			},
+			ConnectionSecret: &api.LocalObjectReference{
+				Name: "my-dbuser-connection-secret",
+			},
+		},
+	},
 }
 
-func launchProjectRefCELTests(t *testing.T, genObjFunc CELTestObjectGenFunc, crdPath string) {
-	testCases := testCasesFor(genObjFunc)
-	for _, tc := range testCases {
-		t.Run(tc.title, func(t *testing.T) {
-			unstructuredObject, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&tc.obj)
-			require.NoError(t, err)
+func TestProjectDualReferenceCELValidations(t *testing.T) {
+	for _, dualRef := range dualRefCRDs {
+		for _, tc := range testCases {
+			title := fmt.Sprintf("%T %s", dualRef.obj, tc.title)
+			obj := dualRef.obj.DeepCopyObject()
+			pdr, ok := obj.(projectReferrerObject)
+			require.True(t, ok)
+			setDualRef(pdr.ProjectDualRef(), tc.ref)
+			old := dualRef.obj.DeepCopyObject()
+			crdPath := filepath.Join(crdBasePath, dualRef.filename)
+			t.Run(title, func(t *testing.T) {
+				unstructuredObject, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&obj)
+				require.NoError(t, err)
 
-			unstructuredOldObject, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&tc.old)
-			require.NoError(t, err)
+				unstructuredOldObject, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&old)
+				require.NoError(t, err)
 
-			validator, err := cel.VersionValidatorFromFile(t, crdPath, "v1")
-			assert.NoError(t, err)
-			errs := validator(unstructuredObject, unstructuredOldObject)
+				validator, err := cel.VersionValidatorFromFile(t, crdPath, "v1")
+				assert.NoError(t, err)
+				errs := validator(unstructuredObject, unstructuredOldObject)
 
-			require.Equal(t, len(tc.expectedErrors), len(errs))
+				require.Equal(t, len(tc.expectedErrors), len(errs))
 
-			for i, err := range errs {
-				assert.Equal(t, tc.expectedErrors[i], err.Error())
-			}
-		})
-	}
-}
-
-func testCasesFor(genObjFunc CELTestObjectGenFunc) []celValidationTestCase {
-	return []celValidationTestCase{
-		{
-			title:          "no project reference is set",
-			obj:            genObjFunc(nil),
-			expectedErrors: []string{"spec: Invalid value: \"object\": must define only one project reference through externalProjectRef or projectRef"},
-		},
-		{
-			title: "both project references are set",
-			obj: genObjFunc(&ProjectDualReference{
-				ProjectRef: &common.ResourceRefNamespaced{
-					Name: "my-project",
-				},
-				ExternalProjectRef: &ExternalProjectReference{
-					ID: "my-project-id",
-				},
-			}),
-			expectedErrors: []string{
-				"spec: Invalid value: \"object\": must define only one project reference through externalProjectRef or projectRef",
-				"spec: Invalid value: \"object\": must define a local connection secret when referencing an external project",
-			},
-		},
-		{
-			title: "external project references is set",
-			obj: genObjFunc(&ProjectDualReference{
-				ExternalProjectRef: &ExternalProjectReference{
-					ID: "my-project-id",
-				},
-			}),
-			expectedErrors: []string{
-				"spec: Invalid value: \"object\": must define a local connection secret when referencing an external project",
-			},
-		},
-		{
-			title: "kubernetes project references is set",
-			obj: genObjFunc(&ProjectDualReference{
-				ProjectRef: &common.ResourceRefNamespaced{
-					Name: "my-project",
-				},
-			}),
-		},
-		{
-			title: "external project references is set without connection secret",
-			obj: genObjFunc(&ProjectDualReference{
-				ExternalProjectRef: &ExternalProjectReference{
-					ID: "my-project-id",
-				},
-			}),
-			expectedErrors: []string{
-				"spec: Invalid value: \"object\": must define a local connection secret when referencing an external project",
-			},
-		},
-		{
-			title: "external project references is set with connection secret",
-			obj: genObjFunc(&ProjectDualReference{
-				ExternalProjectRef: &ExternalProjectReference{
-					ID: "my-project-id",
-				},
-				ConnectionSecret: &api.LocalObjectReference{
-					Name: "my-dbuser-connection-secret",
-				},
-			}),
-		},
-		{
-			title: "kubernetes project references is set without connection secret",
-			obj: genObjFunc(&ProjectDualReference{
-				ProjectRef: &common.ResourceRefNamespaced{
-					Name: "my-project",
-				},
-			}),
-		},
-		{
-			title: "kubernetes project references is set with connection secret",
-			obj: genObjFunc(&ProjectDualReference{
-				ProjectRef: &common.ResourceRefNamespaced{
-					Name: "my-project",
-				},
-				ConnectionSecret: &api.LocalObjectReference{
-					Name: "my-dbuser-connection-secret",
-				},
-			}),
-		},
+				for i, err := range errs {
+					assert.Equal(t, tc.expectedErrors[i], err.Error())
+				}
+			})
+		}
 	}
 }
 
