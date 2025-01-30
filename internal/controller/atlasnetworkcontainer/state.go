3d95daf406ee57f7acd442b1059d30e7a41de966
jose.vazquez <jose.vazquez@mongodb.com>
2025-01-30 14:12:05 +0100

    Cover finalizers valdation in unit tests

--- internal/controller/atlasnetworkcontainer/state.go
+++ internal/controller/atlasnetworkcontainer/state.go
@@ -99,12 +99,12 @@ func (r *AtlasNetworkContainerReconciler) create(workflowCtx *workflow.Context, 
 		wrappedErr := fmt.Errorf("failed to create container: %w", err)
 		return r.terminate(workflowCtx, req.networkContainer, workflow.NetworkContainerNotConfigured, wrappedErr), nil
 	}
-	return r.inProgress(workflowCtx, createdContainer)
+	return r.inProgress(workflowCtx, req.networkContainer, createdContainer)
 }
 
 func (r *AtlasNetworkContainerReconciler) sync(workflowCtx *workflow.Context, req *reconcileRequest, container *networkcontainer.NetworkContainer) (ctrl.Result, error) {
 	if !container.Provisioned {
-		return r.inProgress(workflowCtx, container)
+		return r.inProgress(workflowCtx, req.networkContainer, container)
 	}
 	cfg := networkcontainer.NewNetworkContainerConfig(
 		req.networkContainer.Spec.Provider, &req.networkContainer.Spec.AtlasNetworkContainerConfig)
@@ -120,7 +120,7 @@ func (r *AtlasNetworkContainerReconciler) update(workflowCtx *workflow.Context, 
 		wrappedErr := fmt.Errorf("failed to update container: %w", err)
 		return r.terminate(workflowCtx, req.networkContainer, workflow.NetworkContainerNotConfigured, wrappedErr), nil
 	}
-	return r.inProgress(workflowCtx, updatedContainer)
+	return r.inProgress(workflowCtx, req.networkContainer, updatedContainer)
 }
 
 func (r *AtlasNetworkContainerReconciler) delete(workflowCtx *workflow.Context, req *reconcileRequest, container *networkcontainer.NetworkContainer) (ctrl.Result, error) {
