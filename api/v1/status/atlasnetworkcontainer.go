0454fcb41d2668130ba4fd6588b776c5013756b4
jose.vazquez <jose.vazquez@mongodb.com>
2025-02-12 09:15:37 +0100

    Fix container CRD status

--- api/v1/status/atlasnetworkcontainer.go
+++ api/v1/status/atlasnetworkcontainer.go
@@ -12,6 +12,39 @@ type AtlasNetworkContainerStatus struct {
 
 	// Provisioned is true when clusters have been deployed to the container
 	Provisioned bool `json:"provisioned,omitempty"`
+
+	// AWSStatus contains AWS only related status information
+	AWSStatus *AWSContainerStatus `json:"awsStatus,omitempty"`
+
+	// AzureStatus contains Azure only related status information
+	AzureStatus *AzureContainerStatus `json:"azureStatus,omitempty"`
+
+	// GCPStatus contains GCP only related status information
+	GCPStatus *GCPContainerStatus `json:"gcpStatus,omitempty"`
+}
+
+// AWSContainerStatus contains AWS only related status information
+type AWSContainerStatus struct {
+	// VpcID is AWS VPC id on the Atlas side
+	VpcID string `json:"vpcId,omitempty"`
+}
+
+// AzureContainerStatus contains Azure only related status information
+type AzureContainerStatus struct {
+	// AzureSubscriptionID is Azure Subscription id on the Atlas side
+	AzureSubscriptionID string `json:"azureSubscriptionIDpcId,omitempty"`
+
+	// VnetName is Azure network on the Atlas side
+	VnetName string `json:"vNetName,omitempty"`
+}
+
+// GCPContainerStatus contains GCP only related status information
+type GCPContainerStatus struct {
+	// GCPProjectID is GCP project on the Atlas side
+	GCPProjectID string `json:"gcpProjectID,omitempty"`
+
+	// NetworkName is GCP network on the Atlas side
+	NetworkName string `json:"networkName,omitempty"`
 }
 
 // +kubebuilder:object:generate=false
