6770362814d9d397c7afe522321eae15aed4733b
jose.vazquez <jose.vazquez@mongodb.com>
2025-02-24 15:40:25 +0100

    Move CEL defs

--- api/v1/atlasnetworkpeering_types.go
+++ api/v1/atlasnetworkpeering_types.go
@@ -54,12 +54,11 @@ type AtlasNetworkPeeringList struct {
 	Items           []AtlasNetworkPeering `json:"items"`
 }
 
+// AtlasNetworkPeeringSpec defines the desired state of AtlasNetworkPeering
 // +kubebuilder:validation:XValidation:rule="(has(self.externalProjectRef) && !has(self.projectRef)) || (!has(self.externalProjectRef) && has(self.projectRef))",message="must define only one project reference through externalProjectRef or projectRef"
 // +kubebuilder:validation:XValidation:rule="(has(self.externalProjectRef) && has(self.connectionSecret)) || !has(self.externalProjectRef)",message="must define a local connection secret when referencing an external project"
 // +kubebuilder:validation:XValidation:rule="(has(self.containerRef.name) && !has(self.containerRef.id)) || (!has(self.containerRef.name) && has(self.containerRef.id))",message="must either have a container Atlas id or Kubernetes name, but not both (or neither)"
 // +kubebuilder:validation:XValidation:rule="(self.id == oldSelf.id) || (!has(self.id) && !has(oldSelf.id))",message="id is immutable"
-
-// AtlasNetworkPeeringSpec defines the desired state of AtlasNetworkPeering
 type AtlasNetworkPeeringSpec struct {
 	ProjectDualReference `json:",inline"`
 
