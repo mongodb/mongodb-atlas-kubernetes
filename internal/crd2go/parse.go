54026502747362b7d7590b1b3d71ee1aab95a9e9
jose.vazquez <jose.vazquez@mongodb.com>
2025-04-11 19:11:13 +0200

    broken use of apiextensions.CustomResourceDefinition

--- internal/crd2go/parse.go
+++ internal/crd2go/parse.go
@@ -1,71 +1,21 @@
 package crd2go
 
 import (
-	"errors"
 	"fmt"
 	"io"
 
 	"gopkg.in/yaml.v3"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 )
 
-var (
-	// ErrEmptyDoc fails when an non empty YAML object was expected
-	ErrEmptyDoc = errors.New("empty document")
+func ParseCRD(r io.Reader) (*apiextensions.CustomResourceDefinition, error) {
+	crd := apiextensions.CustomResourceDefinition{}
 
-	// ErrNotAnObject is emited when the YAMl was suposed to contain an object
-	ErrNotAnObject = errors.New("not a YAML object")
-)
-
-type CRD struct {
-	APIVersion string   `yaml:"apiVersion"`
-	Kind       string   `yaml:"kind"`
-	Metadata   Metadata `yaml:"metadata"`
-	Spec       Spec     `yaml:"spec"`
-}
-
-type Metadata struct {
-	Name string `yaml:"name"`
-}
-
-type Spec struct {
-	Group    string    `yaml:"group"`
-	Scope    string    `yaml:"scope"`
-	Names    SpecNames `yaml:"names"`
-	Versions []Version `yaml:"versions"`
-}
-
-type SpecNames struct {
-	Kind     string `yaml:"kind"`
-	ListKind string `yaml:"listKind"`
-}
-
-type Version struct {
-	Name         string         `yaml:"name"`
-	Schema       VersionSchema  `yaml:"schema"`
-	Served       bool           `yaml:"served"`
-	Storage      bool           `yaml:"storage"`
-	Subresources map[string]any `yaml:"subresources"`
-}
-
-type VersionSchema struct {
-	OpenAPIV3Schema OpenAPISchema `yaml:"openAPIV3Schema"`
-}
-
-type OpenAPISchema struct {
-	Type        string                   `yaml:"type"`
-	Properties  map[string]OpenAPISchema `yaml:"properties"`
-	Items       *OpenAPISchema           `yaml:"items,omitempty"`
-	Required    *[]string                `yaml:"required,omitempty"`
-	Title       *string                  `yaml:"title,omitempty"`
-	Description *string                  `yaml:"description,omitempty"`
-	MinLength   *int                     `yaml:"minLength,omitempty"`
-	MaxLength   *int                     `yaml:"maxLength,omitempty"`
-	Format      *string                  `yaml:"format,omitempty"`
-}
-
-func ParseCRD(r io.Reader) (*CRD, error) {
-	crd := CRD{}
-	err := yaml.NewDecoder(r).Decode(&crd)
+	yml, err := io.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read all YAML input: %w", err)
+	}
+	err = yaml.Unmarshal(yml, &crd)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode CRD YAML: %w", err)
 	}
--- internal/crd2go/parse_test.go
+++ internal/crd2go/parse_test.go
@@ -7,6 +7,9 @@ import (
 	"github.com/josvazg/crd2go/internal/crd2go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 //go:embed samples/*
@@ -19,104 +22,110 @@ func TestParseCRD(t *testing.T) {
 
 	crd, err := crd2go.ParseCRD(f)
 	require.NoError(t, err)
-	want := &crd2go.CRD{
-		APIVersion: "apiextensions.k8s.io/v1",
-		Kind:       "CustomResourceDefinition",
-		Metadata: crd2go.Metadata{
+	want := expectedCRD()
+	require.NotNil(t, crd)
+	assert.Equal(t, want, crd)
+}
+
+func expectedCRD() *apiextensions.CustomResourceDefinition {
+	return &apiextensions.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apiextensions.k8s.io/v1",
+			Kind:       "CustomResourceDefinition",
+		},
+		ObjectMeta: metav1.ObjectMeta{
 			Name: "groups.atlas.generated.mongodb.com",
 		},
-		Spec: crd2go.Spec{
+		Spec: apiextensions.CustomResourceDefinitionSpec{
 			Group: "atlas.generated.mongodb.com",
 			Scope: "Namespaced",
-			Names: crd2go.SpecNames{
+			Names: apiextensions.CustomResourceDefinitionNames{
 				Kind:     "Group",
 				ListKind: "GroupList",
 			},
-			Versions: []crd2go.Version{
+			Versions: []apiextensions.CustomResourceDefinitionVersion{
 				{
 					Name:    "v1",
 					Served:  true,
 					Storage: true,
-					Schema: crd2go.VersionSchema{
-						OpenAPIV3Schema: crd2go.OpenAPISchema{
+					Schema: &apiextensions.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextensions.JSONSchemaProps{
 							Type: "object",
-							Properties: map[string]crd2go.OpenAPISchema{
+							Properties: map[string]apiextensions.JSONSchemaProps{
 								"spec": {
 									Type: "object",
-									Properties: map[string]crd2go.OpenAPISchema{
+									Properties: map[string]apiextensions.JSONSchemaProps{
 										"v20231115": {
 											Type: "object",
-											Properties: map[string]crd2go.OpenAPISchema{
+											Properties: map[string]apiextensions.JSONSchemaProps{
 												"parameters": {
 													Type: "object",
-													Properties: map[string]crd2go.OpenAPISchema{
+													Properties: map[string]apiextensions.JSONSchemaProps{
 														"projectOwnerId": {
 															Type:        "string",
-															Description: ptr(`Unique 24-hexadecimal digit string that identifies the MongoDB Cloud user to whom to grant the Project Owner role on the specified project. If you set this parameter, it overrides the default value of the oldest Organization Owner.`),
-															MaxLength:   ptr(24),
-															MinLength:   ptr(24),
+															Description: `Unique 24-hexadecimal digit string that identifies the MongoDB Cloud user to whom to grant the Project Owner role on the specified project. If you set this parameter, it overrides the default value of the oldest Organization Owner.`,
+															MaxLength:   ptr(int64(24)),
+															MinLength:   ptr(int64(24)),
 														},
 													},
 												},
 												"entry": {
 													Type: "object",
-													Properties: map[string]crd2go.OpenAPISchema{
+													Properties: map[string]apiextensions.JSONSchemaProps{
 														"name": {
-															Description: ptr(`Human-readable label that identifies the project included in the MongoDB Cloud organization.`),
-															MaxLength:   ptr(64),
-															MinLength:   ptr(1),
+															Description: `Human-readable label that identifies the project included in the MongoDB Cloud organization.`,
+															MaxLength:   ptr(int64(64)),
+															MinLength:   ptr(int64(1)),
 															Type:        "string",
 														},
 														"orgId": {
-															Description: ptr(`Unique 24-hexadecimal digit string that identifies the MongoDB Cloud organization to which the project belongs.`),
-															MaxLength:   ptr(24),
-															MinLength:   ptr(24),
+															Description: `Unique 24-hexadecimal digit string that identifies the MongoDB Cloud organization to which the project belongs.`,
+															MaxLength:   ptr(int64(24)),
+															MinLength:   ptr(int64(24)),
 															Type:        "string",
 														},
 														"regionUsageRestrictions": {
-															Description: ptr(`Applies to Atlas for Government only.
-
-In Commercial Atlas, this field will be rejected in requests and missing in responses.
-
-This field sets restrictions on available regions in the project.
-
-| Value                             | Available Regions |
-|-----------------------------------|------------|
-| ` + "`COMMERCIAL_FEDRAMP_REGIONS_ONLY` | Only allows deployments in FedRAMP Moderate regions.|" + `
-| ` + "`GOV_REGIONS_ONLY`                | Only allows deployments in GovCloud regions.|"),
-
+															Description: `Applies to Atlas for Government only.
+	In Commercial Atlas, this field will be rejected in requests and missing in responses.
+	This field sets restrictions on available regions in the project.
+	| Value                             | Available Regions |
+	|-----------------------------------|------------|
+	| ` + "`COMMERCIAL_FEDRAMP_REGIONS_ONLY` | Only allows deployments in FedRAMP Moderate regions.|" + `
+	| ` + "`GOV_REGIONS_ONLY`                | Only allows deployments in GovCloud regions.|",
 															Type: "string",
 														},
 														"tags": {
-															Description: ptr("List that contains key-value pairs between 1 to 255 characters in length for tagging and categorizing the project."),
+															Description: "List that contains key-value pairs between 1 to 255 characters in length for tagging and categorizing the project.",
 															Type:        "array",
-															Items: &crd2go.OpenAPISchema{
-																Description: ptr(`Key-value pair that tags and categorizes a MongoDB Cloud organization, project, or cluster. For example, ` + "`environment : production`."),
-																Title:       ptr("Resource Tag"),
-																Type:        "object",
-																Properties: map[string]crd2go.OpenAPISchema{
-																	"key": {
-																		Description: ptr(`Constant that defines the set of the tag. For example, ` + "`environment` in the `environment : production` tag."),
-																		MaxLength:   ptr(255),
-																		MinLength:   ptr(1),
-																		Type:        "string",
+															Items: &apiextensions.JSONSchemaPropsOrArray{
+																Schema: &apiextensions.JSONSchemaProps{
+																	Description: `Key-value pair that tags and categorizes a MongoDB Cloud organization, project, or cluster. For example, ` + "`environment : production`.",
+																	Title:       "Resource Tag",
+																	Type:        "object",
+																	Properties: map[string]apiextensions.JSONSchemaProps{
+																		"key": {
+																			Description: `Constant that defines the set of the tag. For example, ` + "`environment` in the `environment : production` tag.",
+																			MaxLength:   ptr(int64(255)),
+																			MinLength:   ptr(int64(1)),
+																			Type:        "string",
+																		},
+																		"value": {
+																			Description: `Variable that belongs to the set of the tag. For example, ` + "`production` in the `environment : production` tag.",
+																			MaxLength:   ptr(int64(255)),
+																			MinLength:   ptr(int64(1)),
+																			Type:        "string",
+																		},
 																	},
-																	"value": {
-																		Description: ptr(`Variable that belongs to the set of the tag. For example, ` + "`production` in the `environment : production` tag."),
-																		MaxLength:   ptr(255),
-																		MinLength:   ptr(1),
-																		Type:        "string",
-																	},
+																	Required: []string{"key", "value"},
 																},
-																Required: &[]string{"key", "value"},
 															},
 														},
 														"withDefaultAlertsSettings": {
-															Description: ptr(`Flag that indicates whether to create the project with default alert settings.`),
+															Description: `Flag that indicates whether to create the project with default alert settings.`,
 															Type:        "boolean",
 														},
 													},
-													Required: &[]string{"name", "orgId"},
+													Required: []string{"name", "orgId"},
 												},
 											},
 										},
@@ -124,62 +133,64 @@ This field sets restrictions on available regions in the project.
 								},
 								"status": {
 									Type: "object",
-									Properties: map[string]crd2go.OpenAPISchema{
+									Properties: map[string]apiextensions.JSONSchemaProps{
 										"conditions": {
-											Description: ptr("Represents the latest available observations of a resource's current state."),
+											Description: "Represents the latest available observations of a resource's current state.",
 											Type:        "array",
-											Items: &crd2go.OpenAPISchema{
-												Properties: map[string]crd2go.OpenAPISchema{
-													"lastTransitionTime": {
-														Description: ptr("Last time the condition transitioned from one status to another."),
-														Format:      ptr("date-time"),
-														Type:        "string",
+											Items: &apiextensions.JSONSchemaPropsOrArray{
+												Schema: &apiextensions.JSONSchemaProps{
+													Properties: map[string]apiextensions.JSONSchemaProps{
+														"lastTransitionTime": {
+															Description: "Last time the condition transitioned from one status to another.",
+															Format:      "date-time",
+															Type:        "string",
+														},
+														"message": {
+															Description: "A human readable message indicating details about the transition.",
+															Type:        "string",
+														},
+														"observedGeneration": {
+															Description: "observedGeneration represents the .metadata.generation that the condition was set based upon.",
+															Type:        "integer",
+														},
+														"reason": {
+															Description: "The reason for the condition's last transition.",
+															Type:        "string",
+														},
+														"status": {
+															Description: "Status of the condition, one of True, False, Unknown.",
+															Type:        "string",
+														},
+														"type": {
+															Description: "Type of condition.",
+															Type:        "string",
+														},
 													},
-													"message": {
-														Description: ptr("A human readable message indicating details about the transition."),
-														Type:        "string",
-													},
-													"observedGeneration": {
-														Description: ptr("observedGeneration represents the .metadata.generation that the condition was set based upon."),
-														Type:        "integer",
-													},
-													"reason": {
-														Description: ptr("The reason for the condition's last transition."),
-														Type:        "string",
-													},
-													"status": {
-														Description: ptr("Status of the condition, one of True, False, Unknown."),
-														Type:        "string",
-													},
-													"type": {
-														Description: ptr("Type of condition."),
-														Type:        "string",
-													},
+													Required: []string{"type", "status"},
+													Type:     "object",
 												},
-												Required: &[]string{"type", "status"},
-												Type:     "object",
 											},
 										},
 										"v20231115": {
-											Properties: map[string]crd2go.OpenAPISchema{
+											Properties: map[string]apiextensions.JSONSchemaProps{
 												"clusterCount": {
-													Description: ptr("Quantity of MongoDB Cloud clusters deployed in this project."),
-													Format:      ptr("int64"),
+													Description: "Quantity of MongoDB Cloud clusters deployed in this project.",
+													Format:      "int64",
 													Type:        "integer",
 												},
 												"created": {
-													Description: ptr("Date and time when MongoDB Cloud created this project. This parameter expresses its value in the ISO 8601 timestamp format in UTC."),
-													Format:      ptr("date-time"),
+													Description: "Date and time when MongoDB Cloud created this project. This parameter expresses its value in the ISO 8601 timestamp format in UTC.",
+													Format:      "date-time",
 													Type:        "string",
 												},
 												"id": {
-													Description: ptr("Unique 24-hexadecimal digit string that identifies the MongoDB Cloud project."),
-													MaxLength:   ptr(24),
-													MinLength:   ptr(24),
+													Description: "Unique 24-hexadecimal digit string that identifies the MongoDB Cloud project.",
+													MaxLength:   ptr(int64(24)),
+													MinLength:   ptr(int64(24)),
 													Type:        "string",
 												},
 											},
-											Required: &[]string{"clusterCount", "created"},
+											Required: []string{"clusterCount", "created"},
 											Type:     "object",
 										},
 									},
@@ -187,15 +198,14 @@ This field sets restrictions on available regions in the project.
 							},
 						},
 					},
-					Subresources: map[string]any{
-						"status": map[string]any{},
+					Subresources: &apiextensions.CustomResourceSubresources{
+						Status: &apiextensions.CustomResourceSubresourceStatus{},
 					},
 				},
 			},
 		},
+		Status: apiextensions.CustomResourceDefinitionStatus{},
 	}
-	require.NotNil(t, crd)
-	assert.Equal(t, want, crd)
 }
 
 func ptr[T any](t T) *T {
