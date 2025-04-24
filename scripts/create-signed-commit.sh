35997b51accadaac99e9476d779d632023b34c0b
jose.vazquez <jose.vazquez@mongodb.com>
2025-04-24 14:29:20 +0200

    pull rebase

--- scripts/create-signed-commit.sh
+++ scripts/create-signed-commit.sh
@@ -106,6 +106,6 @@ git clean -f
 git fetch "${remote}"
 git checkout -b "${branch}" "${remote}/${branch}" || git checkout "${branch}"
 git branch -u "${remote}/${branch}"
-git pull
+git pull --rebase
 
 echo "Local branch set to ${remote}/${branch}"
