d12d5c590d04f05e1dbb480997e6909093c5382c
josvaz <jose.vazquez@mongodb.com>
2025-04-24 15:02:24 +0200

    CLOUDP-307033: Sign also release PRs and SBOMs (#2289)
    * CLOUDP-307033: Sign also releas PRs and SBOMs
    
    * trace create commit
    
    * Send blobs from files
    
    * Sync remote created commit locally
    
    * Allow set a different upstream branch
    
    * force clean
    
    * fetch remote
    
    * Debug changing the branch reference
    
    * Ensure target branch exists, no need for upstream
    
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>
    
    * Checkout and pull local if present
    
    * Create remote branch on demand
    
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>
    
    * pull rebase
    
    * Don't pull
    
    * Remove reviewers
    
    * Remove traces
    
    ---------
    
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- scripts/create-signed-commit.sh
+++ scripts/create-signed-commit.sh
@@ -25,6 +25,20 @@ repo_owner="${REPO_OWNER:-mongodb}"
 repo_name="${REPO_NAME:-mongodb-atlas-kubernetes}"
 branch="${BRANCH:?}"
 commit_message="${COMMIT_MESSAGE:?}"
+remote=${REMOTE:-origin}
+
+# Ensure remote branch exists
+echo "Check remote branch ${branch} exists..."
+
+function create_branch() {
+  echo "Creating branch ${remote}/${branch}"
+  git stash
+  git push "${remote}" "${branch}"
+  git stash pop
+  git add .
+}
+
+git ls-remote --exit-code --heads origin "${branch}" >/dev/null || create_branch
 
 # Fetch the latest commit SHA
 LATEST_COMMIT_SHA=$(curl -s -H "Authorization: token $github_token" \
@@ -47,12 +61,14 @@ for FILE_PATH in $MODIFIED_FILES; do
   # Read file content encoded to base64  
   ENCODED_CONTENT=$(base64 -w0 < "${FILE_PATH}")
 
-  # Create blob  
+  # Create blob
+  echo "{\"content\": \"$ENCODED_CONTENT\", \"encoding\": \"base64\"}" > content.blob
   BLOB_JSON=$(curl -s -X POST -H "Authorization: token $github_token" \
     -H "Accept: application/vnd.github.v3+json" \
-    -d "{\"content\": \"$ENCODED_CONTENT\", \"encoding\": \"base64\"}" \
+    -d @content.blob \
     "https://api.github.com/repos/$repo_owner/$repo_name/git/blobs")
   BLOB_SHA=$(echo "${BLOB_JSON}" | jq -r '.sha')
+  rm content.blob
 
   # Append file info to tree JSON  
   NEW_TREE_ARRAY="${NEW_TREE_ARRAY}{\"path\": \"$FILE_PATH\", \"mode\": \"100644\", \"type\": \"blob\", \"sha\": \"$BLOB_SHA\"},"  
@@ -61,11 +77,13 @@ done
 # Remove trailing comma and close the array  
 NEW_TREE_ARRAY="${NEW_TREE_ARRAY%,}]"
 
-# Create new tree  
+# Create new tree
+echo "{\"base_tree\": \"$LATEST_TREE_SHA\", \"tree\": $NEW_TREE_ARRAY}" > new_tree_spec.json
 NEW_TREE_SHA=$(curl -s -X POST -H "Authorization: token $github_token" \
   -H "Accept: application/vnd.github.v3+json" \
-  -d "{\"base_tree\": \"$LATEST_TREE_SHA\", \"tree\": $NEW_TREE_ARRAY}" \
+  -d @new_tree_spec.json \
   "https://api.github.com/repos/$repo_owner/$repo_name/git/trees" | jq -r '.sha')  
+rm new_tree_spec.json
 
 echo "New tree SHA: $NEW_TREE_SHA"  
 
@@ -76,9 +94,16 @@ NEW_COMMIT_SHA=$(curl -s -X POST -H "Authorization: token $github_token" \
   "https://api.github.com/repos/$repo_owner/$repo_name/git/commits" | jq -r '.sha')
 echo "New commit SHA: $NEW_COMMIT_SHA"  
 
-# Update the reference of the branch to point to the new commit  
+# Update the reference of the branch to point to the new commit
 curl -s -X PATCH -H "Authorization: token $github_token" \
   -H "Accept: application/vnd.github.v3+json" -d "{\"sha\": \"$NEW_COMMIT_SHA\"}" \
-  "https://api.github.com/repos/$repo_owner/$repo_name/git/refs/heads/$branch"  
+  "https://api.github.com/repos/$repo_owner/$repo_name/git/refs/heads/$branch"
 
 echo "Branch ${branch} updated to new commit ${NEW_COMMIT_SHA}."  
+git restore --staged .
+git restore .
+git clean -f
+git fetch "${remote}"
+git checkout -b "${branch}" "${remote}/${branch}" || git checkout "${branch}"
+
+echo "Local branch set to ${remote}/${branch}"
