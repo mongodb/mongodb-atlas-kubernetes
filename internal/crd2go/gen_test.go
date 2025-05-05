16bb5f1c4ca12f0661254ab6f9210d642d125ea5
jose.vazquez <jose.vazquez@mongodb.com>
2025-05-05 18:49:45 +0200

    Add another CRD and document comments

--- internal/crd2go/gen_test.go
+++ internal/crd2go/gen_test.go
@@ -13,21 +13,40 @@ import (
 var samples embed.FS
 
 func TestGenerateCode(t *testing.T) {
-	f, err := samples.Open("samples/group.crd.yaml")
-	require.NoError(t, err)
-	defer f.Close()
-
-	sf, err := samples.Open("samples/v1/crd.go")
-	require.NoError(t, err)
-	sample, err := io.ReadAll(sf)
-	require.NoError(t, err)
-	want := string(sample)
-	defer sf.Close()
-
-	crd, err := crd2go.ParseCRD(f)
-	require.NoError(t, err)
-	stmt, err := crd2go.Generate(crd, crd2go.FirstVersion)
-	require.NoError(t, err)
-	require.NotNil(t, stmt)
-	require.Equal(t, want, stmt.GoString())
+	for _, tc := range []struct {
+		name string
+		crd string
+		src string
+	}{
+		{
+			name: "group",
+			crd:  "samples/group.crd.yaml",
+			src: "samples/v1/group.go",
+		},
+		{
+			name: "group",
+			crd:  "samples/networkpermission.crd.yaml",
+			src: "samples/v1/networkpermission.go",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			f, err := samples.Open(tc.crd)
+			require.NoError(t, err)
+			defer f.Close()
+		
+			sf, err := samples.Open(tc.src)
+			require.NoError(t, err)
+			sample, err := io.ReadAll(sf)
+			require.NoError(t, err)
+			want := string(sample)
+			defer sf.Close()
+		
+			crd, err := crd2go.ParseCRD(f)
+			require.NoError(t, err)
+			stmt, err := crd2go.Generate(crd, crd2go.FirstVersion)
+			require.NoError(t, err)
+			require.NotNil(t, stmt)
+			require.Equal(t, want, stmt.GoString())
+		})
+	}
 }
