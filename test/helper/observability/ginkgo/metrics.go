6269869a25d0a80b5b4102330f17092074ee675c
Roo Thorp <roo.thorp@mongodb.com>
2025-06-12 14:03:55 +0100

    add add licenses

--- test/helper/observability/ginkgo/metrics.go
+++ test/helper/observability/ginkgo/metrics.go
@@ -1,7 +1,22 @@
+// Copyright 2025 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ginkgo
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -86,7 +101,7 @@ func sendMetrics(report types.Report) error {
 	}
 	compressed := snappy.Encode(nil, data)
 
-	httpReq, err := http.NewRequest("POST", "http://localhost:30000/api/v1/write", bytes.NewReader(compressed))
+	httpReq, err := http.NewRequestWithContext(context.Background(), "POST", "http://localhost:30000/api/v1/write", bytes.NewReader(compressed))
 	if err != nil {
 		return fmt.Errorf("error creating http request: %w", err)
 	}
