7bd35950081b1e7bf2c8762cb4e63c8e18bc509d
Igor Karpukhin <karpukhinigor@gmail.com>
2025-07-09 16:51:11 +0200

    Fixed linter error

--- test/int/atlas_streams_test.go
+++ test/int/atlas_streams_test.go
@@ -17,13 +17,13 @@ package int
 import (
 	"context"
 	"fmt"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"net/http"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
