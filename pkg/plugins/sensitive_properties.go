e658a183fd1b8d90f115e55a9976bc4bd9c22cb3
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2025-07-10 09:56:40 +0200

    fix property selector for secrets

--- pkg/plugins/sensitive_properties.go
+++ pkg/plugins/sensitive_properties.go
@@ -33,7 +33,7 @@ func (n *SensitiveProperties) ProcessProperty(g Generator, propertyConfig *confi
 	extensionsSchema.Value.Extensions["x-kubernetes-mapping"] = map[string]interface{}{
 		"gvr":               "secrets/v1",
 		"nameSelector":      ".name",
-		"propertySelectors": []string{"$.data.key"},
+		"propertySelectors": []string{"$.data.#"},
 	}
 
 	extensionsSchema.Value.Extensions["x-openapi-mapping"] = map[string]interface{}{
