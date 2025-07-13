85e5731429dd1d70d47519fcc86924ba2631e8a3
andrpac <andrei.pacurar@mongodb.com>
2025-07-13 20:43:24 +0100

    feat: create release pipeline

--- .github/actions/certify-openshift-images/entrypoint.sh
+++ .github/actions/certify-openshift-images/entrypoint.sh
@@ -16,7 +16,7 @@
 
 set -eou pipefail
 
-docker login -u mongodb+mongodb_atlas_kubernetes -p "${REGISTRY_PASSWORD}" "${REGISTRY}"
+echo "${REGISTRY_PASSWORD}" | docker login -u "${REGISTRY_USERNAME}" --password-stdin "${REGISTRY}"
 
 submit_flag=--submit
 if [ "${SUBMIT}" == "false" ]; then
@@ -27,6 +27,6 @@ echo "Check and Submit result to RedHat Connect"
 # Send results to RedHat if preflight finished wthout errors
 preflight check container "${REGISTRY}/${REPOSITORY}:${VERSION}" \
   --pyxis-api-token="${RHCC_TOKEN}" \
-  --certification-project-id="${RHCC_PROJECT}" \
+  --certification-component-id="${RHCC_PROJECT}" \
   --docker-config="${HOME}/.docker/config.json" \
   ${submit_flag}
