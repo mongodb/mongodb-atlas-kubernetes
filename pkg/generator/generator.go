7d3831625c83a3325c272c71659e05390285a7b1
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2025-07-17 09:40:45 +0200

    add CRD plugin

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -18,18 +18,13 @@ import (
 	"context"
 	"fmt"
 	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/mongodb/atlas2crd/pkg/atlas"
-	"log"
-	"sigs.k8s.io/yaml"
-	"strings"
-
 	"github.com/mongodb/atlas2crd/pkg/apis/config/v1alpha1"
+	"github.com/mongodb/atlas2crd/pkg/atlas"
 	"github.com/mongodb/atlas2crd/pkg/config"
 	"github.com/mongodb/atlas2crd/pkg/plugins"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	runtimeschema "k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/utils/ptr"
+	"log"
+	"sigs.k8s.io/yaml"
 )
 
 type Generator struct {
@@ -58,55 +53,10 @@ func (g *Generator) majorVersions() []string {
 }
 
 func (g *Generator) Generate(ctx context.Context) (*apiextensions.CustomResourceDefinition, error) {
-	pluralGvk, singularGvk := guessKindToResource(g.config.GVK)
-
-	crd := &apiextensions.CustomResourceDefinition{
-		ObjectMeta: v1.ObjectMeta{
-			Name: fmt.Sprintf("%s.%s", pluralGvk.Resource, pluralGvk.Group),
-		},
-		Spec: apiextensions.CustomResourceDefinitionSpec{
-			Group: pluralGvk.Group,
-			Scope: apiextensions.NamespaceScoped,
-			Names: apiextensions.CustomResourceDefinitionNames{
-				Kind:     g.config.GVK.Kind,
-				ListKind: fmt.Sprintf("%sList", g.config.GVK.Kind),
-				Plural:   pluralGvk.Resource,
-				Singular: singularGvk.Resource,
-			},
-			Versions: []apiextensions.CustomResourceDefinitionVersion{
-				{
-					Name:    g.config.GVK.Version,
-					Served:  true,
-					Storage: true,
-				},
-			},
-			PreserveUnknownFields: ptr.To(false),
-			Validation: &apiextensions.CustomResourceValidation{
-				OpenAPIV3Schema: &apiextensions.JSONSchemaProps{
-					Type:        "object",
-					Description: fmt.Sprintf("A %v, managed by the MongoDB Kubernetes Atlas Operator.", singularGvk.Resource),
-					Properties: map[string]apiextensions.JSONSchemaProps{
-						"spec": {
-							Type: "object",
-							Description: fmt.Sprintf(`Specification of the %v supporting the following versions:
-
-%v
-
-At most one versioned spec can be specified. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status`, singularGvk.Resource, strings.Join(g.majorVersions(), "\n")),
-							Properties: map[string]apiextensions.JSONSchemaProps{},
-						},
-						"status": {
-							Type:        "object",
-							Description: fmt.Sprintf(`Most recently observed read-only status of the %v for the specified resource version. This data may not be up to date and is populated by the system. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status`, singularGvk.Resource),
-							Properties:  map[string]apiextensions.JSONSchemaProps{},
-						},
-					},
-				},
-			},
-		},
-	}
+	crd := &apiextensions.CustomResourceDefinition{}
 
 	g.plugins = []plugins.Plugin{
+		plugins.NewCrdPlugin(crd),
 		plugins.NewMajorVersionPlugin(crd),
 		plugins.NewParametersPlugin(crd),
 		plugins.NewEntryPlugin(crd),
@@ -123,6 +73,12 @@ At most one versioned spec can be specified. More info: https://git.k8s.io/commu
 	extensionsSchema := openapi3.NewSchema()
 	extensionsSchema.Properties = map[string]*openapi3.SchemaRef{
 		"spec": {Value: &openapi3.Schema{Properties: map[string]*openapi3.SchemaRef{}}},
+	}
+
+	for _, p := range g.plugins {
+		if err := p.ProcessCRD(g, &g.config); err != nil {
+			return nil, fmt.Errorf("error processing CRD in plugin %q: %w", p.Name(), err)
+		}
 	}
 
 	for _, mapping := range g.config.Mappings {
@@ -165,81 +121,11 @@ At most one versioned spec can be specified. More info: https://git.k8s.io/commu
 		crd.Annotations["api-mappings"] = string(d)
 	}
 
-	crd.Spec.Validation.OpenAPIV3Schema.Properties["status"].Properties["conditions"] = apiextensions.JSONSchemaProps{
-		Type:        "array",
-		Description: "Represents the latest available observations of a resource's current state.",
-		Items: &apiextensions.JSONSchemaPropsOrArray{
-			Schema: &apiextensions.JSONSchemaProps{
-				Type:     "object",
-				Required: []string{"type", "status"},
-				Properties: map[string]apiextensions.JSONSchemaProps{
-					"type":               {Type: "string", Description: "Type of condition."},
-					"status":             {Type: "string", Description: "Status of the condition, one of True, False, Unknown."},
-					"observedGeneration": {Type: "integer", Description: "observedGeneration represents the .metadata.generation that the condition was set based upon."},
-					"message":            {Type: "string", Description: "A human readable message indicating details about the transition."},
-					"reason":             {Type: "string", Description: "The reason for the condition's last transition."},
-					"lastTransitionTime": {Type: "string", Format: "date-time", Description: "Last time the condition transitioned from one status to another."},
-				},
-			},
-		},
-		XListMapKeys: []string{
-			"type",
-		},
-		XListType: ptr.To("map"),
-	}
-
-	crd.Status.StoredVersions = []string{}
-
-	// enable status subresource
-	crd.Spec.Subresources = &apiextensions.CustomResourceSubresources{
-		Status: &apiextensions.CustomResourceSubresourceStatus{},
-	}
-
-	crd.Spec.Names.Categories = g.config.Categories
-	crd.Spec.Names.ShortNames = g.config.ShortNames
-
-	for _, version := range crd.Spec.Versions {
-		if version.Storage {
-			crd.Status.StoredVersions = append(crd.Status.StoredVersions, version.Name)
-		}
-	}
-
-	for _, p := range g.plugins {
-		if err := p.ProcessCRD(g, &g.config); err != nil {
-			return nil, fmt.Errorf("error processing CRD in plugin %q: %w", p.Name(), err)
-		}
-	}
-
 	if err := ValidateCRD(ctx, crd); err != nil {
 		log.Printf("Error validating CRD: %v", err)
 	}
 
 	return crd, nil
-}
-
-func guessKindToResource(gvk v1.GroupVersionKind) ( /*plural*/ runtimeschema.GroupVersionResource /*singular*/, runtimeschema.GroupVersionResource) {
-	runtimeGVK := runtimeschema.GroupVersionKind{
-		Group:   gvk.Group,
-		Version: gvk.Version,
-		Kind:    gvk.Kind,
-	}
-	kindName := runtimeGVK.Kind
-	if len(kindName) == 0 {
-		return runtimeschema.GroupVersionResource{}, runtimeschema.GroupVersionResource{}
-	}
-	singularName := strings.ToLower(kindName)
-	singular := runtimeGVK.GroupVersion().WithResource(singularName)
-
-	switch string(singularName[len(singularName)-1]) {
-	case "s":
-		return runtimeGVK.GroupVersion().WithResource(singularName + "es"), singular
-	case "x":
-		return runtimeGVK.GroupVersion().WithResource(singularName + "es"), singular
-	case "y":
-		return runtimeGVK.GroupVersion().WithResource(strings.TrimSuffix(singularName, "y") + "ies"), singular
-	}
-
-	return runtimeGVK.GroupVersion().WithResource(singularName + "s"), singular
 }
 
 func clearPropertiesWithoutExtensions(schema *openapi3.Schema) bool {
--- pkg/plugins/crd.go
+++ pkg/plugins/crd.go
@@ -1,9 +1,149 @@
 package plugins
 
-type Crd struct {
+import (
+	"fmt"
+	configv1alpha1 "github.com/mongodb/atlas2crd/pkg/apis/config/v1alpha1"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtimeschema "k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/utils/ptr"
+	"strings"
+)
+
+type CrdPlugin struct {
 	NoOp
+	crd *apiextensions.CustomResourceDefinition
 }
 
-func (c Crd) Name() string {
+func (c CrdPlugin) Name() string {
 	return "crd"
+}
+
+func NewCrdPlugin(crd *apiextensions.CustomResourceDefinition) *CrdPlugin {
+	return &CrdPlugin{
+		crd: crd,
+	}
+}
+
+func (p *CrdPlugin) ProcessCRD(g Generator, crdConfig *configv1alpha1.CRDConfig) error {
+	pluralGvk, singularGvk := guessKindToResource(crdConfig.GVK)
+
+	p.crd.ObjectMeta = v1.ObjectMeta{
+		Name: fmt.Sprintf("%s.%s", pluralGvk.Resource, pluralGvk.Group),
+	}
+
+	p.crd.Spec = apiextensions.CustomResourceDefinitionSpec{
+		Group: pluralGvk.Group,
+		Scope: apiextensions.NamespaceScoped,
+		Names: apiextensions.CustomResourceDefinitionNames{
+			Kind:     crdConfig.GVK.Kind,
+			ListKind: fmt.Sprintf("%sList", crdConfig.GVK.Kind),
+			Plural:   pluralGvk.Resource,
+			Singular: singularGvk.Resource,
+		},
+		Versions: []apiextensions.CustomResourceDefinitionVersion{
+			{
+				Name:    crdConfig.GVK.Version,
+				Served:  true,
+				Storage: true,
+			},
+		},
+		PreserveUnknownFields: ptr.To(false),
+		Validation: &apiextensions.CustomResourceValidation{
+			OpenAPIV3Schema: &apiextensions.JSONSchemaProps{
+				Type:        "object",
+				Description: fmt.Sprintf("A %v, managed by the MongoDB Kubernetes Atlas Operator.", singularGvk.Resource),
+				Properties: map[string]apiextensions.JSONSchemaProps{
+					"spec": {
+						Type: "object",
+						Description: fmt.Sprintf(`Specification of the %v supporting the following versions:
+
+%v
+
+At most one versioned spec can be specified. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status`, singularGvk.Resource, strings.Join(majorVersions(crdConfig), "\n")),
+						Properties: map[string]apiextensions.JSONSchemaProps{},
+					},
+					"status": {
+						Type:        "object",
+						Description: fmt.Sprintf(`Most recently observed read-only status of the %v for the specified resource version. This data may not be up to date and is populated by the system. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status`, singularGvk.Resource),
+						Properties:  map[string]apiextensions.JSONSchemaProps{},
+					},
+				},
+			},
+		},
+	}
+
+	p.crd.Spec.Validation.OpenAPIV3Schema.Properties["status"].Properties["conditions"] = apiextensions.JSONSchemaProps{
+		Type:        "array",
+		Description: "Represents the latest available observations of a resource's current state.",
+		Items: &apiextensions.JSONSchemaPropsOrArray{
+			Schema: &apiextensions.JSONSchemaProps{
+				Type:     "object",
+				Required: []string{"type", "status"},
+				Properties: map[string]apiextensions.JSONSchemaProps{
+					"type":               {Type: "string", Description: "Type of condition."},
+					"status":             {Type: "string", Description: "Status of the condition, one of True, False, Unknown."},
+					"observedGeneration": {Type: "integer", Description: "observedGeneration represents the .metadata.generation that the condition was set based upon."},
+					"message":            {Type: "string", Description: "A human readable message indicating details about the transition."},
+					"reason":             {Type: "string", Description: "The reason for the condition's last transition."},
+					"lastTransitionTime": {Type: "string", Format: "date-time", Description: "Last time the condition transitioned from one status to another."},
+				},
+			},
+		},
+		XListMapKeys: []string{
+			"type",
+		},
+		XListType: ptr.To("map"),
+	}
+
+	p.crd.Status.StoredVersions = []string{}
+
+	// enable status subresource
+	p.crd.Spec.Subresources = &apiextensions.CustomResourceSubresources{
+		Status: &apiextensions.CustomResourceSubresourceStatus{},
+	}
+
+	p.crd.Spec.Names.Categories = crdConfig.Categories
+	p.crd.Spec.Names.ShortNames = crdConfig.ShortNames
+
+	for _, version := range p.crd.Spec.Versions {
+		if version.Storage {
+			p.crd.Status.StoredVersions = append(p.crd.Status.StoredVersions, version.Name)
+		}
+	}
+
+	return nil
+}
+
+func majorVersions(crdConfig *configv1alpha1.CRDConfig) []string {
+	var result []string
+	for _, m := range crdConfig.Mappings {
+		result = append(result, "- "+m.MajorVersion)
+	}
+	return result
+}
+
+func guessKindToResource(gvk v1.GroupVersionKind) ( /*plural*/ runtimeschema.GroupVersionResource /*singular*/, runtimeschema.GroupVersionResource) {
+	runtimeGVK := runtimeschema.GroupVersionKind{
+		Group:   gvk.Group,
+		Version: gvk.Version,
+		Kind:    gvk.Kind,
+	}
+	kindName := runtimeGVK.Kind
+	if len(kindName) == 0 {
+		return runtimeschema.GroupVersionResource{}, runtimeschema.GroupVersionResource{}
+	}
+	singularName := strings.ToLower(kindName)
+	singular := runtimeGVK.GroupVersion().WithResource(singularName)
+
+	switch string(singularName[len(singularName)-1]) {
+	case "s":
+		return runtimeGVK.GroupVersion().WithResource(singularName + "es"), singular
+	case "x":
+		return runtimeGVK.GroupVersion().WithResource(singularName + "es"), singular
+	case "y":
+		return runtimeGVK.GroupVersion().WithResource(strings.TrimSuffix(singularName, "y") + "ies"), singular
+	}
+
+	return runtimeGVK.GroupVersion().WithResource(singularName + "s"), singular
 }
