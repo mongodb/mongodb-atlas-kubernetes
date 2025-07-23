f330528dcc8e810bc1f6dfb3507b66739e512cea
Tom van der Woerdt <tom.vanderwoerdt@mongodb.com>
2025-07-23 11:20:14 -0400

    CLOUDP-333238: add configServerManagementMode to AtlasDeployment

--- api/v1/atlasdeployment_types.go
+++ api/v1/atlasdeployment_types.go
@@ -157,6 +157,10 @@ type AdvancedDeploymentSpec struct {
 	// A list of atlas search indexes configuration for the current deployment
 	// +optional
 	SearchIndexes []SearchIndex `json:"searchIndexes,omitempty"`
+	// Config Server Management Mode for creating or updating a sharded cluster.
+	// +kubebuilder:validation:Enum=ATLAS_MANAGED;FIXED_TO_DEDICATED
+	// +optional
+	ConfigServerManagementMode string `json:"configServerManagementMode,omitempty"`
 }
 
 // ToAtlas converts the AdvancedDeploymentSpec to native Atlas client ToAtlas format.
