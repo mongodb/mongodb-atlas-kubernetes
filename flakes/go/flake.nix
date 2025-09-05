9ed42691b53b8d93ee7c9a0c779ba8991ed6c1f8
jose.vazquez <jose.vazquez@mongodb.com>
2025-09-05 09:14:50 +0200

    Revert flake update only

--- flakes/go/flake.nix
+++ flakes/go/flake.nix
@@ -1,44 +1,21 @@
 {
-  description = "A flake for multi-platform Go toolchain selection";
+  description = "A flake for Go 1.24.4";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        goArchives = {
-          "x86_64-linux" = {
-            goFileName = "go1.25.0.linux-amd64.tar.gz";
-            sha256 = "sha256-KFKvDLIKExObNEiZLmm4aOUO0Pih5ZQO4d6eGaEjthM=";
-          };
-          "aarch64-darwin" = {
-            goFileName = "go1.25.0.darwin-arm64.tar.gz";
-            sha256 = "sha256-4n9F10sLz2x2QWk0kSj+8J/J/1t9+D/P/g/5J/J/t9+";
-          };
-        };
       in
       {
-        #  packages.go-nixpkgs-overridden = pkgs.go_1_24.overrideAttrs (old: {
-        #  version = "1.24.4";
-        #  src = pkgs.fetchurl {
-        #    url = "https://golang.org/dl/go1.24.4.linux-amd64.tar.gz";
-        #    sha256 = "sha256-d+XaM7tyrq7xukQYtv5RG8TQQYc8v4LlqmMYdA35hxc=";
-        #  };
-        # });
-
-        packages.go-binary = pkgs.runCommand "go-binary-1.25.0" {
-          nativeBuildInputs = [ pkgs.unzip pkgs.gzip pkgs.gnutar ];
+        packages.default = pkgs.go_1_24.overrideAttrs (old: {
+          version = "1.24.6";
           src = pkgs.fetchurl {
-            url = "https://go.dev/dl/${goArchives.${system}.goFileName}";
-            sha256 = goArchives.${system}.sha256;
+            url = "https://golang.org/dl/go1.24.6.linux-amd64.tar.gz";
+            sha256 = "sha256-u8o3zDlcl0/6SJPuNYGa0j67J0Jt+Hr5LpOp7GbvhxI=";
           };
-        } ''
-          tar -xf $src --one-top-level
-          mv go $out
-        '';
-
-        packages.default = self.packages.${system}.go-binary;
+        });
       });
 }
