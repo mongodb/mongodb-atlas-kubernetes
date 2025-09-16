f8e04b1c50b1d6acd3c03296b048af4ae4b8409f
josvaz <jose.vazquez@mongodb.com>
2025-09-16 15:20:07 +0200

    Add CRD2API library (#2677)
    * Initial commit
    
    * PoC of translation extracted from crd2go
    
    * Update README.md
    
    * Converge to a single ToAPI function
    
    * Document current public API
    
    * Add direct groupId test
    
    * Update README.md
    
    * Draft FromAPI
    
    * fix basic translation
    
    * Refactoring to expand/contract refs
    
    * Basic FromAPI support
    
    * Fix FromAPI naming
    
    * Add basic CI checks
    
    * Fix k8s dep cloned in-house
    
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>
    
    * Force UTC in time test
    
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>
    
    * fix lint issues
    
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>
    
    * Refactor ToAPI test shape
    
    * Add 3rd party test case
    
    * Separate deps to its own file
    
    * Rename secret encoding/decoding
    
    * Update README.md
    
    * Add license headers
    
    ---------
    
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

---
+++ tools/crd2api/pkg/translate/ref.go
@@ -0,0 +1,359 @@
+// Copyright 2025 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package translate
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/utils/strings/slices"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	v1 "github.com/josvazg/akotranslate/pkg/translate/samples/v1"
+)
+
+type EncodeDecodeFunc func(any) (any, error)
+
+var (
+	decoders = map[string]EncodeDecodeFunc{
+		"v1/secrets": func(in any) (any, error) {
+			return secretDecode((in).(string))
+		},
+	}
+
+	encoders = map[string]EncodeDecodeFunc{
+		"v1/secrets": func(in any) (any, error) {
+			return secretEncode((in).(string))
+		},
+	}
+
+	optionalExpansions = []string{"groupRef"}
+
+	supportedKubeObjects = map[string]func(obj map[string]any) (client.Object, error){
+		"v1/secrets":                            newKubeObjectFactory[corev1.Secret](),
+		"atlas.generated.mongodb.com/v1/groups": newKubeObjectFactory[v1.Group](),
+	}
+)
+
+func newKubeObjectFactory[T any, P PtrClientObj[T]]() func(map[string]any) (client.Object, error) {
+	return func(unstructured map[string]any) (client.Object, error) {
+		obj := new(T)
+		initializedObj, err := initObject(obj, unstructured)
+		if err != nil {
+			return nil, err
+		}
+		// Assert the concrete pointer type (*P) to the interface type.
+		// This is guaranteed to be safe because of the PtrClientObj constraint
+		return any(initializedObj).(client.Object), nil
+	}
+}
+
+func initObject[T any](obj *T, unstructured map[string]any) (*T, error) {
+	if unstructured != nil {
+		if err := fromUnstructured(obj, unstructured); err != nil {
+			return nil, err
+		}
+	}
+	return obj, nil
+}
+
+type refMapping struct {
+	XKubernetesMapping kubeMapping    `json:"x-kubernetes-mapping"`
+	XOpenAPIMapping    openAPIMapping `json:"x-openapi-mapping"`
+}
+
+type namedRef struct {
+	*refMapping
+	name string
+}
+
+func isReference(obj map[string]any) bool {
+	return obj["x-kubernetes-mapping"] != nil && obj["x-openapi-mapping"] != nil
+}
+
+func newRef(name string, rm *refMapping) *namedRef {
+	return &namedRef{name: name, refMapping: rm}
+}
+
+func (ref *namedRef) Expand(deps DependencyRepo, pathHint []string, obj map[string]any) error {
+	path := ref.pathToExpand(pathHint)
+	rawValue, err := accessField[any](obj, base(path))
+	if errors.Is(err, ErrNotFound) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("failed accessing value at path %v: %w", path, err)
+	}
+
+	value, err := ref.XKubernetesMapping.Encode(rawValue)
+	if err != nil {
+		return fmt.Errorf("failed to encode value at path %v: %w", path, err)
+	}
+	gvr := ref.XKubernetesMapping.GVR()
+	depUnstructured, err := unstructuredKubeObjectFor(gvr)
+	if err != nil {
+		return fmt.Errorf("failed to populate unstructured dependency: %w", err)
+	}
+	dep, err := ref.XKubernetesMapping.setAtPropertySelectors(gvr, depUnstructured, ref.XOpenAPIMapping.Property, value)
+	if err != nil {
+		if ref.isExpansionOptional() {
+			return nil
+		}
+		return fmt.Errorf("failed to populate final dependency object: %w", err)
+	}
+	dep.SetName(ref.Name(deps.MainObject().GetName(), path))
+	dep.SetNamespace(SetFallbackNamespace)
+	deps.Add(dep)
+	refData := map[string]any{"name": dep.GetName()}
+	if ref.XOpenAPIMapping.Property != "" {
+		path := resolveXPath(ref.XOpenAPIMapping.Property)
+		refData["key"] = base(path)
+	}
+	obj[ref.name] = refData
+	return nil
+}
+
+func (ref *namedRef) pathToExpand(pathHint []string) []string {
+	path := make([]string, len(pathHint))
+	copy(path, pathHint)
+	path[len(path)-1] = base(resolveXPath(ref.XOpenAPIMapping.Property))
+	return path
+}
+
+func (ref *namedRef) isExpansionOptional() bool {
+	return slices.Contains(optionalExpansions, ref.name)
+}
+
+func (ref *namedRef) Name(prefix string, path []string) string {
+	if path[0] == "entry" {
+		path = path[1:]
+	}
+	return strings.ToLower(strings.Join(append([]string{prefix}, path...), "-"))
+}
+
+func (ref *namedRef) Collapse(deps DependencyRepo, path []string, obj map[string]any) error {
+	reference, err := accessField[map[string]any](obj, base(path))
+	if errors.Is(err, ErrNotFound) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("failed accessing value at path %v: %w", path, err)
+	}
+	if len(reference) == 0 {
+		return nil
+	}
+
+	targetPath := ref.XOpenAPIMapping.TargetPath()
+	key, ok := reference["key"].(string)
+	if !ok || key == "" {
+		key = base(targetPath)
+	}
+	value, err := ref.XKubernetesMapping.FetchReferencedValue(key, reference, deps)
+	if err != nil {
+		return fmt.Errorf("failed to fetch referenced value %s: %w", key, err)
+	}
+	return createField(obj, value, targetPath...)
+}
+
+type kubeMapping struct {
+	NameSelector      string   `json:"nameSelector"`
+	PropertySelectors []string `json:"propertySelectors"`
+	Properties        []string `json:"properties"`
+	Type              kubeType `json:"type"`
+}
+
+type kubeType struct {
+	Kind     string `json:"kind"`
+	Group    string `json:"group,omitempty"`
+	Resource string `json:"resource"`
+	Version  string `json:"version"`
+}
+
+func (km kubeMapping) GVK() string {
+	if km.Type.Group == "" {
+		return fmt.Sprintf("%s, Kind=%s", km.Type.Version, km.Type.Kind)
+	}
+	return fmt.Sprintf("%s/%s, Kind=%s", km.Type.Group, km.Type.Version, km.Type.Kind)
+}
+
+func (km kubeMapping) GVR() string {
+	if km.Type.Group == "" {
+		return fmt.Sprintf("%s/%s", km.Type.Version, km.Type.Resource)
+	}
+	return fmt.Sprintf("%s/%s/%s", km.Type.Group, km.Type.Version, km.Type.Resource)
+}
+
+func (km kubeMapping) Equal(gvk schema.GroupVersionKind) bool {
+	return km.Type.Group == gvk.Group && km.Type.Version == gvk.Version && km.Type.Kind == gvk.Kind
+}
+
+func (km kubeMapping) FetchReferencedValue(target string, reference map[string]any, deps DependencyRepo) (any, error) {
+	refPath := km.NameSelector
+	if refPath == "" {
+		return nil, errors.New("cannot solve reference without a x-kubernetes-mapping.nameSelector")
+	}
+	refName, err := accessField[string](reference, asPath(refPath)...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to access field %q at %v: %w", refPath, reference, err)
+	}
+	resource := deps.Find(refName, SetFallbackNamespace)
+	if resource == nil {
+		return nil, fmt.Errorf("failed to find Kubernetes resource %q: %w", refName, err)
+	}
+	gvk := resource.GetObjectKind().GroupVersionKind()
+	if km.Type.Kind != "" && !km.Equal(gvk) {
+		return nil, fmt.Errorf("resource %q had to be a %q but got %q", refName, km.GVK(), gvk)
+	}
+	resourceMap, err := toUnstructured(resource)
+	if err != nil {
+		return nil, fmt.Errorf("failed to turn resource %q into an unestuctued map: %w", refName, err)
+	}
+	value, err := km.fetchFromProperties(resourceMap)
+	if err != nil && !errors.Is(err, ErrNotFound) {
+		return nil, fmt.Errorf("failed to resolve reference properties: %w", err)
+	}
+	if err == ErrNotFound {
+		var err error
+		value, err = km.fetchFromPropertySelectors(resourceMap, target)
+		if errors.Is(err, ErrNotFound) {
+			return nil, fmt.Errorf("failed to resolve reference properties or property selectors: %w", err)
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to resolve reference property selectors: %w", err)
+		}
+	}
+	return km.Decode(value)
+}
+
+func (km kubeMapping) Decode(value any) (any, error) {
+	decode := decoders[km.GVR()]
+	if decode != nil {
+		return decode(value)
+	}
+	return value, nil
+}
+
+func (km kubeMapping) Encode(value any) (any, error) {
+	encode := encoders[km.GVR()]
+	if encode != nil {
+		return encode(value)
+	}
+	return value, nil
+}
+
+func (km kubeMapping) fetchFromProperties(resource map[string]any) (any, error) {
+	for _, prop := range km.Properties {
+		path := resolveXPath(prop)
+		value, err := accessField[any](resource, path...)
+		if errors.Is(err, ErrNotFound) {
+			continue
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to access property as %v: %w", path, err)
+		}
+		return value, nil
+	}
+	return nil, ErrNotFound
+}
+
+func (km kubeMapping) fetchFromPropertySelectors(resource map[string]any, target string) (any, error) {
+	for _, selector := range km.PropertySelectors {
+		prop := selector
+		if strings.HasSuffix(prop, ".#") {
+			prop = fmt.Sprintf("%s.%s", prop[:len(prop)-2], target)
+		}
+		path := resolveXPath(prop)
+		value, err := accessField[any](resource, path...)
+		if errors.Is(err, ErrNotFound) {
+			continue
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to access selected property as %v: %w", path, err)
+		}
+		return value, nil
+	}
+	return nil, ErrNotFound
+}
+
+func (km kubeMapping) setAtPropertySelectors(gvr string, unstructured map[string]any, target string, value any) (client.Object, error) {
+	for _, selector := range km.PropertySelectors {
+		prop := selector
+		if strings.HasSuffix(prop, ".#") {
+			targetPath := resolveXPath(target)
+			prop = fmt.Sprintf("%s.%s", prop[:len(prop)-2], base(targetPath))
+		}
+		path := resolveXPath(prop)
+		if err := createField(unstructured, value, path...); err != nil {
+			return nil, fmt.Errorf("failed to set value at %q: %w", path, err)
+		}
+		obj, err := initializedKubeObjectFor(gvr, unstructured)
+		if err != nil {
+			return nil, fmt.Errorf("failed to initialize Kubernetes object: %w", err)
+		}
+		unstructuredCopy, err := toUnstructured(obj)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read Kubernetes object contents: %w", err)
+		}
+		valueCopy, err := accessField[any](unstructuredCopy, path...)
+		if reflect.DeepEqual(value, valueCopy) {
+			return obj, nil
+		}
+		if err != nil && !errors.Is(err, ErrNotFound) {
+			return nil, fmt.Errorf("failed to check Kubernetes object contents: %w", err)
+		}
+	}
+	return nil, ErrNotFound
+}
+
+type openAPIMapping struct {
+	Property string `json:"property"`
+	Type     string `json:"type"`
+}
+
+func (oam openAPIMapping) TargetPath() []string {
+	return resolveXPath(oam.Property)
+}
+
+func resolveXPath(xpath string) []string {
+	if strings.HasPrefix(xpath, "$.") {
+		return asPath(xpath[1:])
+	}
+	return asPath(xpath)
+}
+
+func unstructuredKubeObjectFor(gvr string) (map[string]any, error) {
+	objCopy, err := kubeObjectFor(gvr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get unstructured kube object for GVR %q: %w", gvr, err)
+	}
+	return toUnstructured(objCopy)
+}
+
+func kubeObjectFor(gvr string) (client.Object, error) {
+	return initializedKubeObjectFor(gvr, nil)
+}
+
+func initializedKubeObjectFor(gvr string, unstructuredData map[string]any) (client.Object, error) {
+	objFn, ok := supportedKubeObjects[gvr]
+	if !ok {
+		return nil, fmt.Errorf("unsupported kube object for GVR %q", gvr)
+	}
+	return objFn(unstructuredData)
+}
---
+++ tools/crd2api/pkg/translate/unstructured_test.go
@@ -0,0 +1,94 @@
+// Copyright 2025 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package translate
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	admin2025 "go.mongodb.org/atlas-sdk/v20250312005/admin"
+
+	"github.com/josvazg/akotranslate/internal/pointer"
+)
+
+type testStruct struct {
+	ID        int
+	YesNo     bool
+	Text      string
+	Data      float32
+	Timestamp time.Time
+	SubStruct []subStruct
+}
+
+type subStruct struct {
+	SubID   int
+	YesNo   bool
+	Text    string
+	Data    float32
+	TheTime time.Time
+}
+
+func TestParamsFill(t *testing.T) {
+	unstructuredSample := map[string]any{
+		"groupid": "62b6e34b3d91647abb20e7b8",
+		"groupalertsconfig": map[string]any{
+			"enabled":       true,
+			"eventtypename": "some-event",
+		},
+	}
+	result := admin2025.CreateAlertConfigurationApiParams{}
+	require.NoError(t, fromUnstructured(&result, unstructuredSample))
+	assert.Equal(t, admin2025.CreateAlertConfigurationApiParams{
+		GroupId: "62b6e34b3d91647abb20e7b8",
+		GroupAlertsConfig: &admin2025.GroupAlertsConfig{
+			Enabled:       pointer.Get(true),
+			EventTypeName: pointer.Get("some-event"),
+		},
+	}, result)
+}
+
+func TestToAndFromUnstructured(t *testing.T) {
+	sample := testStruct{
+		ID:        12,
+		YesNo:     true,
+		Text:      "some text",
+		Data:      2.4,
+		Timestamp: time.Now().UTC().Round(time.Nanosecond),
+		SubStruct: []subStruct{
+			{
+				SubID:   15,
+				YesNo:   true,
+				Text:    "more text",
+				Data:    0.67,
+				TheTime: time.Now().Add(-24 * 7 * time.Hour).UTC().Round(time.Nanosecond),
+			},
+			{
+				SubID:   14,
+				YesNo:   false,
+				Text:    "and even more text",
+				Data:    1.67,
+				TheTime: time.Now().Add(-24 * 15 * time.Hour).UTC().Round(time.Nanosecond),
+			},
+		},
+	}
+	sampleMap, err := toUnstructured(sample)
+	require.NoError(t, err)
+	result := testStruct{}
+	require.NoError(t, fromUnstructured(&result, sampleMap))
+	assert.Equal(t, sample, result)
+}
