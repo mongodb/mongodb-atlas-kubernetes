11c11027f2ed892576ce45cca4853d7b98f13f8f
jose.vazquez <jose.vazquez@mongodb.com>
2025-09-17 15:43:15 +0200

    CLOUDP-310817: Add the AKO to API library CI
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- tools/ako2api/go.mod
+++ tools/ako2api/go.mod
@@ -29,6 +29,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/magefile/mage v1.15.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
---
+++ tools/ako2api/magefile.go
@@ -0,0 +1,75 @@
+// Copyright 2025 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+//go:build mage
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+// CI runs all linting and validation checks.
+func CI() {
+	mg.SerialDeps(Addlicense, GCI)
+}
+
+// Addlicense runs the addlicense check to ensure source files have license headers.
+func Addlicense() error {
+	fmt.Println("Running license header check...")
+
+	// sh.RunV runs the command verbosely (streaming output)
+	// and returns an error if the command fails.
+	return sh.RunV("addlicense",
+		"-check",
+		"-l", "apache",
+		"-c", "MongoDB Inc",
+		"-ignore", "**/*.md",
+		"-ignore", "**/*.yaml",
+		"-ignore", "**/*.yml",
+		"-ignore", "**/*.nix",
+		"-ignore", ".devbox/**",
+		"-ignore", "magefile.go",
+		".",
+	)
+}
+
+// GCI runs gci to check that Go import orders are correct.
+func GCI() error {
+	fmt.Println("🧹 Formatting Go imports...")
+	if err := sh.RunV(
+		"gci", "write",
+		"--skip-generated",
+		"-s", "standard",
+		"-s", "default",
+		"-s", "localmodule",
+		".",
+	); err != nil {
+		return fmt.Errorf("gci write command failed: %w", err)
+	}
+
+	fmt.Println("🔍 Checking for changes...")
+	if err := sh.Run("git", "diff-index", "--quiet", "HEAD", "--"); err != nil {
+		fmt.Println("❗️ Go files were not correctly formatted. The following files have changes:")
+		sh.RunV("git", "diff-index", "--name-only", "HEAD")
+		return fmt.Errorf("please run 'mage gci' and commit the changes")
+	}
+
+	fmt.Println("✅ Go imports are correctly formatted.")
+	return nil
+}
