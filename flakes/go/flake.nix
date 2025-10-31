90f477a83c4bfd92392279de7df46daacab27075
josvaz <jose.vazquez@mongodb.com>
2025-10-31 15:22:08 +0100

    CLOUDP-355439: Backport signatures fix to tag 2.11.0 (#2850)

--- flakes/go/flake.nix
+++ flakes/go/flake.nix
@@ -8,7 +8,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        version = "1.24.7";
+        version = "1.24.9";
       in
       {
         packages.default = pkgs.go_1_24.overrideAttrs (old: {
@@ -16,7 +16,7 @@
           inherit version;
           src = pkgs.fetchurl {
             url = "https://golang.org/dl/go${version}.linux-amd64.tar.gz";
-            sha256 = "sha256-2hgZHdt9uKkzmBbz4rVL3e2AR83CpdZwWUePjRWVxD8=";
+            sha256 = "sha256-W3iZWRwt1unaGAn95KL62ELEXT9rnesjW6giFuMeNKY=";
           };
         });
       });
