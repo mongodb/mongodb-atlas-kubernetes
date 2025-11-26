b04c6e42e88ced3bc2e1fb5bacf1c30dafb35f18
Igor Karpukhin <karpukhinigor@gmail.com>
2025-11-26 15:25:14 +0100

    Fixed the pipeline again

--- internal/generated/controller/databaseuser/databaseuser_controller.go
+++ internal/generated/controller/databaseuser/databaseuser_controller.go
@@ -15,17 +15,16 @@
 package databaseuser
 
 import (
+	atlas "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/controller/atlas"
+	reconciler "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/controller/reconciler"
+	crapi "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/crapi"
+	akov2generated "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/nextapi/generated/v1"
+	ctrlstate "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/state"
 	v20250312sdk "go.mongodb.org/atlas-sdk/v20250312006/admin"
 	zap "go.uber.org/zap"
 	client "sigs.k8s.io/controller-runtime/pkg/client"
 	cluster "sigs.k8s.io/controller-runtime/pkg/cluster"
 	predicate "sigs.k8s.io/controller-runtime/pkg/predicate"
-
-	atlas "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/controller/atlas"
-	reconciler "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/controller/reconciler"
-	translate "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/crapi"
-	akov2generated "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/nextapi/generated/v1"
-	ctrlstate "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/state"
 )
 
 // +kubebuilder:rbac:groups=atlas.generated.mongodb.com,resources=databaseusers,verbs=get;list;watch;create;update;patch;delete
@@ -64,6 +63,6 @@ func NewDatabaseUserReconciler(c cluster.Cluster, atlasProvider atlas.Provider, 
 
 	return ctrlstate.NewStateReconciler(databaseuserHandler, ctrlstate.WithCluster[akov2generated.DatabaseUser](c), ctrlstate.WithReapplySupport[akov2generated.DatabaseUser](reapplySupport))
 }
-func handlerv20250312Func(kubeClient client.Client, atlasClient *v20250312sdk.APIClient, translatorRequest *translate.Request, deletionProtection bool) ctrlstate.StateHandler[akov2generated.DatabaseUser] {
+func handlerv20250312Func(kubeClient client.Client, atlasClient *v20250312sdk.APIClient, translatorRequest *crapi.Request, deletionProtection bool) ctrlstate.StateHandler[akov2generated.DatabaseUser] {
 	return NewHandlerv20250312(kubeClient, atlasClient, translatorRequest, deletionProtection)
 }
