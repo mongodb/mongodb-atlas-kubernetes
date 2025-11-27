7802573496330f3dea9614045519ed9fdaa41e56
Roo Thorp <roo.thorp@mongodb.com>
2025-11-27 12:53:48 +0000

    CLOUDP-362263: Add update Atlas SDK script from CLI (#2945)
    * add update sdk script from CLI
    
    * add SDK Update GHA
    
    * correct current sdk version
    
    * use go.mod for current sdk rather than file
    
    * add license text
    
    * appease linter

---
+++ scripts/update-sdk.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+CURRENT_SDK_RELEASE=$(go list -m all | grep go.mongodb.org/atlas-sdk | awk -F '/| ' '{print $3}')
+echo "CURRENT_SDK_RELEASE: $CURRENT_SDK_RELEASE"
+
+LATEST_SDK_TAG=$(curl -sSfL -X GET  https://api.github.com/repos/mongodb/atlas-sdk-go/releases/latest | jq -r '.tag_name')
+echo "LATEST_SDK_TAG: $LATEST_SDK_TAG"
+
+LATEST_SDK_RELEASE=$(echo "${LATEST_SDK_TAG}" | cut -d '.' -f 1)
+echo "LATEST_SDK_RELEASE: $LATEST_SDK_RELEASE"
+echo  "==> Updating SDK to latest major version ${LATEST_SDK_TAG}"
+
+go tool --modfile tools/toolbox/go.mod gomajor get --rewrite "go.mongodb.org/atlas-sdk/${CURRENT_SDK_RELEASE}" "go.mongodb.org/atlas-sdk/${LATEST_SDK_RELEASE}@${LATEST_SDK_TAG}"
+go mod tidy
+
+echo "Done"
