33217859605bd096260f6d34128ae2d28aa9c311
jose.vazquez <jose.vazquez@mongodb.com>
2025-11-28 14:14:07 +0100

    CLOUDP-359047: Add Flex Cluster reconciler support
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- internal/generated/controller/flexcluster/handler_v20250312.go
+++ internal/generated/controller/flexcluster/handler_v20250312.go
@@ -16,6 +16,8 @@ package flexcluster
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	v20250312sdk "go.mongodb.org/atlas-sdk/v20250312006/admin"
 	controllerruntime "sigs.k8s.io/controller-runtime"
@@ -24,11 +26,16 @@ import (
 	controller "sigs.k8s.io/controller-runtime/pkg/controller"
 	reconcile "sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	"github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/controller/customresource"
 	crapi "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/crapi"
 	akov2generated "github.com/mongodb/mongodb-atlas-kubernetes/v2/internal/nextapi/generated/v1"
 	ctrlstate "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/controller/state"
 	result "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/result"
 	state "github.com/mongodb/mongodb-atlas-kubernetes/v2/pkg/state"
+)
+
+var (
+	errMissingFlexGroupId = reconcile.TerminalError(errors.New("missing Flex cluster group id"))
 )
 
 type Handlerv20250312 struct {
@@ -49,65 +56,118 @@ func NewHandlerv20250312(kubeClient client.Client, atlasClient *v20250312sdk.API
 
 // HandleInitial handles the initial state for version v20250312
 func (h *Handlerv20250312) HandleInitial(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement initial state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateUpdated, "Updated AtlasFlexCluster.")
+	if flexcluster.Spec.V20250312.GroupId == nil {
+		return result.Error(state.StateInitial, errMissingFlexGroupId)
+	}
+	groupID := *flexcluster.Spec.V20250312.GroupId
+	flexClusterRequest := v20250312sdk.FlexClusterDescriptionCreate20241113{}
+	if err := h.translator.ToAPI(&flexClusterRequest, flexcluster); err != nil {
+		return result.Error(state.StateInitial, fmt.Errorf("failed to translate flex create request: %w", err))
+	}
+	atlasFlexCluster, _, err := h.atlasClient.FlexClustersApi.CreateFlexCluster(ctx, groupID, &flexClusterRequest).Execute()
+	if err != nil {
+		return result.Error(state.StateInitial, fmt.Errorf("failed to create flex cluster: %w", err))
+	}
+	newFlexCluster := flexcluster.DeepCopy()
+	if _, err := h.translator.FromAPI(newFlexCluster, atlasFlexCluster); err != nil {
+		return result.Error(state.StateInitial, fmt.Errorf("failed to translate flex create response: %w", err))
+	}
+
+	err = h.kubeClient.Status().Patch(ctx, newFlexCluster, client.MergeFrom(flexcluster))
+	if err != nil {
+		return result.Error(state.StateInitial, fmt.Errorf("failed to patch flex cluster status: %w", err))
+	}
+	return result.NextState(state.StateCreating, "Creating Flex Cluster.")
 }
 
 // HandleImportRequested handles the importrequested state for version v20250312
 func (h *Handlerv20250312) HandleImportRequested(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement importrequested state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateImported, "Import completed")
+	externalName, ok := flexcluster.GetAnnotations()["mongodb.com/external-name"]
+	if !ok {
+		return result.Error(state.StateImportRequested, errors.New("missing mongodb.com/external-name"))
+	}
+
+	externalGroupID, ok := flexcluster.GetAnnotations()["mongodb.com/external-group-id"]
+	if !ok {
+		return result.Error(state.StateImportRequested, errors.New("missing mongodb.com/external-group-id"))
+	}
+	flexClusterCopy := flexcluster.DeepCopy()
+	flexClusterCopy.Spec.V20250312.Entry.Name = externalName
+	flexClusterCopy.Spec.V20250312.GroupId = &externalGroupID
+	_, err := h.getAndPatchStatus(ctx, flexClusterCopy)
+	if err != nil {
+		return result.Error(state.StateImportRequested, err)
+	}
+	return result.NextState(state.StateImported, "Imported Flex Cluster.")
 }
 
 // HandleImported handles the imported state for version v20250312
 func (h *Handlerv20250312) HandleImported(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement imported state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateUpdated, "Ready")
+	return h.handleIdle(ctx, flexcluster, state.StateCreated, state.StateUpdating)
 }
 
 // HandleCreating handles the creating state for version v20250312
 func (h *Handlerv20250312) HandleCreating(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement creating state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateCreated, "Resource created")
+	return h.handleUpserting(ctx, flexcluster, state.StateCreating, state.StateCreated)
 }
 
 // HandleCreated handles the created state for version v20250312
 func (h *Handlerv20250312) HandleCreated(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement created state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateUpdated, "Ready")
+	return h.handleIdle(ctx, flexcluster, state.StateCreated, state.StateUpdating)
 }
 
 // HandleUpdating handles the updating state for version v20250312
 func (h *Handlerv20250312) HandleUpdating(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement updating state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateUpdated, "Update completed")
+	return h.handleUpserting(ctx, flexcluster, state.StateUpdating, state.StateUpdated)
 }
 
 // HandleUpdated handles the updated state for version v20250312
 func (h *Handlerv20250312) HandleUpdated(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement updated state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateUpdated, "Ready")
+	return h.handleIdle(ctx, flexcluster, state.StateUpdated, state.StateUpdating)
 }
 
 // HandleDeletionRequested handles the deletionrequested state for version v20250312
 func (h *Handlerv20250312) HandleDeletionRequested(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement deletionrequested state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateDeleting, "Deletion started")
+	if customresource.IsResourcePolicyKeepOrDefault(flexcluster, h.deletionProtection) {
+		return result.NextState(state.StateDeleted, "Flex Cluster deleted.")
+	}
+
+	if flexcluster.Status.V20250312 == nil {
+		return result.NextState(state.StateDeleted, "Flex Cluster is unamanged.")
+	}
+
+	if flexcluster.Spec.V20250312.GroupId == nil {
+		return result.Error(state.StateInitial, errMissingFlexGroupId)
+	}
+	groupID := *flexcluster.Spec.V20250312.GroupId
+	_, err := h.atlasClient.FlexClustersApi.DeleteFlexCluster(
+		ctx, groupID, flexcluster.Spec.V20250312.Entry.Name).Execute()
+
+	switch {
+	case v20250312sdk.IsErrorCode(err, "CLUSTER_NOT_FOUND"):
+		return result.NextState(state.StateDeleted, "Flex Cluster was deleted in Atlas.")
+	case err != nil:
+		return result.Error(state.StateDeletionRequested, fmt.Errorf("failed to delete flex cluster: %w", err))
+	}
+
+	return result.NextState(state.StateDeleting, "Deleting Flex Cluster.")
 }
 
 // HandleDeleting handles the deleting state for version v20250312
 func (h *Handlerv20250312) HandleDeleting(ctx context.Context, flexcluster *akov2generated.FlexCluster) (ctrlstate.Result, error) {
-	// TODO: Implement deleting state logic
-	// TODO: Use h.atlasProvider.SdkClientSet(ctx, h.globalSecretRef, h.log) to get Atlas SDK client
-	return result.NextState(state.StateDeleted, "Deleted")
+	if flexcluster.Spec.V20250312.GroupId == nil {
+		return result.Error(state.StateDeleting, errMissingFlexGroupId)
+	}
+	groupID := *flexcluster.Spec.V20250312.GroupId
+	_, _, err := h.atlasClient.FlexClustersApi.GetFlexCluster(
+		ctx, groupID, flexcluster.Spec.V20250312.Entry.Name).Execute()
+	switch {
+	case v20250312sdk.IsErrorCode(err, "CLUSTER_NOT_FOUND"):
+		return result.NextState(state.StateDeleted, "Deleted")
+	case err != nil:
+		return result.Error(state.StateDeletionRequested, fmt.Errorf("failed to delete flexcluster: %w", err))
+	}
+	return result.NextState(state.StateDeleting, "Deleting Flex Cluster.")
 }
 
 // For returns the resource and predicates for the controller
@@ -117,6 +177,75 @@ func (h *Handlerv20250312) For() (client.Object, builder.Predicates) {
 
 // SetupWithManager sets up the controller with the Manager
 func (h *Handlerv20250312) SetupWithManager(mgr controllerruntime.Manager, rec reconcile.Reconciler, defaultOptions controller.Options) error {
-	// This method is not used for version-specific handlers but required by StateHandler interface
-	return nil
+	panic("do not setup Handlerv20250312")
+}
+
+// HandleUpserting handles the creating and updating state for flex version v20250312
+func (h *Handlerv20250312) handleUpserting(ctx context.Context, flexcluster *akov2generated.FlexCluster, currentState, finalState state.ResourceState) (ctrlstate.Result, error) {
+	atlasFlexCluster, err := h.getAndPatchStatus(ctx, flexcluster)
+	if err != nil {
+		return result.Error(currentState, err)
+	}
+	if atlasFlexCluster.GetStateName() == "CREATING" || atlasFlexCluster.GetStateName() == "UPDATING" {
+		return result.NextState(currentState, "Upserting Flex Cluster.")
+	}
+	return result.NextState(finalState, "Upserted Flex Cluster.")
+}
+
+// HandleIdle handles the creating and updating state for flex version v20250312
+func (h *Handlerv20250312) handleIdle(ctx context.Context, flexcluster *akov2generated.FlexCluster, currentState, finalState state.ResourceState) (ctrlstate.Result, error) {
+	update, err := ctrlstate.ShouldUpdate(flexcluster)
+	if err != nil {
+		return result.Error(currentState, reconcile.TerminalError(err))
+	}
+
+	if !update {
+		return result.NextState(currentState, "Flex cluster up to date. No update required.")
+	}
+
+	if flexcluster.Spec.V20250312.GroupId == nil {
+		return result.Error(state.StateInitial, errMissingFlexGroupId)
+	}
+	groupID := *flexcluster.Spec.V20250312.GroupId
+	flexClusterUpdate := v20250312sdk.FlexClusterDescriptionUpdate20241113{}
+	if err := h.translator.ToAPI(&flexClusterUpdate, flexcluster); err != nil {
+		return result.Error(state.StateInitial, fmt.Errorf("failed to translate update flex cluster request: %w", err))
+	}
+	atlasFlexCluster, _, err := h.atlasClient.FlexClustersApi.UpdateFlexCluster(
+		ctx, groupID, flexcluster.Spec.V20250312.Entry.Name, &flexClusterUpdate).Execute()
+	if err != nil {
+		return result.Error(currentState, fmt.Errorf("failed to get update cluster: %w", err))
+	}
+	flexclusterCopy := flexcluster.DeepCopy()
+	if _, err := h.translator.FromAPI(flexclusterCopy, atlasFlexCluster); err != nil {
+		return result.Error(currentState, fmt.Errorf("failed to translate update cluster response: %w", err))
+	}
+
+	err = h.kubeClient.Status().Patch(ctx, flexclusterCopy, client.MergeFrom(flexcluster))
+	if err != nil {
+		return result.Error(currentState, fmt.Errorf("failed to patch cluster status: %w", err))
+	}
+	return result.NextState(finalState, "Updating Flex Cluster.")
+}
+
+func (h *Handlerv20250312) getAndPatchStatus(ctx context.Context, flexcluster *akov2generated.FlexCluster) (*v20250312sdk.FlexClusterDescription20241113, error) {
+	if flexcluster.Spec.V20250312.GroupId == nil {
+		return nil, errMissingFlexGroupId
+	}
+	groupID := *flexcluster.Spec.V20250312.GroupId
+	atlasFlexCluster, _, err := h.atlasClient.FlexClustersApi.GetFlexCluster(
+		ctx, groupID, flexcluster.Spec.V20250312.Entry.Name).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get cluster: %w", err)
+	}
+	flexclusterCopy := flexcluster.DeepCopy()
+	if _, err := h.translator.FromAPI(flexclusterCopy, atlasFlexCluster); err != nil {
+		return nil, fmt.Errorf("failed to translate get cluster response: %w", err)
+	}
+
+	err = h.kubeClient.Status().Patch(ctx, flexclusterCopy, client.MergeFrom(flexcluster))
+	if err != nil {
+		return nil, fmt.Errorf("failed to patch cluster status: %w", err)
+	}
+	return atlasFlexCluster, nil
 }
