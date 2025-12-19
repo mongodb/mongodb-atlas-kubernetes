0fb7a17f2d7dd80da0658298c7c1f8905354be7e
jose.vazquez <jose.vazquez@mongodb.com>
2025-12-19 09:42:25 +0100

    Add explanatory comment
    Signed-off-by: jose.vazquez <jose.vazquez@mongodb.com>

--- tools/openapi2crd/pkg/plugins/print_conditions.go
+++ tools/openapi2crd/pkg/plugins/print_conditions.go
@@ -11,6 +11,17 @@ func (p *PrintConditions) Name() string {
 }
 
 func (p *PrintConditions) Process(req *MappingProcessorRequest) error {
+	// Given that new SDK versions are supported by different spec subfields, it
+	// is unlikely there will be more than one supported Kubernetes version.
+	//
+	// The CRD validation fails if all versions use the same print columns, in
+	// such case they have to be set at the top level to avoid errors.
+	// Note that the produced YAML still places the print columns at the version 
+	// level anyways.
+	//
+	// Check out the upstream code for more details:
+	// https://github.com/kubernetes/apiextensions-apiserver/blob/a780e0393511161d7ef1e6466035181a4f84f347/pkg/apis/apiextensions/validation/validation.go#L438C5-L438C34
+	// https://github.com/kubernetes/apiextensions-apiserver/blob/a780e0393511161d7ef1e6466035181a4f84f347/pkg/apis/apiextensions/validation/validation.go#L762
 	req.CRD.Spec.AdditionalPrinterColumns = []apiextensions.CustomResourceColumnDefinition{
 		{
 			JSONPath: `.status.conditions[?(@.type=="Ready")].status`,
