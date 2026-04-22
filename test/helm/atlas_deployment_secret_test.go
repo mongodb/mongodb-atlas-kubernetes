ef41676e4af3a4e7321e448ec3d94653d01367cb
Maciej Karaś <maciej.karas@mongodb.com>
2026-04-22 22:20:29 +0200

    atlas-deployment chart: support Service Account credentials

---
+++ test/helm/atlas_deployment_secret_test.go
@@ -0,0 +1,71 @@
+// Copyright 2025 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const atlasDeploymentChartPath = "../../helm-charts/atlas-deployment"
+
+func TestAtlasDeployment_RendersAPIKeySecret(t *testing.T) {
+	stdout, stderr, err := helmTemplate(t,
+		"--namespace=default",
+		"--values=atlas_deployment_apikey_values.yaml",
+		atlasDeploymentChartPath,
+	)
+	require.NoError(t, err, "stderr: %s", stderr)
+
+	secret := findCredentialsSecret(t, stdout)
+	require.NotNil(t, secret, "expected a credentials Secret in rendered output")
+
+	assert.Equal(t, "6500000000000000000000aa", string(secret.Data["orgId"]))
+	assert.Equal(t, "abcdefgh", string(secret.Data["publicApiKey"]))
+	assert.Equal(t, "12345678-1234-1234-1234-1234567890ab", string(secret.Data["privateApiKey"]))
+	assert.NotContains(t, secret.Data, "clientId")
+	assert.NotContains(t, secret.Data, "clientSecret")
+}
+
+func TestAtlasDeployment_RendersServiceAccountSecret(t *testing.T) {
+	stdout, stderr, err := helmTemplate(t,
+		"--namespace=default",
+		"--values=atlas_deployment_sa_values.yaml",
+		atlasDeploymentChartPath,
+	)
+	require.NoError(t, err, "stderr: %s", stderr)
+
+	secret := findCredentialsSecret(t, stdout)
+	require.NotNil(t, secret, "expected a credentials Secret in rendered output")
+
+	assert.Equal(t, "6500000000000000000000aa", string(secret.Data["orgId"]))
+	assert.Equal(t, "mdb_sa_id_01234567890abcdef", string(secret.Data["clientId"]))
+	assert.Equal(t, "mdb_sa_sk_01234567890abcdefghijklmnop", string(secret.Data["clientSecret"]))
+	assert.NotContains(t, secret.Data, "publicApiKey")
+	assert.NotContains(t, secret.Data, "privateApiKey")
+}
+
+func TestAtlasDeployment_RejectsBothCredentialTypes(t *testing.T) {
+	_, stderr, err := helmTemplate(t,
+		"--namespace=default",
+		"--values=atlas_deployment_both_values.yaml",
+		atlasDeploymentChartPath,
+	)
+	require.Error(t, err, "expected helm template to fail when both credential types are set")
+	assert.Contains(t, stderr, "set either (publicApiKey,privateApiKey) or (clientId,clientSecret), not both",
+		"stderr did not include the mutual-exclusion message; got: %s", stderr)
+}
