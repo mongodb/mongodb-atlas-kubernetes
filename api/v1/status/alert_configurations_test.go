07673cfa83312e12b1a233e4196da70f5a477a25
Jose Vazquez <jose.vazquez@mongodb.com>
2026-09-08 17:49:13 +0200

    Fix main after stale SDK bump merge

--- api/v1/status/alert_configurations_test.go
+++ api/v1/status/alert_configurations_test.go
@@ -20,7 +20,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.mongodb.org/atlas-sdk/v20250312023/admin"
+	"go.mongodb.org/atlas-sdk/v20250312024/admin"
 	"go.uber.org/zap/zaptest"
 
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/api/v1/status"
