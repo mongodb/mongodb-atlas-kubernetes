109003ae8ed86b02c8887917a79f6d513d86d3c3
Jose Vazquez <jose.vazquez@mongodb.com>
2026-09-21 14:08:01 +0200

    Remove inneffective Helm chart e2e retry

--- test/e2e/helm_chart_test.go
+++ test/e2e/helm_chart_test.go
@@ -35,7 +35,7 @@ import (
 	"github.com/mongodb/mongodb-atlas-kubernetes/v2/test/helper/e2e/utils"
 )
 
-var _ = Describe("HELM charts", Ordered, FlakeAttempts(2), func() {
+var _ = Describe("HELM charts", Ordered, func() {
 	var data model.TestDataProvider
 	skipped := false
 
